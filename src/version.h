25427b3f88b67a621b2aa69cbf8a984d0a210184
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2000-04-24 13:48:43 +0000

    Bumped version up to 1.23 for the imminent release.

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@
 //
 //----------------------------------------------------------------------------
 
-#define EDGEVER    0x122
+#define EDGEVER    0x123
 #define EDGEVERFIX 0x100
 
 // -ES- 2000/03/04 The version of EDGE.WAD we require.
