4029aeb66fbf7a85f7fb31ecd2a88d83d345f7e9
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2000-05-30 07:11:22 +0000

    Bumped the required version of EDGE.WAD back down to 1.2.0.

--- edge/src/version.h
+++ edge/src/version.h
@@ -21,7 +21,7 @@
 
 // -ES- 2000/03/04 The version of EDGE.WAD we require.
 #define EDGE_WAD_VERSION 1
-#define EDGE_WAD_VERSION_FRAC 3
+#define EDGE_WAD_VERSION_FRAC 2
 #define EDGE_WAD_SUB_VERSION 0
 
 // Currently this must be one byte.  Should be removed later (and just
