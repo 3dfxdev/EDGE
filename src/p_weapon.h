db0a752f497880ac4128cf1fb8f5313acfd33e39
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2000-10-17 13:42:41 +0000

    Reworked the finite state machine code again, to make the JUMP() action work properly.

--- src/p_weapon.h
+++ src/p_weapon.h
@@ -52,6 +52,9 @@ typedef struct
   // current state.  NULL state means not active
   state_t *state;
 
+  // state to enter next.
+  int next_state;
+
   // time (in tics) remaining for current state
   int tics;
   
