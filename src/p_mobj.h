fb738a3a7d2b0f176ec59919fdd79d6b396ef40d
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2000-10-20 02:33:22 +0000

    Implemented crouching. Added support for VIEW_HEIGHT things.ddf command.

--- src/p_mobj.h
+++ src/p_mobj.h
@@ -311,7 +311,10 @@ typedef enum
   EF_BLOCKSHOTS = 0x1000000,
 
   // Thing never casts a shadow.  -AJA- 2000/09/29
-  EF_NOSHADOW = 0x2000000
+  EF_NOSHADOW = 0x2000000,
+
+  // Player is currently crouching.  -AJA- 2000/10/19
+  EF_CROUCHING = 0x4000000
 }
 mobjextendedflag_t;
 
