13f323832a3eaeb157e3e028ed38a06b6f623752
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2000-10-25 11:28:09 +0000

    Added P_RandomTest and M_RandomTest routines.

--- src/m_random.h
+++ src/m_random.h
@@ -28,39 +28,11 @@
 
 #include "dm_type.h"
 
-// 
-// M_Random
-//
-// Returns a number from 0 to 255.
-//
-// -AJA- Note: this function should be called for all random values
-// that do not interfere with demo/netgame synchronisation (for example,
-// placement of bullet puffs).
-
 int M_Random(void);
-
-// 
-// P_Random
-//
-// Returns a number from 0 to 255.
-//
-// -AJA- Note: that this function should be called for all random values
-// values that determine demo/netgame synchronisation (for example,
-// which way a monster should travel).
-
 int P_Random(void);
-
-// 
-// P_RandomNegPos
-//
-// Returns a number between -255 and 255, but skewed so that values near
-// zero have a higher probability.  Replaces "P_Random()-P_Random()" in
-// the code, which as Lee Killough points out can produce different
-// results depending upon the order of evaluation.
-//
-// -AJA- Note: same usage rules as P_Random.
-
 int P_RandomNegPos(void);
+boolean_t M_RandomTest(percent_t chance);
+boolean_t P_RandomTest(percent_t chance);
 
 // Fix randoms for demos.
 void M_SeedRandom(int seed);
