b1bc19ba4b5eb2f868012502fff241fbf456b1fd
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2001-05-09 16:25:09 +0000

    Made the result of SHORT() and LONG() macros signed, and added unsigned versions in the form USHORT() and ULONG().

--- src/m_swap.h
+++ src/m_swap.h
@@ -32,14 +32,14 @@
 #ifdef __BIG_ENDIAN__
 
 // Swap 16bit, that is, MSB and LSB byte.
-static unsigned short SHORT(unsigned short x)
+static short SHORT(unsigned short x)
 {
   // No masking with 0xFF should be necessary.
   return (x >> 8) | (x << 8);
 }
 
 // Swapping 32bit.
-static unsigned long LONG(unsigned long x)
+static long LONG(unsigned long x)
 {
   return
       (x >> 24)
@@ -50,9 +50,12 @@ static unsigned long LONG(unsigned long x)
 
 #else
 
-#define SHORT(x) (x)
-#define LONG(x)  (x)
+#define SHORT(x) ((short) (x))
+#define LONG(x)  ((long) (x))
 
 #endif
+
+#define USHORT(x) ((unsigned short) SHORT(x))
+#define ULONG(x)  ((unsigned long) LONG(x))
 
 #endif
