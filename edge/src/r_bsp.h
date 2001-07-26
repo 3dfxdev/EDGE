fb66ac96e177bfd65e083ac7a4fe72eedf14cfcb
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2001-07-26 14:25:43 +0000

    Updated year in file headers.

--- edge/src/r_bsp.h
+++ edge/src/r_bsp.h
@@ -2,7 +2,7 @@
 //  EDGE BSP Handling Code
 //----------------------------------------------------------------------------
 // 
-//  Copyright (c) 1999-2000  The EDGE Team.
+//  Copyright (c) 1999-2001  The EDGE Team.
 // 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
