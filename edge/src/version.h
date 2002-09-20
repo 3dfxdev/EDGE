669a889ff5c5b41e3f64341c2de5088743373927
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2002-09-20 09:32:07 +0000

    EPI Update

--- edge/src/version.h
+++ edge/src/version.h
@@ -2,7 +2,7 @@
 //  EDGE Version Header
 //----------------------------------------------------------------------------
 // 
-//  Copyright (c) 1999-2001  The EDGE Team.
+//  Copyright (c) 1999-2002  The EDGE Team.
 // 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
