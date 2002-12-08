ed80936ce2f81a15ef6aac22f011d3ca166515de
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2002-12-08 18:48:10 +0000

    Continuing EPI Work + Additional GCC V3 work

--- src/am_map.h
+++ src/am_map.h
@@ -1,4 +1,3 @@
-<<<<<<< am_map.h
 ///----------------------------------------------------------------------------
 //  EDGE Automap Functions
 //----------------------------------------------------------------------------
@@ -62,68 +61,3 @@ void AM_Drawer(void);
 void AM_Stop(void);
 
 #endif
-=======
-//----------------------------------------------------------------------------
-//  EDGE Automap Functions
-//----------------------------------------------------------------------------
-// 
-//  Copyright (c) 1999-2001  The EDGE Team.
-// 
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License
-//  as published by the Free Software Foundation; either version 2
-//  of the License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//----------------------------------------------------------------------------
-//
-//  Based on the DOOM source code, released by Id Software under the
-//  following copyright:
-//
-//    Copyright (C) 1993-1996 by id Software, Inc.
-//
-//----------------------------------------------------------------------------
-
-#ifndef __AMMAP_H__
-#define __AMMAP_H__
-
-#include "dm_defs.h"
-#include "e_event.h"
-
-//
-// Automap drawing structs
-//
-typedef struct
-{
-  flo_t x, y;
-}
-mpoint_t;
-
-typedef struct
-{
-  mpoint_t a, b;
-}
-mline_t;
-
-void AM_InitResolution(void);
-
-// Called by main loop.
-boolean_t AM_Responder(event_t * ev);
-
-// Called by main loop.
-void AM_Ticker(void);
-
-// Called by main loop,
-// called instead of view drawer if automap active.
-void AM_Drawer(void);
-
-// Called to force the automap to quit
-// if the level is completed while it is up.
-void AM_Stop(void);
-
-#endif
->>>>>>> 1.4
