b8985bc95cfb5b0758900c18a7f7beba20fe2bda
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2003-10-11 09:31:35 +0000

    Added Copyright Header.

--- src/s_sound.cc
+++ src/s_sound.cc
@@ -2,6 +2,8 @@
 //  EDGE Sound Handling Code (ENGINE LEVEL)
 //----------------------------------------------------------------------------
 // 
+//  Copyright (c) 1999-2003  The EDGE Team.
+//
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
 //  as published by the Free Software Foundation; either version 2
