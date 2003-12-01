e263f6a7e3b9517f903751227b553229e5376604
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2003-12-01 01:25:08 +0000

    Implemented new RESISTANCE_CLASS command for things.ddf. Made SHOT attacks honor the ddf RANGE command. Fix for bug that caused invincible enemies / inward thrusting when the true3Dgameplay option was disabled.

--- edge/src/p_map.cc
+++ edge/src/p_map.cc
@@ -1871,7 +1871,7 @@ static bool PIT_RadiusAttack(mobj_t * thing)
 
 	// recompute dist to be in range 0.0 (far away) to 1.0 (close)
 	CHECKVAL(bomb_I.range);
-	dist = (dist - bomb_I.range) / bomb_I.range;
+	dist = (bomb_I.range - dist) / bomb_I.range;
 
 	if (P_CheckSight(bomb_I.spot, thing))
 	{
