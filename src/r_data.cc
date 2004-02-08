939cfd8159caf6bb6ddb7cdc226e6be51046a915
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-02-08 08:17:44 +0000

    Reformatted. Fixed bug in R_AddTextureAnim(), which was calling W_ImageFromFlat() ! Fixed bug in PreCacheLevel, wasn't checking 'bottom' image of sidedefs.

--- src/r_data.cc
+++ src/r_data.cc
@@ -67,58 +67,58 @@
 // 
 void R_AddFlatAnim(animdef_t *anim)
 {
-  int start = W_CheckNumForName(anim->startname);
-  int end   = W_CheckNumForName(anim->endname);
+	int start = W_CheckNumForName(anim->startname);
+	int end   = W_CheckNumForName(anim->endname);
 
-  int file;
-  int s_offset, e_offset;
-  const int *lumps;
-  int total;
+	int file;
+	int s_offset, e_offset;
+	const int *lumps;
+	int total;
 
-  int i;
-  const image_t **flats;
-  
-  if (start == -1 || end == -1)
-  {
-    // sequence not valid.  Maybe it is the DOOM 1 IWAD.
-    return;
-  }
+	int i;
+	const image_t **flats;
 
-  file = W_FindFlatSequence(anim->startname, anim->endname, 
-      &s_offset, &e_offset);
+	if (start == -1 || end == -1)
+	{
+		// sequence not valid.  Maybe it is the DOOM 1 IWAD.
+		return;
+	}
 
-  if (file < 0)
-  {
-    I_Warning("Missing flat animation: %s-%s not in any wad.\n",
-        anim->startname, anim->endname);
-    return;
-  }
+	file = W_FindFlatSequence(anim->startname, anim->endname, 
+			&s_offset, &e_offset);
 
-  DEV_ASSERT2(s_offset <= e_offset);
+	if (file < 0)
+	{
+		I_Warning("Missing flat animation: %s-%s not in any wad.\n",
+				anim->startname, anim->endname);
+		return;
+	}
 
-  lumps = W_GetListLumps(file, LMPLST_Flats, &total);
+	DEV_ASSERT2(s_offset <= e_offset);
 
-  // determine animation sequence
-  lumps += s_offset;
-  total = e_offset - s_offset + 1;
+	lumps = W_GetListLumps(file, LMPLST_Flats, &total);
 
-  flats = Z_New(const image_t *, total);
+	// determine animation sequence
+	lumps += s_offset;
+	total = e_offset - s_offset + 1;
 
-  // lookup each flat
-  for (i=0; i < total; i++)
-  {
-    const char *name = W_GetLumpName(lumps[i]);
+	flats = Z_New(const image_t *, total);
 
-    // Note we use W_ImageFromFlat() here.  It might seem like a good
-    // optimisation to use the lump number directly, but we can't do
-    // that -- the lump list does NOT take overriding flats (in newer
-    // pwads) into account.
-    
-    flats[i] = W_ImageFromFlat(name);
-  }
+	// lookup each flat
+	for (i=0; i < total; i++)
+	{
+		const char *name = W_GetLumpName(lumps[i]);
 
-  W_AnimateImageSet(flats, total, anim->speed);
-  Z_Free(flats);
+		// Note we use W_ImageFromFlat() here.  It might seem like a good
+		// optimisation to use the lump number directly, but we can't do
+		// that -- the lump list does NOT take overriding flats (in newer
+		// pwads) into account.
+
+		flats[i] = W_ImageFromFlat(name);
+	}
+
+	W_AnimateImageSet(flats, total, anim->speed);
+	Z_Free(flats);
 }
 
 //
@@ -147,35 +147,35 @@ void R_AddFlatAnim(animdef_t *anim)
 // 
 void R_AddTextureAnim(animdef_t *anim)
 {
-  int set, s_offset, e_offset;
-  
-  int i, total;
-  const image_t **texs;
- 
-  set = W_FindTextureSequence(anim->startname, anim->endname,
-      &s_offset, &e_offset);
+	int set, s_offset, e_offset;
 
-  if (set < 0)
-  {
-    // sequence not valid.  Maybe it is the DOOM 1 IWAD.
-    return;
-  }
+	int i, total;
+	const image_t **texs;
 
-  DEV_ASSERT2(s_offset <= e_offset);
+	set = W_FindTextureSequence(anim->startname, anim->endname,
+			&s_offset, &e_offset);
 
-  total = e_offset - s_offset + 1;
-  
-  texs = Z_New(const image_t *, total);
+	if (set < 0)
+	{
+		// sequence not valid.  Maybe it is the DOOM 1 IWAD.
+		return;
+	}
 
-  // lookup each texture
-  for (i=0; i < total; i++)
-  {
-    const char *name = W_TextureNameInSet(set, s_offset + i);
-    texs[i] = W_ImageFromFlat(name);
-  }
+	DEV_ASSERT2(s_offset <= e_offset);
 
-  W_AnimateImageSet(texs, total, anim->speed);
-  Z_Free(texs);
+	total = e_offset - s_offset + 1;
+
+	texs = Z_New(const image_t *, total);
+
+	// lookup each texture
+	for (i=0; i < total; i++)
+	{
+		const char *name = W_TextureNameInSet(set, s_offset + i);
+		texs[i] = W_ImageFromTexture(name);
+	}
+
+	W_AnimateImageSet(texs, total, anim->speed);
+	Z_Free(texs);
 }
 
 //
@@ -183,70 +183,70 @@ void R_AddTextureAnim(animdef_t *anim)
 // 
 bool R_InitFlats(void)
 {
-  int max_file = W_GetNumFiles();
-  int j, file;
-  
-  int *F_lumps = NULL;
-  int numflats = 0;
-   
-  I_Printf("R_InitFlats...\n");
+	int max_file = W_GetNumFiles();
+	int j, file;
 
-  // iterate over each file, creating our big array of flats
+	int *F_lumps = NULL;
+	int numflats = 0;
 
-  for (file=0; file < max_file; file++)
-  {
-    const int *lumps;
-    int lumpnum;
+	I_Printf("R_InitFlats...\n");
 
-    lumps = W_GetListLumps(file, LMPLST_Flats, &lumpnum);
+	// iterate over each file, creating our big array of flats
 
-    if (lumpnum == 0)
-      continue;
+	for (file=0; file < max_file; file++)
+	{
+		const int *lumps;
+		int lumpnum;
 
-    Z_Resize(F_lumps, int, numflats + lumpnum);
+		lumps = W_GetListLumps(file, LMPLST_Flats, &lumpnum);
 
-    for (j=0; j < lumpnum; j++, numflats++)
-      F_lumps[numflats] = lumps[j];
-  }
+		if (lumpnum == 0)
+			continue;
 
-  if (numflats == 0)
-    I_Error("No flats found !  Make sure the chosen IWAD is valid.\n");
+		Z_Resize(F_lumps, int, numflats + lumpnum);
 
-  // now sort the flats, primarily by increasing name, secondarily by
-  // increasing lump number (a measure of newness).
+		for (j=0; j < lumpnum; j++, numflats++)
+			F_lumps[numflats] = lumps[j];
+	}
+
+	if (numflats == 0)
+		I_Error("No flats found !  Make sure the chosen IWAD is valid.\n");
+
+	// now sort the flats, primarily by increasing name, secondarily by
+	// increasing lump number (a measure of newness).
 
 #define CMP(a, b)  \
-    (strcmp(W_GetLumpName(a), W_GetLumpName(b)) < 0 || \
-     (strcmp(W_GetLumpName(a), W_GetLumpName(b)) == 0 && a < b))
-  QSORT(int, F_lumps, numflats, CUTOFF);
+	(strcmp(W_GetLumpName(a), W_GetLumpName(b)) < 0 || \
+	 (strcmp(W_GetLumpName(a), W_GetLumpName(b)) == 0 && a < b))
+		QSORT(int, F_lumps, numflats, CUTOFF);
 #undef CMP
 
-  // remove duplicate names.  We rely on the fact that newer lumps
-  // have greater lump values than older ones.  Because the QSORT took
-  // newness into account, only the last entry in a run of identically
-  // named flats needs to be kept.
+	// remove duplicate names.  We rely on the fact that newer lumps
+	// have greater lump values than older ones.  Because the QSORT took
+	// newness into account, only the last entry in a run of identically
+	// named flats needs to be kept.
 
-  for (j=1; j < numflats; j++)
-  {
-    int a = F_lumps[j - 1];
-    int b = F_lumps[j];
+	for (j=1; j < numflats; j++)
+	{
+		int a = F_lumps[j - 1];
+		int b = F_lumps[j];
 
-    if (strcmp(W_GetLumpName(a), W_GetLumpName(b)) == 0)
-      F_lumps[j - 1] = -1;
-  }
+		if (strcmp(W_GetLumpName(a), W_GetLumpName(b)) == 0)
+			F_lumps[j - 1] = -1;
+	}
 
 #if 0  // DEBUGGING
-  for (j=0; j < numflats; j++)
-  {
-    L_WriteDebug("FLAT #%d:  lump=%d  name=[%s]\n", j,
-        F_lumps[j], W_GetLumpName(F_lumps[j]));
-  }
+	for (j=0; j < numflats; j++)
+	{
+		L_WriteDebug("FLAT #%d:  lump=%d  name=[%s]\n", j,
+				F_lumps[j], W_GetLumpName(F_lumps[j]));
+	}
 #endif
-  
-  W_ImageCreateFlats(F_lumps, numflats); 
-  Z_Free(F_lumps);
 
-  return true;
+	W_ImageCreateFlats(F_lumps, numflats); 
+	Z_Free(F_lumps);
+
+	return true;
 }
 
 //
@@ -256,21 +256,23 @@ bool R_InitFlats(void)
 //
 bool R_InitPicAnims(void)
 {
-  int i;
+	int i;
 
-  // loop through animdefs, and add relevant anims.
-  // Note: reverse order, give priority to newer anims.
-  for (i = numanimdefs-1; i >= 0; i--)
-  {
-    animdef_t *A = animdefs[i];
+	// loop through animdefs, and add relevant anims.
+	// Note: reverse order, give priority to newer anims.
+	for (i = numanimdefs-1; i >= 0; i--)
+	{
+		animdef_t *A = animdefs[i];
 
-    if (A->istexture == true)
-      R_AddTextureAnim(A);
-    else
-      R_AddFlatAnim(A);
-  }
+		DEV_ASSERT2(A);
 
-  return true;
+		if (A->istexture)
+			R_AddTextureAnim(A);
+		else
+			R_AddFlatAnim(A);
+	}
+
+	return true;
 }
 
 //
@@ -278,55 +280,55 @@ bool R_InitPicAnims(void)
 //
 static void R_PrecacheSprites(void)
 {
-  int i;
-  byte *sprite_present;
-  mobj_t *mo;
-    
-  DEV_ASSERT2(numsprites > 0);
+	int i;
+	byte *sprite_present;
+	mobj_t *mo;
 
-  sprite_present = Z_ClearNew(byte, numsprites);
+	DEV_ASSERT2(numsprites > 0);
 
-  for (mo = mobjlisthead; mo; mo = mo->next)
-  {
-    if (mo->sprite < 0 || mo->sprite >= numsprites)
-      continue;
+	sprite_present = Z_ClearNew(byte, numsprites);
 
-    sprite_present[mo->sprite] = 1;
-  }
+	for (mo = mobjlisthead; mo; mo = mo->next)
+	{
+		if (mo->sprite < 0 || mo->sprite >= numsprites)
+			continue;
 
-  for (i=0; i < numsprites; i++)
-  {
-    spritedef_t *sp = sprites + i;
-    int fr, rot;
+		sprite_present[mo->sprite] = 1;
+	}
 
-    const image_t *cur_image;
-    const image_t *last_image = NULL;  // an optimisation
+	for (i=0; i < numsprites; i++)
+	{
+		spritedef_t *sp = sprites + i;
+		int fr, rot;
 
-    if (! sprite_present[i] || sp->numframes == 0)
-      continue;
+		const image_t *cur_image;
+		const image_t *last_image = NULL;  // an optimisation
 
-    DEV_ASSERT2(sp->frames);
+		if (! sprite_present[i] || sp->numframes == 0)
+			continue;
 
-    for (fr=0; fr < sp->numframes; fr++)
-    {
-      if (! sp->frames[fr].finished)
-        continue;
+		DEV_ASSERT2(sp->frames);
 
-      for (rot=0; rot < 16; rot++)
-      {
-        cur_image = sp->frames[fr].images[rot];
+		for (fr=0; fr < sp->numframes; fr++)
+		{
+			if (! sp->frames[fr].finished)
+				continue;
 
-        if (cur_image == NULL || cur_image == last_image)
-          continue;
+			for (rot=0; rot < 16; rot++)
+			{
+				cur_image = sp->frames[fr].images[rot];
 
-        W_ImagePreCache(cur_image);
+				if (cur_image == NULL || cur_image == last_image)
+					continue;
 
-        last_image = cur_image;
-      }
-    }
-  }
- 
-  Z_Free(sprite_present);
+				W_ImagePreCache(cur_image);
+
+				last_image = cur_image;
+			}
+		}
+	}
+
+	Z_Free(sprite_present);
 }
 
 //
@@ -338,76 +340,76 @@ static void R_PrecacheSprites(void)
 //                   
 void R_PrecacheLevel(void)
 {
-  const image_t ** images;
+	const image_t ** images;
 
-  int max_image;
-  int count = 0;
-  int i;
+	int max_image;
+	int count = 0;
+	int i;
 
-  if (demoplayback)
-    return;
+	if (demoplayback)
+		return;
 
-  // do sprites first -- when memory is tight, they'll be the first
-  // images to be removed from the cache(s).
- 
-  if (M_CheckParm("-fastsprite") || M_CheckParm("-fastsprites"))
-  {
-    R_PrecacheSprites();
-  }
- 
-  // maximum possible images
-  max_image = 1 + 3 * numsides + 2 * numsectors;
-   
-  images = Z_New(const image_t *, max_image);
+	// do sprites first -- when memory is tight, they'll be the first
+	// images to be removed from the cache(s).
 
-  // Sky texture is always present.
-  images[count++] = sky_image;
+	if (M_CheckParm("-fastsprite") || M_CheckParm("-fastsprites"))
+	{
+		R_PrecacheSprites();
+	}
 
-  // add in sidedefs
-  for (i=0; i < numsides; i++)
-  {
-    if (sides[i].top.image)
-      images[count++] = sides[i].top.image;
-    
-    if (sides[i].middle.image)
-      images[count++] = sides[i].middle.image;
+	// maximum possible images
+	max_image = 1 + 3 * numsides + 2 * numsectors;
 
-    if (sides[i].middle.image)
-      images[count++] = sides[i].middle.image;
-  }
+	images = Z_New(const image_t *, max_image);
 
-  DEV_ASSERT2(count <= max_image);
+	// Sky texture is always present.
+	images[count++] = sky_image;
 
-  // add in planes
-  for (i=0; i < numsectors; i++)
-  {
-    if (sectors[i].floor.image)
-      images[count++] = sectors[i].floor.image;
+	// add in sidedefs
+	for (i=0; i < numsides; i++)
+	{
+		if (sides[i].top.image)
+			images[count++] = sides[i].top.image;
 
-    if (sectors[i].ceil.image)
-      images[count++] = sectors[i].ceil.image;
-  }
+		if (sides[i].middle.image)
+			images[count++] = sides[i].middle.image;
 
-  DEV_ASSERT2(count <= max_image);
+		if (sides[i].bottom.image)
+			images[count++] = sides[i].bottom.image;
+	}
 
-  // Sort the images, so we can ignore the duplicates
-  
+	DEV_ASSERT2(count <= max_image);
+
+	// add in planes
+	for (i=0; i < numsectors; i++)
+	{
+		if (sectors[i].floor.image)
+			images[count++] = sectors[i].floor.image;
+
+		if (sectors[i].ceil.image)
+			images[count++] = sectors[i].ceil.image;
+	}
+
+	DEV_ASSERT2(count <= max_image);
+
+	// Sort the images, so we can ignore the duplicates
+
 #define CMP(a, b)  (a < b)
-  QSORT(const image_t *, images, count, CUTOFF);
+	QSORT(const image_t *, images, count, CUTOFF);
 #undef CMP
 
-  for (i=0; i < count; i++)
-  {
-    DEV_ASSERT2(images[i]);
-    
-    if (i+1 < count && images[i] == images[i + 1])
-      continue;
+	for (i=0; i < count; i++)
+	{
+		DEV_ASSERT2(images[i]);
 
-    if (images[i] == skyflatimage)
-      continue;
+		if (i+1 < count && images[i] == images[i + 1])
+			continue;
 
-    W_ImagePreCache(images[i]);
-  }
+		if (images[i] == skyflatimage)
+			continue;
 
-  Z_Free(images);
+		W_ImagePreCache(images[i]);
+	}
+
+	Z_Free(images);
 }
