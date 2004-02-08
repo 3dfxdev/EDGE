f51b8be699c24733e8b7ead79ec1e5085e0251c6
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-02-08 04:24:22 +0000

    Moved common definitions to the top. Added MACOSX section. Produce an #error if no system is defined. Fix for __int64 under GCC.

--- deh_edge/i_defs.h
+++ deh_edge/i_defs.h
@@ -13,93 +13,75 @@
 #ifndef __SYSTEM_SPECIFIC_DEFS__
 #define __SYSTEM_SPECIFIC_DEFS__
 
-// DOS GCC
-#ifdef DJGPP
-
-typedef long long Int64;
+// COMMON STUFF...
 #define FLOAT_IEEE_754
 typedef unsigned char byte;
 
-#include <ctype.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <math.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <values.h>
-#include <assert.h>
+#include <ctype.h>
 #include <errno.h>
-#include <sys\stat.h>
+
+#include <limits.h>
+#include <values.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+// DOS GCC
+#if defined(DJGPP)
+
+typedef long long Int64;
+
+#include <unistd.h>
 #include <dos.h>
 
-#endif
-
-// Borland C++ V5.5 for Win32
-#ifdef WIN32
-#ifdef __BORLANDC__
+// Windows
+#elif defined(WIN32)
 
 #define STRICT
 #define _WINDOWS
 #define WIN32_LEAN_AND_MEAN
 
+#ifdef __GNUC__
+typedef long long Int64;
+#else
 typedef __int64 Int64;
-#define FLOAT_IEEE_754
-
-typedef unsigned char byte;
+#endif
 
 #include <windows.h>
 
-#include <ctype.h>
-#include <direct.h>
-#include <fcntl.h>
 #include <io.h>
-#include <limits.h>
-#include <math.h>
 #include <malloc.h>
 #include <mem.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <errno.h>
-#include <sys\stat.h>
-#include <time.h>
 
 // Access() define values. Nicked from DJGPP's <unistd.h>
+#ifndef R_OK
 #define R_OK    0x02
 #define W_OK    0x04
-
-#endif
 #endif
 
-// LINUX GCC
-#ifdef LINUX
+// LINUX
+#elif defined(LINUX)
+
+#include <unistd.h>
 
 typedef long long Int64;
-#define FLOAT_IEEE_754
-typedef unsigned char byte;
 
-#include <ctype.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <math.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <errno.h>
-#include <string.h>
-#include <time.h>
+// MacOS X
+#elif defined (MACOSX)
+
 #include <unistd.h>
-#include <values.h>
-#include <sys/stat.h>
 
+typedef long long Int64;
+
+#else
+#error Unknown System (not DJGPP, WIN32, LINUX or MACOSX)
 #endif
 
 #endif /*__SYSTEM_SPECIFIC_DEFS__*/
