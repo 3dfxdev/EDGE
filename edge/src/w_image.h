378a475e3205110622e192a9e36749711e80a307
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2004-06-10 16:21:34 +0000

    colourmap_t -> colourmap_s

--- edge/src/w_image.h
+++ edge/src/w_image.h
@@ -176,7 +176,7 @@ void W_UnlockImagesOGL(void);
 
 const cached_image_t *W_ImageCache(const image_t *image, 
 								   image_mode_e mode, int mip, bool anim,
-								   const colourmap_t *trans = NULL);
+								   const colourmap_c *trans = NULL);
 void W_ImageDone(const cached_image_t *c);
 void W_ImagePreCache(const image_t *image);
 
