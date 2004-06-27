96f381733963708a980366b538b4ac081047c969
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2004-06-27 11:34:20 +0000

    Implemented new gamedef/world intermission classes.

--- edge/src/ddf_game.cc
+++ edge/src/ddf_game.cc
@@ -29,59 +29,26 @@
 #include "p_mobj.h"
 #include "z_zone.h"
 
+#include "./epi/epistring.h"
+
 #undef  DF
 #define DF  DDF_CMD
 
-static wi_map_t buffer_wi;
-static wi_map_t *dynamic_wi;
+gamedef_container_c gamedefs;
 
-static const wi_map_t template_wi = 
-{
-	DDF_BASE_NIL,	// ddf
+static gamedef_c buffer_gamedef;
+static wi_animdef_c buffer_animdef;
+static wi_framedef_c buffer_framedef;
 
-	NULL,			// anims
-	0,				// numanims
-	NULL,			// mappos
-	NULL,			// mapdone
-	0,				// nummaps
-
-	"",				// background
-	"",				// splatpic
-	{"", ""},		// yah[2]
-	"",				// bg_camera
-	0,				// music
-
-	sfx_None,		// percent
-	sfx_None,		// done
-	sfx_None,		// endmap
-	sfx_None,		// nextmap
-	sfx_None,		// accel_snd
-	sfx_None,		// frag_snd
-
-	"",				// firstmap
-	"",				// namegraphic
-	NULL,			// titlepics
-	0,				// numtitlepics
-	0,				// titlemusic;
-	TICRATE * 4,	// titletics;
-	0 				// special_music;
-};
+static gamedef_c* dynamic_gamedef;
 
 static void DDF_GameGetPic (const char *info, void *storage);
 static void DDF_GameGetFrames (const char *info, void *storage);
 static void DDF_GameGetMap (const char *info, void *storage);
 
-wi_map_t **wi_maps = NULL;
-int num_wi_maps = 0;
+#define DDF_CMD_BASE  buffer_gamedef
 
-static stack_array_t wi_maps_a;
-
-static wi_anim_t buffer_anim;
-static wi_frame_t buffer_frame;
-
-#define DDF_CMD_BASE  buffer_wi
-
-static const commandlist_t wi_commands[] = 
+static const commandlist_t gamedef_commands[] = 
 {
 	DF ("INTERMISSION GRAPHIC", background, DDF_MainGetInlineStr10),
 	DF ("INTERMISSION CAMERA", bg_camera, DDF_MainGetInlineStr32),
@@ -104,7 +71,7 @@ static const commandlist_t wi_commands[] =
 	// these don't quite fit in yet
 	DF ("TITLE GRAPHIC", ddf, DDF_GameGetPic),
 	DF ("MAP", ddf, DDF_GameGetMap),
-	{"ANIM", DDF_GameGetFrames, &buffer_frame, NULL},
+	{"ANIM", DDF_GameGetFrames, &buffer_framedef, NULL},
 
 	DDF_CMD_END
 };
@@ -116,50 +83,34 @@ static const commandlist_t wi_commands[] =
 
 static bool GameStartEntry (const char *name)
 {
-	int i;
-	bool replaces = false;
+	gamedef_c *existing = NULL;
 
 	if (name && name[0])
-	{
-		for (i = 0; i < num_wi_maps; i++)
-		{
-			if (DDF_CompareName (wi_maps[i]->ddf.name, name) == 0)
-			{
-				dynamic_wi = wi_maps[i];
-				replaces = true;
-				break;
-			}
-		}
-
-		// if found, adjust pointer array to keep newest entries at end
-		if (replaces && i < (num_wi_maps - 1))
-		{
-			Z_MoveData (wi_maps + i, wi_maps + i + 1, wi_map_t *,
-				    num_wi_maps - i);
-			wi_maps[num_wi_maps - 1] = dynamic_wi;
-		}
-	}
+		existing = gamedefs.Lookup(name);
 
 	// not found, create a new one
-	if (!replaces)
+	if (! existing)
 	{
-		Z_SetArraySize (&wi_maps_a, ++num_wi_maps);
+		dynamic_gamedef = new gamedef_c;
 
-		dynamic_wi = wi_maps[num_wi_maps - 1];
-		dynamic_wi->ddf.name = (name && name[0]) ? Z_StrDup (name) :
-			DDF_MainCreateUniqueName ("UNNAMED_WI_MAP",
-						  num_wi_maps);
+		dynamic_gamedef->ddf.name = (name && name[0]) ? Z_StrDup(name) :
+			DDF_MainCreateUniqueName("UNNAMED_GAMEDEF", gamedefs.GetSize());
+
+		gamedefs.Insert(dynamic_gamedef);
+	}
+	else
+	{
+		dynamic_gamedef = existing;
 	}
 
-	dynamic_wi->ddf.number = 0;
+	dynamic_gamedef->ddf.number = 0;
 
-	Z_Clear (&buffer_anim, wi_anim_t, 1);
-	Z_Clear (&buffer_frame, wi_frame_t, 1);
+	// instantiate the static entries
+	buffer_gamedef.Default();
+	buffer_animdef.Default();
+	buffer_framedef.Default();
 
-	// instantiate the static entry
-	buffer_wi = template_wi;
-
-	return replaces;
+	return (existing != NULL);
 }
 
 static void GameParseField (const char *field, const char *contents,
@@ -169,36 +120,28 @@ static void GameParseField (const char *field, const char *contents,
 	L_WriteDebug ("GAME_PARSE: %s = %s;\n", field, contents);
 #endif
 
-	if (!DDF_MainParseField (wi_commands, field, contents))
+	if (!DDF_MainParseField (gamedef_commands, field, contents))
 		DDF_WarnError ("Unknown games.ddf command: %s\n", field);
 }
 
 static void GameFinishEntry (void)
 {
-	ddf_base_t base;
-
 	// FIXME: check stuff...
 
 	// transfer static entry to dynamic entry
-
-	base = dynamic_wi->ddf;
-	dynamic_wi[0] = buffer_wi;
-	dynamic_wi->ddf = base;
+	dynamic_gamedef->CopyDetail(buffer_gamedef);
 
 	// compute CRC...
-	CRC32_Init (&dynamic_wi->ddf.crc);
-
+	// FIXME: Use EPI CRC Class
+	CRC32_Init (&dynamic_gamedef->ddf.crc);
 	// FIXME: more stuff...
-
-	CRC32_Done (&dynamic_wi->ddf.crc);
+	CRC32_Done (&dynamic_gamedef->ddf.crc);
 }
 
 static void GameClearAll (void)
 {
 	// safe to delete game entries
-
-	num_wi_maps = 0;
-	Z_SetArraySize (&wi_maps_a, num_wi_maps);
+	gamedefs.Clear();
 }
 
 
@@ -232,55 +175,53 @@ void DDF_ReadGames (void *data, int size)
 	DDF_MainReadFile (&games);
 }
 
-void DDF_GameInit (void)
+void DDF_GameInit(void)
 {
-	Z_InitStackArray (&wi_maps_a, (void ***) &wi_maps, sizeof (wi_map_t),
-			  0);
+	gamedefs.Clear();			// <-- Consistent with existing behaviour (-ACB- 2004/05/23)
 }
 
-void DDF_GameCleanUp (void)
+void DDF_GameCleanUp(void)
 {
-	if (num_wi_maps == 0)
-		I_Error ("There are no games defined in DDF !\n");
+	if (gamedefs.GetSize() == 0)
+		I_Error("There are no games defined in DDF !\n");
+		
+	gamedefs.Trim();
 }
 
-
-static void DDF_GameAddFrame (void)
+static void DDF_GameAddFrame(void)
 {
-	Z_Resize (buffer_anim.frames, wi_frame_t, buffer_anim.numframes + 1);
-	buffer_anim.frames[buffer_anim.numframes++] = buffer_frame;
-	memset (&buffer_frame, 0, sizeof (wi_frame_t));
+	wi_framedef_c *f = new wi_framedef_c(buffer_framedef);
+	
+	buffer_animdef.frames.Insert(f);
+	buffer_framedef.Default();
 }
 
-static void DDF_GameAddAnim (void)
+static void DDF_GameAddAnim(void)
 {
-	Z_Resize (buffer_wi.anims, wi_anim_t, buffer_wi.numanims + 1);
-	if (buffer_anim.level[0])
-		buffer_anim.type = WI_LEVEL;
+	wi_animdef_c *a = new wi_animdef_c(buffer_animdef);
+	
+	if (a->level[0])
+		a->type = wi_animdef_c::WI_LEVEL;
 	else
-		buffer_anim.type = WI_NORMAL;
-	buffer_wi.anims[buffer_wi.numanims++] = buffer_anim;
-	memset (&buffer_anim, 0, sizeof (wi_anim_t));
+		a->type = wi_animdef_c::WI_NORMAL;	
+		
+	buffer_gamedef.anims.Insert(a);
+	buffer_animdef.Default();
 }
 
-static void DDF_GameGetFrames (const char *info, void *storage)
+static void DDF_GameGetFrames(const char *info, void *storage)
 {
-	char *s = Z_StrDup (info);
-	char *tok;
-
-	wi_frame_t *f = (wi_frame_t *) storage;
-
-	int i;
-
-	struct
-	{
-		int type;
-		void *dest;
-	}
-	f_dest[4];
-
+	epi::string_c s, s2;
+	wi_framedef_c *f;
+	int i, pos;
+	
+	struct { int type; void *dest; } f_dest[4];
+	
+	f = (wi_framedef_c *) storage;
+	s = info;
+	
 	f_dest[0].type = 1;
-	f_dest[0].dest = f->pic;
+	f_dest[0].dest = &f->pic;
 	f_dest[1].type = 0;
 	f_dest[1].dest = &f->tics;
 	f_dest[2].type = 0;
@@ -288,97 +229,662 @@ static void DDF_GameGetFrames (const char *info, void *storage)
 	f_dest[3].type = 2;
 	f_dest[3].dest = &f->pos.y;
 
-	tok = strtok (s, ":");
-
-	if (tok[0] == '#')
+	if (s.GetLength() && s.GetAt(0) == '#')
 	{
-		if (!buffer_anim.numframes)
+		pos = s2.Find(':');
+
+		s2 = s;
+		s2.TruncateAt(pos);
+		
+		if (buffer_animdef.frames.GetSize() == 0)
 		{
-			Z_StrNCpy (buffer_anim.level, tok + 1, 8);
-			tok = strtok (NULL, ":");
+			// Remove the hash
+			s2.RemoveLeft(1);
+
+			// Copy 
+			buffer_animdef.level.Set(s2.GetString());
+			 
+			// Strip out the map from the working buffer
+			s.RemoveLeft(pos+1);
 		}
-		else if (!strncmp (tok, "#END", 4))
+		else if (s2 == "#END")
 		{
-			DDF_GameAddAnim ();
-			Z_Free (s);
+			DDF_GameAddAnim();
 			return;
 		}
 		else
-			DDF_Error ("Invalid # command '%s'\n", tok);
+			DDF_Error ("Invalid # command '%s'\n", s2.GetString());
 	}
-
-	for (i = 0; tok && i < 4; tok = strtok (NULL, ":"), i++)
+	
+	for (i = 0; i < 4; i++)
 	{
-		if (f_dest[i].type & 1)
-			Z_StrNCpy ((char *) f_dest[i].dest, tok, 8);
-		else
-			*(int *) f_dest[i].dest = atoi (tok);
+		pos = s.Find(':');
 
+		s2 = s;
+
+		if (pos >= 0)
+		{
+			s2.TruncateAt(pos);
+			s.RemoveLeft(pos+1);
+		}
+
+		if (f_dest[i].type & 1)
+		{	
+			lumpname_c *ln = (lumpname_c*)f_dest[i].dest;
+			ln->Set(s2.GetString());
+		}
+		else
+		{
+			*(int *) f_dest[i].dest = atoi (s2);
+		}
+		
 		if (f_dest[i].type & 2)
 		{
-			DDF_GameAddFrame ();
-			Z_Free (s);
+			DDF_GameAddFrame();
 			return;
 		}
+
+		if (pos<0)
+			break;
 	}
+
 	DDF_Error ("Bad Frame command '%s'\n", info);
 }
 
 static void DDF_GameGetMap(const char *info, void *storage)
 {
-	char *s = Z_StrDup (info);
-	char *tok;
-	struct
-	{
-		int type;
-		void *dest;
-	}
-	dest[3];
-	int i;
+	epi::string_c s, s2;
+	int i, pos;
+	wi_mapposdef_c *mp;
+	
+	struct { int type; void *dest; } dest[3];
 
-	Z_Resize (buffer_wi.mappos, mappos_t, buffer_wi.nummaps + 1);
-	Z_Resize (buffer_wi.mapdone, bool, buffer_wi.nummaps + 1);
-
+	s = info;
+	mp = new wi_mapposdef_c();
+	
 	dest[0].type = 1;
-	dest[0].dest = buffer_wi.mappos[buffer_wi.nummaps].name;
+	dest[0].dest = &mp->name;
 	dest[1].type = 0;
-	dest[1].dest = &buffer_wi.mappos[buffer_wi.nummaps].pos.x;
+	dest[1].dest = &mp->pos.x;
 	dest[2].type = 2;
-	dest[2].dest = &buffer_wi.mappos[buffer_wi.nummaps++].pos.y;
-	for (tok = strtok (s, ":"), i = 0;
-	     tok && i < 3; tok = strtok (NULL, ":"), i++)
+	dest[2].dest = &mp->pos.y;
+
+	for (i = 0; i < 3; i++)
 	{
+		pos = s.Find(':');
+
+		s2 = s;
+
+		if (pos >= 0)
+		{
+			s2.TruncateAt(pos);
+			s.RemoveLeft(pos+1);
+		}
+
 		if (dest[i].type & 1)
-			Z_StrNCpy ((char *) dest[i].dest, tok, 8);
+		{
+			lumpname_c *ln = (lumpname_c*)dest[i].dest;
+			ln->Set(s2);
+		}
 		else
-			*(int *) dest[i].dest = atoi (tok);
+		{
+			*(int *) dest[i].dest = atoi (s2);
+		}
+			
 		if (dest[i].type & 2)
 		{
-			Z_Free (s);
+			buffer_gamedef.mappos.Insert(mp);
 			return;
 		}
+		
+		if (pos < 0)
+			break;
 	}
+	
+	if (mp)
+		delete mp;
+	
 	DDF_Error ("Bad Map command '%s'\n", info);
 }
 
 static void DDF_GameGetPic (const char *info, void *storage)
 {
-	Z_Resize (buffer_wi.titlepics, char *, buffer_wi.numtitlepics + 1);
-
-	buffer_wi.titlepics[buffer_wi.numtitlepics] = Z_StrDup (info);
-	buffer_wi.numtitlepics++;
+	buffer_gamedef.titlepics.Insert(info);
 }
 
-const wi_map_t *DDF_GameLookup (const char *name)
-{
-	int i;
+// --> world intermission mappos class
 
-	for (i = 0; i < num_wi_maps; i++)
+//
+// wi_mapposdef_c Constructor
+//
+wi_mapposdef_c::wi_mapposdef_c()
+{
+}
+
+//
+// wi_mapposdef_c Copy constructor
+//
+wi_mapposdef_c::wi_mapposdef_c(wi_mapposdef_c &rhs)
+{
+	Copy(rhs);
+}
+
+//
+// wi_mapposdef_c Destructor
+//
+wi_mapposdef_c::~wi_mapposdef_c()
+{
+}
+
+//
+// wi_mapposdef_c::Copy()
+//
+void wi_mapposdef_c::Copy(wi_mapposdef_c &src)
+{
+	name = src.name;
+	pos = src.pos;
+}
+
+//
+// wi_mapposdef_c assignment operator
+//
+wi_mapposdef_c& wi_mapposdef_c::operator=(wi_mapposdef_c &rhs)
+{
+	if (&rhs != this)
+		Copy(rhs);
+
+	return *this;
+}
+
+// --> world intermission mappos container class
+
+//
+// wi_mapposdef_container_c Constructor
+//
+wi_mapposdef_container_c::wi_mapposdef_container_c() 
+	: epi::array_c(sizeof(wi_mapposdef_c*))
+{
+}
+
+//
+// wi_mapposdef_container_c Copy constructor
+//
+wi_mapposdef_container_c::wi_mapposdef_container_c(wi_mapposdef_container_c &rhs)
+	: epi::array_c(sizeof(wi_mapposdef_c*))
+{
+	Copy(rhs);
+}
+
+//
+// wi_mapposdef_container_c Destructor
+//
+wi_mapposdef_container_c::~wi_mapposdef_container_c()
+{
+	Clear();
+}
+
+//
+// wi_mapposdef_container_c::CleanupObject()
+//
+void wi_mapposdef_container_c::CleanupObject(void *obj)
+{
+	wi_mapposdef_c *mp = *(wi_mapposdef_c**)obj;
+
+	if (mp)
+		delete mp;
+
+	return;
+}
+
+//
+// wi_mapposdef_container_c::Copy()
+//
+void wi_mapposdef_container_c::Copy(wi_mapposdef_container_c &src)
+{
+	epi::array_iterator_c it;
+	wi_mapposdef_c *wi;
+	wi_mapposdef_c *wi2;
+	
+	Size(src.GetSize());
+
+	for (it = src.GetBaseIterator(); it.IsValid(); it++)
 	{
-		if (DDF_CompareName (name, wi_maps[i]->ddf.name) == 0)
-			return wi_maps[i];
+		wi = ITERATOR_TO_TYPE(it, wi_mapposdef_c*);
+		if (wi)
+		{
+			wi2 = new wi_mapposdef_c(*wi);
+			Insert(wi2);
+		}
 	}
 
-	DDF_Error ("Unknown game name: %s\n", name);
+	Trim();
+}
+
+//
+// wi_mapposdef_container_c assignment operator
+//
+wi_mapposdef_container_c& wi_mapposdef_container_c::operator=(wi_mapposdef_container_c &rhs)
+{
+	if (&rhs != this)
+	{
+		Clear();
+		Copy(rhs);
+	}
+
+	return *this;
+}
+
+// --> world intermission framedef class
+
+//
+// wi_framedef_c Constructor
+//
+wi_framedef_c::wi_framedef_c()
+{
+	Default();
+}
+
+//
+// wi_framedef_c Copy constructor
+//
+wi_framedef_c::wi_framedef_c(wi_framedef_c &rhs)
+{
+	Copy(rhs); 
+}
+
+//
+// wi_framedef_c Destructor
+//
+wi_framedef_c::~wi_framedef_c()
+{
+}
+
+//
+// wi_framedef_c::Copy()
+//
+void wi_framedef_c::Copy(wi_framedef_c &src)
+{
+	tics = src.tics;
+	pos = src.pos;	
+	pic = src.pic;
+}
+
+//
+// wi_framedef_c::Default()
+//
+void wi_framedef_c::Default()
+{
+	tics = 0;
+	pos.x = 0;	
+	pos.y = 0;	
+	pic.Clear(); 
+}
+
+//
+// wi_framedef_c assignment operator
+//
+wi_framedef_c& wi_framedef_c::operator=(wi_framedef_c &rhs)
+{
+	if (&rhs != this)
+		Copy(rhs);
+
+	return *this;
+}
+
+// --> world intermission framedef container class
+
+//
+// wi_framedef_container_c Constructor
+//
+wi_framedef_container_c::wi_framedef_container_c()
+	: epi::array_c(sizeof(wi_framedef_c*))
+{
+}
+
+//
+// wi_framedef_container_c Copy constructor
+//
+wi_framedef_container_c::wi_framedef_container_c(wi_framedef_container_c &rhs)
+	: epi::array_c(sizeof(wi_framedef_c*))
+{
+	Copy(rhs);
+}
+
+//
+// wi_framedef_container_c Destructor
+//
+wi_framedef_container_c::~wi_framedef_container_c()
+{
+	Clear();
+}
+
+//
+// wi_framedef_container_c::CleanupObject()
+//
+void wi_framedef_container_c::CleanupObject(void *obj)
+{
+	wi_framedef_c *f = *(wi_framedef_c**)obj;
+
+	if (f)
+		delete f;
+
+	return;
+}
+
+//
+// wi_framedef_container_c::Copy()
+//
+void wi_framedef_container_c::Copy(wi_framedef_container_c &src)
+{
+	epi::array_iterator_c it;
+	wi_framedef_c *f;
+	wi_framedef_c *f2;
+	
+	Size(src.GetSize());
+
+	for (it = src.GetBaseIterator(); it.IsValid(); it++)
+	{
+		f = ITERATOR_TO_TYPE(it, wi_framedef_c*);
+		if (f)
+		{
+			f2 = new wi_framedef_c(*f);
+			Insert(f2);
+		}
+	}
+
+	Trim();
+}
+
+//
+// wi_framedef_container_c assignment operator
+//
+wi_framedef_container_c& wi_framedef_container_c::operator=(wi_framedef_container_c& rhs)
+{
+	if (&rhs != this)
+	{
+		Clear();
+		Copy(rhs);
+	}
+
+	return *this;
+}
+
+// --> world intermission animdef class
+
+//
+// wi_animdef_c Constructor
+//
+wi_animdef_c::wi_animdef_c()
+{
+	Default();
+}
+
+//
+// wi_animdef_c Copy constructor
+//
+wi_animdef_c::wi_animdef_c(wi_animdef_c &rhs)
+{
+	Copy(rhs);
+}
+
+//
+// wi_animdef_c Destructor
+//
+wi_animdef_c::~wi_animdef_c()
+{
+}
+
+//
+// void Copy()
+//
+void wi_animdef_c::Copy(wi_animdef_c &src)
+{
+	type = src.type;
+	level = src.level;
+	frames = src.frames;
+}
+
+//
+// wi_animdef_c::Default()
+//
+void wi_animdef_c::Default()
+{
+	type = WI_NORMAL;
+	level.Clear();
+	frames.Clear();
+}
+
+
+//
+// wi_animdef_c assignment operator
+//
+wi_animdef_c& wi_animdef_c::operator=(wi_animdef_c &rhs)
+{
+	if (&rhs != this)
+		Copy(rhs);
+
+	return *this;
+}
+
+// --> world intermission anim container class
+
+//
+// wi_animdef_container_c Constructor
+//
+wi_animdef_container_c::wi_animdef_container_c() : epi::array_c(sizeof(wi_animdef_c*))
+{
+}
+
+//
+// wi_animdef_container_c Copy constructor
+//
+wi_animdef_container_c::wi_animdef_container_c(wi_animdef_container_c &rhs)
+	: epi::array_c(sizeof(wi_animdef_c*))
+{
+	Copy(rhs);
+}
+
+//
+// wi_animdef_container_c Destructor
+//
+wi_animdef_container_c::~wi_animdef_container_c()
+{
+	Clear();
+}
+
+//
+// wi_animdef_container_c::CleanupObject()
+//
+void wi_animdef_container_c::CleanupObject(void *obj)
+{
+	wi_animdef_c *a = *(wi_animdef_c**)obj;
+
+	if (a)
+		delete a;
+
+	return;
+}
+
+//
+// wi_animdef_container_c::Copy()
+//
+void wi_animdef_container_c::Copy(wi_animdef_container_c &src)
+{
+	epi::array_iterator_c it;
+	wi_animdef_c *a;
+	wi_animdef_c *a2;
+	
+	Size(src.GetSize());
+
+	for (it = src.GetBaseIterator(); it.IsValid(); it++)
+	{
+		a = ITERATOR_TO_TYPE(it, wi_animdef_c*);
+		if (a)
+		{
+			a2 = new wi_animdef_c(*a);
+			Insert(a2);
+		}
+	}
+
+	Trim();
+}
+
+
+//
+// wi_animdef_container_c assignment operator
+//
+wi_animdef_container_c& wi_animdef_container_c::operator=(wi_animdef_container_c &rhs)
+{
+	if (&rhs != this)
+	{
+		Clear();
+		Copy(rhs);
+	}
+
+	return *this;
+}
+
+
+// --> game definition class 
+
+//
+// gamedef_c Constructor
+//
+gamedef_c::gamedef_c()
+{
+	Default();
+}
+
+//
+// gamedef_c Destructor
+//
+gamedef_c::~gamedef_c()
+{
+}
+
+//
+// gamedef_c::CopyDetail()
+//
+void gamedef_c::CopyDetail(gamedef_c &src)
+{
+	anims = src.anims;
+	mappos = src.mappos;
+		
+	background = src.background;
+	splatpic = src.splatpic;
+
+	yah[0] = src.yah[0];
+	yah[1] = src.yah[1];
+	
+	strcpy(bg_camera, src.bg_camera);
+	music = src.music;
+
+	percent = src.percent;
+	done = src.done;
+	endmap = src.endmap;
+	nextmap = src.nextmap;
+	accel_snd = src.accel_snd;
+	frag_snd = src.frag_snd;
+
+	firstmap = src.firstmap;
+	namegraphic = src.namegraphic;
+	
+	titlepics = src.titlepics;
+	
+	titlemusic = src.titlemusic;
+	titletics = src.titletics;
+	
+	special_music = src.special_music;
+}
+
+//
+// gamedef_c::Default()
+//
+void gamedef_c::Default()
+{
+	// FIXME: ddf.Clear() ?
+	ddf.name	= "";
+	ddf.number	= 0;	
+	ddf.crc		= 0;
+
+	anims.Clear();
+	mappos.Clear();
+		
+	background.Clear();
+	splatpic.Clear();
+
+	yah[0].Clear();
+	yah[1].Clear();
+	
+	bg_camera[0] = '\0';
+	music = 0;
+
+	percent = sfx_None;
+	done = sfx_None;
+	endmap = sfx_None;
+	nextmap = sfx_None;
+	accel_snd = sfx_None;
+	frag_snd = sfx_None;
+
+	firstmap.Clear();
+	namegraphic.Clear();
+	
+	titlepics.Clear();
+	
+	titlemusic = 0;
+	titletics = TICRATE * 4;
+	
+	special_music = 0;
+}
+
+// --> game definition container class
+
+//
+// gamedef_container_c Constructor
+//
+gamedef_container_c::gamedef_container_c() : epi::array_c(sizeof(gamedef_c*))
+{
+}
+
+//
+// gamedef_container_c Destructor
+//
+gamedef_container_c::~gamedef_container_c()
+{
+}
+
+//
+// gamedef_container_c::CleanupObject()
+//
+void gamedef_container_c::CleanupObject(void *obj)
+{
+	gamedef_c *g = *(gamedef_c**)obj;
+
+	if (g)
+		delete g;
+
+	return;
+}
+
+//
+// gamedef_c* gamedef_container_c::Lookup()
+//
+// Looks an gamedef by name, returns a fatal error if it does not exist.
+//
+gamedef_c* gamedef_container_c::Lookup(const char *refname)
+{
+	epi::array_iterator_c it;
+	gamedef_c *g;
+
+	if (!refname || !refname[0])
+		return NULL;
+
+	for (it = GetBaseIterator(); it.IsValid(); it++)
+	{
+		g = ITERATOR_TO_TYPE(it, gamedef_c*);
+		if (DDF_CompareName(g->ddf.name, refname) == 0)
+			return g;
+	}
+
 	return NULL;
 }
