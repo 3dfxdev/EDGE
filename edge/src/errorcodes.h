892ce630281133bad73162cd477ac2b023c0c8ed
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2004-08-18 12:19:33 +0000

    Implemented errorcodes

---
+++ edge/src/errorcodes.h
@@ -0,0 +1,31 @@
+//----------------------------------------------------------------------------
+//  Errorcodes
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2004  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+// -ACB- 2004/08/18 Written
+//
+#ifndef __ERRORCODES__
+#define __ERRORCODES__
+
+enum error_e
+{
+	ERR_GENERIC = 0x1,
+	ERR_MUSIC   = 0x2,
+	ERR_NUMTYPES
+};
+
+#endif
