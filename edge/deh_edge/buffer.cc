13f26affcf842a522e893b2ac3b3ba17cf26c206
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-09-14 00:13:39 +0000

    New file buffering code.

---
+++ edge/deh_edge/buffer.cc
@@ -0,0 +1,171 @@
+//------------------------------------------------------------------------
+//  BUFFER for Parsing
+//------------------------------------------------------------------------
+//
+//  DEH_EDGE  Copyright (C) 2004  The EDGE Team
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License (in COPYING.txt) for more details.
+//
+//------------------------------------------------------------------------
+//
+//  DEH_EDGE is based on:
+//
+//  +  DeHackEd source code, by Greg Lewis.
+//  -  DOOM source code (C) 1993-1996 id Software, Inc.
+//  -  Linux DOOM Hack Editor, by Sam Lantinga.
+//  -  PrBoom's DEH/BEX code, by Ty Halderman, TeamTNT.
+//
+//------------------------------------------------------------------------
+
+#include "i_defs.h"
+#include "buffer.h"
+
+#include "dh_embed.h"
+#include "system.h"
+
+namespace Deh_Edge
+{
+
+#define DEBUG_BUFFER  0
+
+class memory_buffer_c : public parse_buffer_api
+{
+private:
+	const char *data;
+	int length;
+
+	const char *ptr; // current pointer into data
+
+public:
+	memory_buffer_c(const char *_data, int _length) :
+		data(_data), length(_length), ptr(_data)
+	{
+		/* NOTE: data is not copied, but _is_ freed */
+	}
+
+	~memory_buffer_c()
+	{
+		delete[] data;
+		data = NULL;
+	}
+
+	bool eof()
+	{
+		return (ptr >= data + length);
+	}
+
+	bool error()
+	{
+		return false;
+	}
+	
+	int read(void *buf, int count)
+	{
+		int avail = data + length - ptr;
+
+		if (avail < count)
+			count = avail;
+
+		if (count <= 0)
+			return 0;
+
+		memcpy(buf, ptr, count);
+
+		ptr += count;
+
+		return count;
+	}
+
+	int getc()
+	{
+		if (eof())
+			return EOF;
+
+		return *ptr++;
+	}
+
+	void ungetc(int c)  /* NOTE: assumes c == last character read */
+	{
+		if (ptr > data)
+			ptr--;
+	}
+
+	void showProgress()
+	{
+		ProgressMinor(ptr - data, length + 1);
+	}
+};
+
+//------------------------------------------------------------------------
+
+namespace Buffer
+{
+
+parse_buffer_api *OpenFile(const char *filename)
+{
+	// Note: always binary - we'll handle CR/LF ourselves
+	FILE *fp = fopen(filename, "rb");
+
+	if (fp == NULL)
+	{
+		int err_num = errno;
+		FatalError("Could not open file: %s\n[%s]\n", filename, strerror(err_num));
+
+		return NULL; /* NOT REACHED */
+	}
+
+	int length;
+
+	// determine file size (by seeking to the end)
+	fseek(fp, 0, SEEK_END);
+	length = (int) ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	if (length < 0)
+	{
+		int err_num = errno;
+		FatalError("Couldn't get file size: %s\n[%s]\n", filename, strerror(err_num));
+
+		return NULL; /* NOT REACHED */
+	}
+
+	char *f_data = new char[length + 1];
+
+	fread(f_data, sizeof(char), length, fp);
+	f_data[length] = 0;
+
+	// close the file
+	fclose(fp);
+
+#if (DEBUG_BUFFER)
+	Debug_PrintMsg("Loaded file: data %p length %d begin: %02x,%02x,%02x,%02x\n",
+		f_data, length, f_data[0], f_data[1], f_data[2], f_data[3]);
+#endif
+
+	return new memory_buffer_c(f_data, length);
+}
+
+parse_buffer_api *OpenLump(const char *data, int length)
+{
+	if (length < 0)
+		FatalError("Illegal length of lump (%d bytes)\n", length);
+	
+	char *d_copy = new char[length + 1];
+
+	memcpy(d_copy, data, length);
+	d_copy[length] = 0;
+
+	return new memory_buffer_c(d_copy, length);
+}
+
+}  // Buffer
+
+}  // Deh_Edge
---
+++ edge/deh_edge/buffer.h
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------
+//  BUFFER for Parsing
+//------------------------------------------------------------------------
+// 
+//  DEH_EDGE  Copyright (C) 2004  The EDGE Team
+// 
+//  This program is under the GNU General Public License.
+//  It comes WITHOUT ANY WARRANTY of any kind.
+//  See COPYING.txt for the full details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __BUFFER_HDR__
+#define __BUFFER_HDR__
+
+namespace Deh_Edge
+{
+
+class parse_buffer_api
+{
+public:
+	parse_buffer_api()  { }
+	virtual ~parse_buffer_api() { }
+
+	virtual bool eof() = 0;
+	virtual bool error() = 0;
+	virtual int  read(void *buf, int count) = 0; 
+	virtual int  getc() = 0;
+	virtual void ungetc(int c) = 0;
+
+	virtual void showProgress() = 0;
+};
+
+namespace Buffer
+{
+	parse_buffer_api *OpenFile(const char *filename);
+	parse_buffer_api *OpenLump(const char *data, int length);
+}
+
+}  // Deh_Edge
+
+#endif /* __BUFFER_HDR__ */
