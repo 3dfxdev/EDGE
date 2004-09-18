ea19728beb79743af4eeb2c75522e56eac0cd90a
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-09-18 13:24:37 +0000

    Tidy up.

--- deh_edge/main.cc
+++ deh_edge/main.cc
@@ -381,6 +381,15 @@ void ValidateArgs(void)
 		output_file = StringDup(ReplaceExtension(output_file,
 			(target_version >= 128) ? "hwa" : "wad"));
 	}
+
+///---	for (j = 0; j < num_inputs; j++)
+///---	{
+///---		if (input_bufs[j]->is_lump)
+///---			continue;
+///---
+///---		if (StrCaseCmp(input_bufs[j]->infoname, output_file) == 0)
+///---			FatalError("Output filename is same as input filename.\n");
+///---	}
 }
 
 }  // Deh_Edge
