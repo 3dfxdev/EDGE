17280531c54d6539f1ccc9cf820456e556aae5de
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-11-14 08:29:05 +0000

    Initial check-in of parsing code for Styles.ddf.

---
+++ edge/src/ddf_style.h
@@ -0,0 +1,150 @@
+//----------------------------------------------------------------------------
+//  EDGE Data Definition File Code (Styles)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2004  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __DDF_STYLE__
+#define __DDF_STYLE__
+
+#include "dm_defs.h"
+
+#include "./epi/epiutil.h"
+
+//
+// -AJA- 2004/11/14 Styles.ddf
+//
+typedef enum
+{
+	BKGSP_Tiled = 0x0001   // image should tile (otherwise covers whole area)
+}
+background_special_e;
+
+class backgroundstyle_c
+{
+public:
+	backgroundstyle_c();
+	backgroundstyle_c(const backgroundstyle_c& rhs);
+	~backgroundstyle_c();
+
+	void Default();
+	backgroundstyle_c& operator= (const backgroundstyle_c& rhs);
+
+	rgbcol_t colour;
+	percent_t translucency;
+
+	epi::strent_c image_name;
+	const struct image_s *cache_image;
+	
+	background_special_e special;
+
+	float scale;
+	float aspect;
+};
+
+class textstyle_c
+{
+public:
+	textstyle_c();
+	textstyle_c(const textstyle_c& rhs);
+	~textstyle_c();
+
+	void Default();
+	textstyle_c& operator= (const textstyle_c& rhs);
+
+	const colourmap_c *colmap;  // FIXME: have a combined colmap/rgbcol
+	rgbcol_t colour;
+
+	percent_t translucency;
+
+	fontdef_c *font;
+
+	float scale;
+	float aspect;
+
+	// FIXME: horizontal and vertical alignment
+};
+
+class soundstyle_c
+{
+public:
+	soundstyle_c();
+	soundstyle_c(const soundstyle_c& rhs);
+	~soundstyle_c();
+
+	void Default();
+	soundstyle_c& operator= (const soundstyle_c& rhs);
+
+	sfx_t *begin;
+	sfx_t *end;
+	sfx_t *select;
+	sfx_t *back;
+	sfx_t *error;
+	sfx_t *move;
+	sfx_t *slider;
+
+};
+
+class styledef_c
+{
+public:
+	styledef_c();
+	styledef_c(const styledef_c &rhs);
+	~styledef_c();
+
+private:
+	void Copy(const styledef_c &src);
+
+public:
+	void CopyDetail(const styledef_c &src);
+	void Default(void);
+	styledef_c& operator= (const styledef_c &rhs);
+
+	// Member vars....
+	ddf_base_c ddf;
+
+	backgroundstyle_c bg;
+
+	textstyle_c title; // title style
+	textstyle_c text;  // main text style
+	textstyle_c alt;   // alternative text style
+
+	soundstyle_c sounds;
+};
+
+// Our styledefs container
+class styledef_container_c : public epi::array_c 
+{
+public:
+	styledef_container_c() : epi::array_c(sizeof(styledef_c*)) {}
+	~styledef_container_c() { Clear(); } 
+
+private:
+	void CleanupObject(void *obj);
+
+public:
+	int GetSize() {	return array_entries; } 
+	int Insert(styledef_c *a) { return InsertObject((void*)&a); }
+	styledef_c* operator[](int idx) { return *(styledef_c**)FetchObject(idx); } 
+
+	// Search Functions
+	styledef_c* Lookup(const char* refname);
+};
+
+extern styledef_container_c styledefs;
+
+bool DDF_ReadStyles(void *data, int size);
+
+#endif  /* __DDF_STYLE__ */
