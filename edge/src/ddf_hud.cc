57b9ccb8ed974de60c09ef5de375f4f8c3fc70d5
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-11-25 03:49:04 +0000

    Check-in of empty shell for HUD.DDF parsing code.

---
+++ edge/src/ddf_hud.cc
@@ -0,0 +1,292 @@
+//----------------------------------------------------------------------------
+//  EDGE Data Definition File Code (HUD)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2004  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+// HUD Setup and Parser Code
+//
+
+#include "i_defs.h"
+
+#include "ddf_locl.h"
+#include "ddf_main.h"
+#include "ddf_hud.h"
+
+#undef  DF
+#define DF  DDF_CMD
+
+static huddef_c buffer_hud;
+static huddef_c *dynamic_hud;
+
+static void DDF_HudGetUsage( const char *info, void *storage);
+
+#define DDF_CMD_BASE  buffer_hud
+
+static const commandlist_t hud_commands[] =
+{
+	DF("USAGE",    usage,     DDF_HudGetUsage),
+
+	DDF_CMD_END
+};
+
+// -ACB- 2004/06/03 Replaced array and size with purpose-built class
+huddef_container_c huddefs;
+
+//
+//  DDF PARSE ROUTINES
+//
+static bool HudStartEntry(const char *name)
+{
+L_WriteDebug("Start hud [%s]\n", name);
+	bool replaces = false;
+
+	if (name && name[0])
+	{
+		epi::array_iterator_c it;
+		huddef_c *a;
+
+		for (it = huddefs.GetBaseIterator(); it.IsValid(); it++)
+		{
+			a = ITERATOR_TO_TYPE(it, huddef_c*);
+			if (DDF_CompareName(a->ddf.name.GetString(), name) == 0)
+			{
+				dynamic_hud = a;
+				replaces = true;
+				break;
+			}
+		}
+	}
+
+	// not found, create a new one
+	if (! replaces)
+	{
+		dynamic_hud = new huddef_c;
+
+		if (name && name[0])
+			dynamic_hud->ddf.name.Set(name);
+		else
+			dynamic_hud->ddf.SetUniqueName("UNNAMED_HUD", huddefs.GetSize());
+
+		huddefs.Insert(dynamic_hud);
+	}
+
+	dynamic_hud->ddf.number = 0;
+
+	// instantiate the static entry
+	buffer_hud.Default();
+	return replaces;
+}
+
+static void HudParseField(const char *field, const char *contents, int index, bool is_last)
+{
+#if (DEBUG_DDF)  
+	L_WriteDebug("HUD_PARSE: %s = %s;\n", field, contents);
+#endif
+
+	if (! DDF_MainParseField(hud_commands, field, contents))
+		DDF_Error("Unknown hud.ddf command: %s\n", field);
+}
+
+static void HudFinishEntry(void)
+{
+	// transfer static entry to dynamic entry
+	dynamic_hud->CopyDetail(buffer_hud);
+
+	// Compute CRC.  In this case, there is no need, since huds
+	// have zero impact on the game simulation.
+	dynamic_hud->ddf.crc.Reset();
+}
+
+static void HudClearAll(void)
+{
+	// safe to just delete all huds
+	huddefs.Clear();
+}
+
+
+bool DDF_ReadHuds(void *data, int size)
+{
+	readinfo_t huds;
+
+	huds.memfile = (char*)data;
+	huds.memsize = size;
+	huds.tag = "HUDS";
+	huds.entries_per_dot = 2;
+
+	if (huds.memfile)
+	{
+		huds.message  = NULL;
+		huds.filename = NULL;
+		huds.lumpname = "DDFHUD";
+	}
+	else
+	{
+		huds.message  = "DDF_InitHuds";
+		huds.filename = "hud.ddf";
+		huds.lumpname = NULL;
+	}
+
+	huds.start_entry  = HudStartEntry;
+	huds.parse_field  = HudParseField;
+	huds.finish_entry = HudFinishEntry;
+	huds.clear_all    = HudClearAll;
+
+	return DDF_MainReadFile(&huds);
+}
+
+//
+// DDF_HudInit
+//
+void DDF_HudInit(void)
+{
+	huddefs.Clear();		// <-- Consistent with existing behaviour (-ACB- 2004/05/04)
+}
+
+//
+// DDF_HudCleanUp
+//
+void DDF_HudCleanUp(void)
+{
+	if (huddefs.GetSize() == 0)
+		I_Error("There are no huds defined in DDF !\n");
+
+	huddefs.Trim();		// <-- Reduce to allocated size
+}
+
+//
+// DDF_HudGetType
+//
+static void DDF_HudGetUsage(const char *info, void *storage)
+{
+	DEV_ASSERT2(storage);
+
+	int *usage = (int *) storage;
+
+	if (DDF_CompareName(info, "PLAYER") == 0)
+		(*usage) = huddef_c::U_Player;
+	else if (DDF_CompareName(info, "AUTOMAP") == 0)
+		(*usage) = huddef_c::U_Automap;
+	else if (DDF_CompareName(info, "RTS") == 0)
+		(*usage) = huddef_c::U_RTS;
+	else
+		DDF_Error("Unknown hud usage: %s\n", info);
+}
+
+
+// ---> huddef_c class
+
+//
+// huddef_c constructor
+//
+huddef_c::huddef_c()
+{
+	Default();
+}
+
+//
+// huddef_c Copy constructor
+//
+huddef_c::huddef_c(const huddef_c &rhs)
+{
+	Copy(rhs);
+}
+
+//
+// huddef_c::Copy()
+//
+void huddef_c::Copy(const huddef_c &src)
+{
+	ddf = src.ddf;
+	CopyDetail(src);
+}
+
+//
+// huddef_c::CopyDetail()
+//
+// Copies all the detail with the exception of ddf info
+//
+void huddef_c::CopyDetail(const huddef_c &src)
+{
+	usage = src.usage;
+	elements = src.elements;  // FIXME: copy list
+}
+
+//
+// huddef_c::Default()
+//
+void huddef_c::Default()
+{
+	ddf.Default();
+
+	usage = 0;
+	elements = NULL;
+}
+
+//
+// huddef_c assignment operator
+//
+huddef_c& huddef_c::operator= (const huddef_c &rhs)
+{
+	if (&rhs != this)
+		Copy(rhs);
+	
+	return *this;
+}
+
+// ---> huddef_container_c class
+
+//
+// huddef_container_c::CleanupObject()
+//
+void huddef_container_c::CleanupObject(void *obj)
+{
+	huddef_c *a = *(huddef_c**)obj;
+
+	if (a) delete a;
+}
+
+//
+// huddef_container_c::Lookup()
+//
+huddef_c* huddef_container_c::Lookup(const char *refname)
+{
+	if (!refname || !refname[0])
+		return NULL;
+	
+	for (epi::array_iterator_c it = GetIterator(0); it.IsValid(); it++)
+	{
+		huddef_c *f = ITERATOR_TO_TYPE(it, huddef_c*);
+		if (DDF_CompareName(f->ddf.name.GetString(), refname) == 0)
+			return f;
+	}
+
+	return NULL;
+}
+
+//
+// DDF_MainLookupHUD
+//
+void DDF_MainLookupHUD(const char *info, void *storage)
+{
+	huddef_c **dest = (huddef_c **)storage;
+
+	*dest = huddefs.Lookup(info);
+
+	// FIXME: throw epi::error_c
+	if (*dest == NULL)
+		DDF_Error("Unknown hud: %s\n", info);
+}
+
---
+++ edge/src/ddf_hud.h
@@ -0,0 +1,99 @@
+//----------------------------------------------------------------------------
+//  EDGE Data Definition File Code (HUD)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2004  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __DDF_HUD__
+#define __DDF_HUD__
+
+#include "dm_defs.h"
+
+#include "./epi/epiutil.h"
+
+//
+// -AJA- 2004/11/25 HUD.ddf
+//
+class hud_element_c
+{
+public:
+	hud_element_c();
+	~hud_element_c();
+
+	hud_element_c *next;  // link in list
+
+	// FIXME !!!!
+};
+
+class huddef_c
+{
+public:
+	huddef_c();
+	huddef_c(const huddef_c &rhs);
+	~huddef_c() {};
+
+private:
+	void Copy(const huddef_c &src);
+
+public:
+	void CopyDetail(const huddef_c &src);
+	void Default(void);
+	huddef_c& operator= (const huddef_c &rhs);
+
+	// Member vars....
+	ddf_base_c ddf;
+
+	enum  // usage values
+	{
+		U_Player = 0,
+		U_Automap,
+		U_RTS
+	};
+
+	int usage;
+
+	int left_size;
+	int right_size;
+	int top_size;
+	int bottom_size;
+
+	hud_element_c *elements;
+};
+
+// Our huddefs container
+class huddef_container_c : public epi::array_c 
+{
+public:
+	huddef_container_c() : epi::array_c(sizeof(huddef_c*)) {}
+	~huddef_container_c() { Clear(); } 
+
+private:
+	void CleanupObject(void *obj);
+
+public:
+	int GetSize() {	return array_entries; } 
+	int Insert(huddef_c *a) { return InsertObject((void*)&a); }
+	huddef_c* operator[](int idx) { return *(huddef_c**)FetchObject(idx); } 
+
+	// Search Functions
+	huddef_c* Lookup(const char* refname);
+};
+
+extern huddef_container_c huddefs;
+
+void DDF_MainLookupHUD(const char *info, void *storage);
+bool DDF_ReadHuds(void *data, int size);
+
+#endif  /* __DDF_HUD__ */
