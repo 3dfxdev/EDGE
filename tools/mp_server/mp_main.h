9d9e53ab7e75afa98ac8e184d92e7a1ee614e061
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-12-03 07:39:12 +0000

    Bump version.

--- tools/mp_server/mp_main.h
+++ tools/mp_server/mp_main.h
@@ -19,7 +19,7 @@
 #ifndef __MP_MAIN_H__
 #define __MP_MAIN_H__
 
-#define MPSERVER_VERSION  "0.40"
+#define MPSERVER_VERSION  "0.41"
 
 #define MPS_DEF_MAX_CLIENT  16
 #define MPS_DEF_ALIVE_WAIT  60 /* seconds */
