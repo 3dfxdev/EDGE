bbe65253c9ca19b5f186e6412a0476a867c48b92
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2004-12-04 13:08:13 +0000

    Check-in of initial work on new networking system.

---
+++ edge/src/n_packet.cc
@@ -0,0 +1,187 @@
+//----------------------------------------------------------------------------
+//  EDGE Packet handling
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 2004  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+#include "nl.h"
+
+#include "epi/epitype.h"
+#include "epi/epiendian.h"
+
+#include "n_packet.h"
+
+//
+// N_GetErrorStr
+//
+const char *N_GetErrorStr(void)
+{
+	int err = nlGetError();
+
+	if (err == NL_NO_ERROR)
+		return "(Unknown Problem)";
+
+	static char err_buf[512];
+
+	if (err == NL_SYSTEM_ERROR)
+	{
+		err = nlGetSystemError();
+		sprintf(err_buf, "%s", nlGetSystemErrorStr(err));
+	}
+	else
+	{
+		sprintf(err_buf, "NL: %s", nlGetErrorStr(err));
+	}
+
+	// remove newlines and other rubbish
+	for (int i = 0; err_buf[i] && (unsigned)i < sizeof(err_buf); i++)
+	{
+		if (! isprint(err_buf[i]))
+			err_buf[i] = ' ';
+	}
+
+	return err_buf;
+}
+
+//
+// N_GetAddrName
+//
+const char *N_GetAddrName(const NLaddress *addr)
+{
+	static char name_buf[NL_MAX_STRING_LENGTH];
+
+	return nlAddrToString(addr, name_buf);
+}
+
+//----------------------------------------------------------------------------
+
+packet_c::packet_c()
+{
+	Clear();
+}
+
+packet_c::~packet_c()
+{
+	/* nothing needed */
+}
+
+void packet_c::Clear()
+{
+	memset(raw, 0, sizeof(raw));
+}
+
+void packet_c::ClearData()
+{
+	memset(raw + HEADER_LEN, 0, sizeof(raw) - HEADER_LEN);
+}
+
+
+bool packet_c::Read(NLsocket sock)
+{
+	int len = nlRead(sock, raw, MAX_PACKET + READ_GAP);
+
+	if (len == 0)  // no packet
+		return false;
+
+	if (len < 0)
+	{
+		L_WriteDebug("PacketRead failed: %s\n", N_GetErrorStr());
+		return false;
+	}
+
+	if (len < HEADER_LEN)
+	{
+		L_WriteDebug("PacketRead: short packet received (%d bytes)\n", len);
+		return false;
+	}
+
+	if (len > MAX_PACKET)
+	{
+		L_WriteDebug("PacketRead: long packet received (%d bytes)\n", len);
+		return false;
+	}
+
+	if (hd().type[0] == 0 || hd().type[1] == 0)
+	{
+		L_WriteDebug("PacketRead: illegal packet type found (0x%2x%2x)\n",
+			hd().type[0], hd().type[1]);
+		return false;
+	}
+	
+	hd().ByteSwap();
+
+	if (hd().data_len < 0 || ! CanHold(hd().data_len))
+	{
+		L_WriteDebug("PacketRead: illegal data length found (%d)\n", hd().data_len);
+		return false;
+	}
+
+	if (hd().data_len != len - HEADER_LEN)
+	{
+		L_WriteDebug("PacketRead: data length does not match packet (%d != %d)\n",
+			hd().data_len, len - HEADER_LEN);
+		return false;
+	}
+	
+	return true;
+}
+
+bool packet_c::Write(NLsocket sock)
+{
+	DEV_ASSERT2(hd().type[0] != 0 && hd().type[1] != 0);
+	DEV_ASSERT2(hd().data_len >= 0 && CanHold(hd().data_len));
+
+	int len = HEADER_LEN + hd().data_len;
+
+	hd().ByteSwap();
+
+	int wrote = nlWrite(sock, raw, len); 
+
+	if (wrote == 0)
+	{
+		// FIXME: try to write later ???
+		L_WriteDebug("PacketWrite failed -- no space in buffers.\n");
+		return false;
+	}
+	else if (wrote < 0)
+	{
+		L_WriteDebug("PacketWrite failed: %s\n", N_GetErrorStr());
+		return false;
+	}
+	else if (wrote != len)
+	{
+		L_WriteDebug("PacketWrite: wrote less data (%d < %d)\n", wrote, len);
+		return false;
+	}
+
+	return true;
+}
+
+void packet_c::SetType(const char *str)
+{
+	DEV_ASSERT2(strlen(str) == 2);
+
+	hd().type[0] = str[0];
+	hd().type[1] = str[1];
+}
+
+bool packet_c::CheckType(const char *str)
+{
+	DEV_ASSERT2(strlen(str) == 2);
+
+	return (hd().type[0] == str[0] && hd().type[1] == str[1]);
+}
+
---
+++ edge/src/protocol.cc
@@ -0,0 +1,115 @@
+//------------------------------------------------------------------------
+//  Protocol structures
+//------------------------------------------------------------------------
+//
+//  Edge MultiPlayer Server (C) 2004  Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifdef EDGEVER
+#include "epi/epitype.h"
+#include "epi/epiendian.h"
+#define SYS_BE_S16  EPI_BE_S16
+#else
+#include "includes.h"
+#endif
+
+#include "protocol.h"
+
+void header_proto_t::ByteSwap()
+{
+	flags      = SYS_BE_S16(flags);
+	request_id = SYS_BE_S16(request_id);
+	data_len   = SYS_BE_S16(data_len);
+
+	source = SYS_BE_S16(source);
+	dest   = SYS_BE_S16(dest);
+	game   = SYS_BE_S16(game);
+}
+
+void error_proto_t::ByteSwap()
+{
+	type     = SYS_BE_S16(type);
+	param[0] = SYS_BE_S16(param[0]);
+	param[1] = SYS_BE_S16(param[1]);
+}
+
+void client_info_t::ByteSwap()
+{
+	game_id = SYS_BE_S16(game_id);
+}
+
+void connect_proto_t::ByteSwap()
+{
+	info.ByteSwap();
+}
+
+void query_client_proto_t::ByteSwap(bool do_info)
+{
+	first_id = SYS_BE_S16(first_id);
+
+	if (do_info)  // FIXME: swap structures manually higher up
+	{
+		for (int c = 0; c < count; c++)
+			info[c].ByteSwap();
+	}
+}
+
+void game_info_t::ByteSwap()
+{
+	min_players = SYS_BE_S16(min_players);
+	engine_ver  = SYS_BE_S16(engine_ver);
+
+	num_players = SYS_BE_S16(num_players);
+	num_votes   = SYS_BE_S16(num_votes);
+}
+
+void new_game_proto_t::ByteSwap()
+{
+	info.ByteSwap();
+}
+
+void query_game_proto_t::ByteSwap(bool do_info)
+{
+	first_id = SYS_BE_S16(first_id);
+
+	if (do_info)
+	{
+		for (int c = 0; c < count; c++)
+			info[c].ByteSwap();
+	}
+}
+
+void play_game_proto_t::ByteSwap()
+{
+	total_players = SYS_BE_S16(total_players);
+	first_player  = SYS_BE_S16(first_player);
+
+	for (int p = 0; p < count; p++)
+		client_id[p] = SYS_BE_S16(client_id[p]);
+}
+
+void ticcmd_proto_t::ByteSwap()
+{
+	/// tic_counter = SYS_BE_S16(tic_counter);
+
+	/* TICCMDs are the engine's responsibility */
+}
+
+void tic_group_proto_t::ByteSwap()
+{
+	/// tic_counter  = SYS_BE_S16(tic_counter);
+	first_player = SYS_BE_S16(first_player);
+
+	/* TICCMDs are the engine's responsibility */
+}
