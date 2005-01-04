86bd689c076f4fa3bf9f5d6be4441cb5922cbc9e
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2005-01-04 12:21:24 +0000

    New special "CROSSHAIR" for images.ddf (for weapon sprites). Fix for NULL pointer dereference in W_AnimateImageSet().

--- edge/src/ddf_image.cc
+++ edge/src/ddf_image.cc
@@ -240,10 +240,11 @@ static void DDF_ImageGetType(const char *info, void *storage)
 
 static specflags_t image_specials[] =
 {
-    {"ALPHA",  IMGSP_NoAlpha, 1},
-    {"MIP",    IMGSP_NoMip,   1},
-    {"CLAMP",  IMGSP_Clamp,   0},
-    {"SMOOTH", IMGSP_Smooth,  0},
+    {"CROSSHAIR", IMGSP_Crosshair, 0},
+    {"ALPHA",     IMGSP_NoAlpha,   1},
+    {"MIP",       IMGSP_NoMip,     1},
+    {"CLAMP",     IMGSP_Clamp,     0},
+    {"SMOOTH",    IMGSP_Smooth,    0},
     {NULL, 0, 0}
 };
 
