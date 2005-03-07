4e1176739c25186348374275b3bd44a79960804b
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2005-03-07 11:59:11 +0000

    Copyright header update

--- linux/i_sysinc.h
+++ linux/i_sysinc.h
@@ -2,7 +2,7 @@
 //  EDGE LINUX System Specific header
 //----------------------------------------------------------------------------
 //
-//  Copyright (c) 1999-2004  The EDGE Team.
+//  Copyright (c) 1999-2005  The EDGE Team.
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
--- src/ddf_colm.h
+++ src/ddf_colm.h
@@ -2,7 +2,7 @@
 //  EDGE Data Definition File Code (Colourmaps)
 //----------------------------------------------------------------------------
 // 
-//  Copyright (c) 1999-2004  The EDGE Team.
+//  Copyright (c) 1999-2005  The EDGE Team.
 // 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
