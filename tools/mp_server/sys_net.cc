254068f5ec442d8e7dcd780ec77379df7bcbb8d4
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2005-03-13 12:04:13 +0000

    Check-in of platform-specific network helpers, currently including some Linux code to determine IP address of local host on a LAN.

---
+++ tools/mp_server/sys_net.cc
@@ -0,0 +1,85 @@
+//----------------------------------------------------------------------------
+//  Platform-specific NET code
+//----------------------------------------------------------------------------
+//
+//  Edge MultiPlayer Server (C) 2005  Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#ifdef LINUX
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/param.h>
+#include <sys/ioctl.h>
+
+#include <arpa/inet.h>
+#include <linux/netdevice.h> 
+
+// possible: <linux/if.h>
+//           <linux/sockios.h>
+
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+
+#endif  // LINUX
+
+
+#ifdef LINUX
+
+//
+// Determine IP address of local host.  Result will be in
+// dotted notation (like 11.22.33.44) or NULL if something
+// went wrong (invalid interface).
+//
+const char * LocalIPAddrString(const char *iface_name)
+{
+	int fd;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (fd < 0)
+	{
+		// perror("socket");
+		return NULL;
+	}
+
+	struct ifreq ifr;
+
+	strcpy(ifr.ifr_name, iface_name);
+
+	if (ioctl(fd, SIOCGIFADDR, &ifr) < 0)
+	{
+		close(fd);
+		return NULL;
+	}
+
+	close(fd);
+
+	struct sockaddr_in *skt = (struct sockaddr_in*) &ifr.ifr_addr;
+
+	if (! skt->sin_addr.s_addr)
+		return NULL;
+
+	static char buffer[1024];
+
+	strcpy(buffer, inet_ntoa(skt->sin_addr));
+	return buffer;
+}
+
+#endif  // LINUX
+
