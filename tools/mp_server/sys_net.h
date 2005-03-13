915bba9f6169230be46fce2e1abdea619aa3e712
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2005-03-13 12:04:13 +0000

    Check-in of platform-specific network helpers, currently including some Linux code to determine IP address of local host on a LAN.

---
+++ tools/mp_server/sys_net.h
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------
+//  Platform-specific NET code
+//----------------------------------------------------------------------------
+//
+//  Edge MultiPlayer Server (C) 2005  Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __SYS_NET__
+#define __SYS_NET__
+
+// LINUX
+const char * LocalIPAddrString(const char *iface_name)
+
+#endif  /* __SYS_NET__ */
