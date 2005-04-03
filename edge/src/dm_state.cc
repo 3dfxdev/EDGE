47cb007c16c94165879436812823fa97e2231493
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2005-04-03 12:20:25 +0000

    Removed unused (for a long time) stuff.

--- edge/src/dm_state.cc
+++ edge/src/dm_state.cc
@@ -23,16 +23,3 @@
 //
 //----------------------------------------------------------------------------
 
-#include "i_defs.h"
-#include "dm_state.h"
-
-/*
-   // Game Mode - identify IWAD as shareware, retail etc.
-   GameMode_t gamemode = indetermined;
-   GameMission_t gamemission = doom;
-
-   // Language.
-   Language_t   language = english;
- */
-// Set if homebrew PWAD stuff has been added.
-bool modifiedgame;
