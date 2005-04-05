4f51ea46e9ee9114a6c115063532601393f6f095
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2005-04-05 18:38:19 +0000

    Tidying

--- tools/ddfinwad/wad_io.c
+++ tools/ddfinwad/wad_io.c
@@ -30,9 +30,9 @@
 //
 typedef struct
 {
-  char id[4];        // IWAD (whole) or PWAD (part)
-  int numlumps;      // Number of Lumps
-  int infotableofs;  // Info table offset
+    char id[4];        // IWAD (whole) or PWAD (part)
+    int numlumps;      // Number of Lumps
+    int infotableofs;  // Info table offset
 }
 wadinfo_t;
 
@@ -41,9 +41,9 @@ wadinfo_t;
 //
 typedef struct
 {
-  int filepos;       // Position in file
-  int size;          // Size
-  char name[8];      // Name
+    int filepos;       // Position in file
+    int size;          // Size
+    char name[8];      // Name
 }
 lumpinfo_t;
 
@@ -52,10 +52,10 @@ lumpinfo_t;
 //
 typedef struct lump_s
 {
-  byte* data;          // Data
-  int filepos;         // Position in file
-  int size;            // Size
-  char name[8];        // Name
+    byte* data;          // Data
+    int filepos;         // Position in file
+    int size;            // Size
+    char name[8];        // Name
 }
 lump_t;
 
@@ -74,19 +74,19 @@ static int numoflumps = 0;
 //
 static void PadFile(FILE* fp, int filepos)
 {
-  int num;
+    int num;
 
-  num = filepos % 4;
+    num = filepos % 4;
 
-  if (!num)
-    return;
+    if (!num)
+        return;
 
-  num = 4 - num;
-  while(num)
-  {
-    fwrite(&num, sizeof(byte), 1, fp);
-    num--;
-  }
+    num = 4 - num;
+    while(num)
+    {
+        fwrite(&num, sizeof(byte), 1, fp);
+        num--;
+    }
 }
 
 //
@@ -94,41 +94,43 @@ static void PadFile(FILE* fp, int filepos)
 //
 static boolean_t AddLumpinfo(lumpinfo_t *lpinf)
 {
-  lumpinfo_t **oldlpinflist;
+    lumpinfo_t **oldlpinflist;
 
-  if (numoflumpinfs && lumpinfolist)
-  {
-    oldlpinflist = lumpinfolist;
+    if (numoflumpinfs && lumpinfolist)
+    {
+        oldlpinflist = lumpinfolist;
+        
+        lumpinfolist = malloc(sizeof(lumpinfo_t*)*(numoflumpinfs+1));
+        if (!lumpinfolist)
+            return false;
 
-    lumpinfolist = malloc(sizeof(lumpinfo_t*)*(numoflumpinfs+1));
-    if (!lumpinfolist)
-      return false;
+        memset(lumpinfolist, 0, sizeof(lumpinfo_t*)*(numoflumpinfs+1));
 
-    memset(lumpinfolist, 0, sizeof(lumpinfo_t*)*(numoflumpinfs+1));
-    memcpy(lumpinfolist, oldlpinflist, sizeof(lumpinfo_t*)*(numoflumpinfs));
+        memcpy(lumpinfolist, oldlpinflist, 
+               sizeof(lumpinfo_t*)*(numoflumpinfs));
 
-    free(oldlpinflist);
-  }
-  else
-  {
-    lumpinfolist = malloc(sizeof(lumpinfo_t*));
+        free(oldlpinflist);
+    }
+    else
+    {
+        lumpinfolist = malloc(sizeof(lumpinfo_t*));
 
-    if (!lumpinfolist)
-      return false;
+        if (!lumpinfolist)
+            return false;
 
-    numoflumpinfs = 0;
-  }
+        numoflumpinfs = 0;
+    }
 
-  lumpinfolist[numoflumpinfs] = malloc(sizeof(lumpinfo_t));
-  if (!lumpinfolist[numoflumpinfs])
-    return false;
+    lumpinfolist[numoflumpinfs] = malloc(sizeof(lumpinfo_t));
+    if (!lumpinfolist[numoflumpinfs])
+        return false;
 
-  lumpinfolist[numoflumpinfs]->filepos = lpinf->filepos;
-  lumpinfolist[numoflumpinfs]->size    = lpinf->size;
-  strncpy(lumpinfolist[numoflumpinfs]->name, lpinf->name, 8);
+    lumpinfolist[numoflumpinfs]->filepos = lpinf->filepos;
+    lumpinfolist[numoflumpinfs]->size    = lpinf->size;
+    strncpy(lumpinfolist[numoflumpinfs]->name, lpinf->name, 8);
 
-  numoflumpinfs++;
-  return true;
+    numoflumpinfs++;
+    return true;
 }
 
 //
@@ -136,53 +138,53 @@ static boolean_t AddLumpinfo(lumpinfo_t *lpinf)
 //
 boolean_t WAD_AddLump(byte *data, int size, char *name)
 {
-  lump_t **oldlumplist;
-  int i;
+    lump_t **oldlumplist;
+    int i;
 
-  // Check for existing lump, overwrite if need be.
-  i=WAD_LumpExists(name);
-  if (i>=0)
-  {
-    free(lumplist[i]->data);
-    lumplist[i]->size = size;
-    lumplist[i]->data = data;
+    // Check for existing lump, overwrite if need be.
+    i=WAD_LumpExists(name);
+    if (i>=0)
+    {
+        free(lumplist[i]->data);
+        lumplist[i]->size = size;
+        lumplist[i]->data = data;
+        return true;
+    }
+
+    if (numoflumps && lumplist)
+    {
+        oldlumplist = lumplist;
+        
+        lumplist = malloc(sizeof(lump_t*)*(numoflumps+1));
+        if (!lumplist)
+            return false;
+
+        memset(lumplist, 0, sizeof(lump_t*)*(numoflumps+1));
+        memcpy(lumplist, oldlumplist, sizeof(lump_t*)*(numoflumps));
+
+        free(oldlumplist);
+    }
+    else
+    {
+        lumplist = malloc(sizeof(lump_t*));
+
+        if (!lumplist)
+            return false;
+
+        numoflumps = 0;
+    }
+
+    lumplist[numoflumps] = malloc(sizeof(lump_t));
+    if (!lumplist[numoflumps])
+        return false;
+
+    lumplist[numoflumps]->data = data;
+    lumplist[numoflumps]->size = size;
+    strncpy(lumplist[numoflumps]->name, name, 8);
+
+    numoflumps++;
+
     return true;
-  }
-
-  if (numoflumps && lumplist)
-  {
-    oldlumplist = lumplist;
-
-    lumplist = malloc(sizeof(lump_t*)*(numoflumps+1));
-    if (!lumplist)
-      return false;
-
-    memset(lumplist, 0, sizeof(lump_t*)*(numoflumps+1));
-    memcpy(lumplist, oldlumplist, sizeof(lump_t*)*(numoflumps));
-
-    free(oldlumplist);
-  }
-  else
-  {
-    lumplist = malloc(sizeof(lump_t*));
-
-    if (!lumplist)
-      return false;
-
-    numoflumps = 0;
-  }
-
-  lumplist[numoflumps] = malloc(sizeof(lump_t));
-  if (!lumplist[numoflumps])
-    return false;
-
-  lumplist[numoflumps]->data = data;
-  lumplist[numoflumps]->size = size;
-  strncpy(lumplist[numoflumps]->name, name, 8);
-
-  numoflumps++;
-
-  return true;
 }
 
 //
@@ -190,18 +192,18 @@ boolean_t WAD_AddLump(byte *data, int size, char *name)
 //
 int WAD_LumpExists(char *name)
 {
-  int i;
+    int i;
 
-  i=0;
-  while (i<numoflumps)
-  {
-    if (!strncmp(lumplist[i]->name, name, 8))
-      return i;
+    i=0;
+    while (i<numoflumps)
+    {
+        if (!strncmp(lumplist[i]->name, name, 8))
+            return i;
 
-    i++;
-  }
+        i++;
+    }
 
-  return -1;
+    return -1;
 }
 
 //
@@ -209,55 +211,55 @@ int WAD_LumpExists(char *name)
 //
 boolean_t WAD_WriteFile(char *name)
 {
-  int i;
-  FILE *fp;
-  char *pwadstr = PWAD_HEADER;
-  int infooffset;
-  int fpos;
+    int i;
+    FILE *fp;
+    char *pwadstr = PWAD_HEADER;
+    int infooffset;
+    int fpos;
 
-  // Open File
-  fp = fopen(name, "wb");
-  if (!fp)
-    return false;
+    // Open File
+    fp = fopen(name, "wb");
+    if (!fp)
+        return false;
 
-  // Write Header
-  fwrite((void*)pwadstr, sizeof(char), 4, fp);
-  fwrite((void*)&numoflumps, sizeof(int), 1, fp);
-  fwrite((void*)&numoflumps, sizeof(int), 1, fp); // dummy - write later
+    // Write Header
+    fwrite((void*)pwadstr, sizeof(char), 4, fp);
+    fwrite((void*)&numoflumps, sizeof(int), 1, fp);
+    fwrite((void*)&numoflumps, sizeof(int), 1, fp); // dummy - write later
 
-  // Write Lumps
-  i=0;
-  while(i<numoflumps)
-  {
+    // Write Lumps
+    i=0;
+    while(i<numoflumps)
+    {
+        fpos = ftell(fp);
+        PadFile(fp, fpos);
+        lumplist[i]->filepos = ftell(fp);
+        fwrite((void*)lumplist[i]->data, sizeof(byte), lumplist[i]->size, fp);
+        i++;
+    }
+
     fpos = ftell(fp);
     PadFile(fp, fpos);
-    lumplist[i]->filepos = ftell(fp);
-    fwrite((void*)lumplist[i]->data, sizeof(byte), lumplist[i]->size, fp);
-    i++;
-  }
+    infooffset = ftell(fp);
 
-  fpos = ftell(fp);
-  PadFile(fp, fpos);
-  infooffset = ftell(fp);
+    // Write Lumpinfo(s)
+    i=0;
+    while(i<numoflumps)
+    {
+        fwrite((void*)&lumplist[i]->filepos, sizeof(int), 1, fp);
+        fwrite((void*)&lumplist[i]->size, sizeof(int), 1, fp);
+        fwrite((void*)lumplist[i]->name, sizeof(char), 8, fp);
+        i++;
+    }
 
-  // Write Lumpinfo(s)
-  i=0;
-  while(i<numoflumps)
-  {
-    fwrite((void*)&lumplist[i]->filepos, sizeof(int), 1, fp);
-    fwrite((void*)&lumplist[i]->size, sizeof(int), 1, fp);
-    fwrite((void*)lumplist[i]->name, sizeof(char), 8, fp);
-    i++;
-  }
+    // Write table offset
+    fseek(fp, 8, SEEK_SET);
+    fwrite((void*)&infooffset, sizeof(int), 1, fp); // Now write table offset
 
-  // Write table offset
-  fseek(fp, 8, SEEK_SET);
-  fwrite((void*)&infooffset, sizeof(int), 1, fp); // Now write table offset
+    // Close File
+    fclose(fp);
 
-  // Close File
-  fclose(fp);
-
-  return true;
+    return true;
 }
 
 //
@@ -265,87 +267,90 @@ boolean_t WAD_WriteFile(char *name)
 //
 boolean_t WAD_ReadFile(char *name)
 {
-  FILE *fp;
-  wadinfo_t wadinfo;
-  lumpinfo_t lumpinfo;
-  lump_t lump;
-  int i;
-
-  byte* data;
-  char lumpname[9];
-
-  // Open File
-  fp = fopen(name, "rb");
-  if (!fp)
-    return false;
-
-  // Read Header
-  fread((void*)&wadinfo, sizeof(wadinfo_t), 1, fp);
-
-  // Check Header is for a PWAD
-  if (strncmp(wadinfo.id, PWAD_HEADER, 4))
-  {
-    printf("Got: %s\n",wadinfo.id);
-    return false;
-  }
-
 #ifdef DEVELOPERS
-  printf("Number of lumps: %d\n", wadinfo.numlumps);
-  printf("Offset: %d\n", wadinfo.infotableofs);
+    char lumpname[9];
+    lump_t lump;
 #endif
 
-  // Read and alloc for lumps
-  fseek(fp, wadinfo.infotableofs, SEEK_SET);
-  i = wadinfo.numlumps;
-  while(i)
-  {
-    fread((void*)&lumpinfo.filepos, sizeof(int),  1, fp);
-    fread((void*)&lumpinfo.size,    sizeof(int),  1, fp);
-    fread((void*)lumpinfo.name,    sizeof(char), 8, fp);
+    FILE *fp;
+    wadinfo_t wadinfo;
+    lumpinfo_t lumpinfo;
+    int i;
+
+    byte* data;
+
+    // Open File
+    fp = fopen(name, "rb");
+    if (!fp)
+        return false;
+
+    // Read Header
+    fread((void*)&wadinfo, sizeof(wadinfo_t), 1, fp);
+
+    // Check Header is for a PWAD
+    if (strncmp(wadinfo.id, PWAD_HEADER, 4))
+    {
+        printf("Got: %s\n",wadinfo.id);
+        return false;
+    }
 
 #ifdef DEVELOPERS
-    memset(lumpname, ' ', 8);
-    strncpy(lumpname, lumpinfo.name, 8);
-    lumpname[8] = '\0';
-    printf("%s,%d,%d,\n", lumpname, lumpinfo.size, lumpinfo.filepos);
+    printf("Number of lumps: %d\n", wadinfo.numlumps);
+    printf("Offset: %d\n", wadinfo.infotableofs);
 #endif
 
-    if (!AddLumpinfo(&lumpinfo))
-      return false;
+    // Read and alloc for lumps
+    fseek(fp, wadinfo.infotableofs, SEEK_SET);
+    i = wadinfo.numlumps;
+    while(i)
+    {
+        fread((void*)&lumpinfo.filepos, sizeof(int),  1, fp);
+        fread((void*)&lumpinfo.size,    sizeof(int),  1, fp);
+        fread((void*)lumpinfo.name,    sizeof(char), 8, fp);
 
-    i--;
-  }
+#ifdef DEVELOPERS
+        memset(lumpname, ' ', 8);
+        strncpy(lumpname, lumpinfo.name, 8);
+        lumpname[8] = '\0';
+        printf("%s,%d,%d,\n", lumpname, lumpinfo.size, lumpinfo.filepos);
+#endif
 
-  // go through lumpinfo list and load lumps
-  i=0;
-  while(i<numoflumpinfs)
-  {
-    fseek(fp, lumpinfolist[i]->filepos, SEEK_SET);
+        if (!AddLumpinfo(&lumpinfo))
+            return false;
 
-    data = malloc(sizeof(byte)*lumpinfolist[i]->size);
-    fread((void*)data, sizeof(byte), lumpinfolist[i]->size, fp);
+        i--;
+    }
 
-    if (!WAD_AddLump(data, lumpinfolist[i]->size, lumpinfolist[i]->name))
-      return false;
+    // go through lumpinfo list and load lumps
+    i=0;
+    while(i<numoflumpinfs)
+    {
+        fseek(fp, lumpinfolist[i]->filepos, SEEK_SET);
 
-    i++;
-  }
+        data = malloc(sizeof(byte)*lumpinfolist[i]->size);
+        fread((void*)data, sizeof(byte), lumpinfolist[i]->size, fp);
 
-  // Close file
-  fclose(fp);
+        if (!WAD_AddLump(data, lumpinfolist[i]->size, lumpinfolist[i]->name))
+            return false;
 
-  // go through lumpinfo list and free lumpinfo - we don't need them now
-  i=0;
-  while(lumpinfolist && i<numoflumpinfs)
-  {
-    free(lumpinfolist[i]);
-    i++;
-  }
+        i++;
+    }
 
-  if (lumpinfolist)
-    free(lumpinfolist);
+    // Close file
+    fclose(fp);
 
-  return true;
+    // go through lumpinfo list and free lumpinfo - we don't need them now
+    i=0;
+    while(lumpinfolist && i<numoflumpinfs)
+    {
+        free(lumpinfolist[i]);
+        i++;
+    }
+
+    if (lumpinfolist)
+        free(lumpinfolist);
+
+    return true;
 }
 
 //
@@ -353,18 +358,18 @@ boolean_t WAD_ReadFile(char *name)
 //
 void WAD_Shutdown(void)
 {
-  int i;
+    int i;
 
-  // go through lump list and free lump
-  i=0;
-  while(lumplist && i<numoflumps)
-  {
-    free(lumplist[i]);
-    i++;
-  }
+    // go through lump list and free lump
+    i=0;
+    while(lumplist && i<numoflumps)
+    {
+        free(lumplist[i]);
+        i++;
+    }
 
-  if (lumplist)
-    free(lumplist);
+    if (lumplist)
+        free(lumplist);
 
-  return;
+    return;
 }
