aecbaad931b54c9f907c9ba4f9b69cce9ae9aea1
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2005-04-23 05:08:03 +0000

    Implemented the boiler-plate stuff for the config classes.

--- tools/edit_ddf/config.cc
+++ tools/edit_ddf/config.cc
@@ -18,51 +18,183 @@
 
 #include "defs.h"
  
-keyword_set_c::keyword_set_c() : head(NULL)
+word_group_c::word_group_c() : num_words(0)
 { }
 
-keyword_set_c::~keyword_set_c()
+word_group_c::word_group_c(const char *W) : num_words(0)
 {
+	Append(W);
+}
+
+word_group_c::word_group_c(const char *W1, const char *W2) : num_words(0)
+{
+	Append(W1);
+	Append(W2);
+}
+
+word_group_c::word_group_c(const word_group_c& other)
+{
+	num_words = other.num_words;
+
+	for (int i=0; i < num_words; i++)
+		words[i] = strdup(other.words[i]);
+}
+
+word_group_c::~word_group_c()
+{
+	Clear();
+}
+
+const char *word_group_c::Get(int index) const
+{
+	SYS_ASSERT(index >= 0);
+	SYS_ASSERT(index < num_words);
+
+	return words[index];
+}
+
+void word_group_c::Clear()
+{
+	for (int w = 0; w < num_words; w++)
+		delete words[w];
+
+	num_words = 0;
+}
+
+void word_group_c::Append(const char *W)
+{
+	SYS_ASSERT(num_words < MAX_WORDS);
+
+	words[num_words++] = strdup(W);
+}
+
+//------------------------------------------------------------------------
+
+keyword_box_c::keyword_box_c(int _type, const char *_name) :
+	type(type), head(NULL), tail(NULL)
+{
+	name = strdup(_name);
+}
+
+keyword_box_c::~keyword_box_c()
+{
+	free((void*)name);
+
 	while (head)
 	{
 		nd_c *cur = head;
-		head = cur->next;
+		head = head->next;
 
 		delete cur;
 	}
 }
 
-keyword_set_c::nd_c::nd_c(const char *K) : next(NULL)
+keyword_box_c::nd_c::nd_c() : next(NULL), group()
+{ }
+
+keyword_box_c::nd_c::~nd_c()
+{ }
+
+bool keyword_box_c::MatchName(const char *req_name) const
 {
-	keyword = strdup(K);
+	return (UtilStrCaseCmp(name, req_name) == 0);
 }
 
-keyword_set_c::nd_c::~nd_c()
+int keyword_box_c::Size() const
 {
-	free(keyword);
+	int size = 0;
+
+	for (nd_c *cur = head; cur; cur = cur->next)
+		size++;
+	
+	return size;
 }
 
-
-//
-// Path Constructor
-//
-defbox_container_c::defbox_container_c() : point_num(0), points(NULL)
+word_group_c *keyword_box_c::Get(int index) const
 {
+	SYS_ASSERT(index >= 0);
+
+	nd_c *cur = head;
+
+	for (; index > 0; index--)
+	{
+		SYS_NULL_CHECK(cur);
+		cur = cur->next;
+	}
+
+	SYS_NULL_CHECK(cur);
+	return &cur->group;
 }
 
-//
-// Path Destructor
-//
-defbox_container_c::~defbox_container_c()
+bool keyword_box_c::HasKeyword(const char *W)
 {
-	if (points)
-		delete[] points;
+	return false; // !!!! FIXME
+}
+
+void keyword_box_c::BeginNew(const char *W)
+{
+	nd_c *cur = new nd_c();
+
+	cur->group.Append(W);
+
+	// link it in
+	if (tail)
+		tail->next = cur;
+	else
+		head = cur;
+	
+	tail = cur;
+}
+
+void keyword_box_c::AddToCurrent(const char *W)
+{
+	SYS_NULL_CHECK(tail);
+
+	tail->group.Append(W);
+}
+
+//------------------------------------------------------------------------
+
+kb_container_c::kb_container_c() : num_boxes(0)
+{ }
+
+kb_container_c::~kb_container_c()
+{
+	Clear();
+}
+
+keyword_box_c *kb_container_c::Get(int index) const
+{
+	SYS_ASSERT(index >= 0);
+	SYS_ASSERT(index < num_boxes);
+
+	return boxes[index];
+}
+
+keyword_box_c *kb_container_c::Find(int type, const char *name)
+{
+	return NULL;  // !!!! FIXME
+}
+
+void kb_container_c::Clear()
+{
+	for (int i = 0; i < num_boxes; i++)
+		delete boxes[i];
+	
+	num_boxes = 0;
+}
+
+void kb_container_c::Append(keyword_box_c *B)
+{
+	SYS_ASSERT(num_boxes < MAX_BOXES);
+
+	boxes[num_boxes++] = B;
 }
 
 //
 // Config Reading
 //
-void defbox_container_c::ReadFile(const char *filename)
+bool kb_container_c::ReadFile(const char *filename)
 {
 	FILE *fp = fopen(filename, "r");
 
@@ -73,21 +205,16 @@ void defbox_container_c::ReadFile(const char *filename)
 	}
 
 	// clear previous stuff
+	Clear();
 
-	defbox_container_c *P = new defbox_container_c();
-
-	P->points = new int[MAX_PTS * 2];
-
-	for (P->point_num = 0; P->point_num < MAX_PTS; P->point_num++)
-	{
-		int *cur_pt = P->points + (P->point_num * 2);
-
-		if (fscanf(fp, " %d %d ", cur_pt, cur_pt+1) != 2)
-			break;
-	}
+	// !!!! FIXME:
 
 	fclose(fp);
 
-	return P;
+	return true;
 }
 
+bool kb_container_c::WriteFile(const char *filename)
+{
+	return false; // !!!! FIXME
+}
