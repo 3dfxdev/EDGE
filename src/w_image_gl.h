f238c75f8f60323d81310d8e0f0db5219587a35e
darkknight <darkknight@28424279-154d-4b81-a449-c98279361746>
2005-11-13 15:02:44 +0000

    GL Specific image methods moved to seperate file

---
+++ src/w_image_gl.h
@@ -0,0 +1,37 @@
+//----------------------------------------------------------------------------
+//  EDGE Generalised Image Handling (OpenGL Specifics)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 2005  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+//  Based on the DOOM source code, released by Id Software under the
+//  following copyright:
+//
+//    Copyright (C) 1993-1996 by id Software, Inc.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __W_IMAGE_OGL__
+#define __W_IMAGE_OGL__
+
+#include "w_image.h"
+
+void W_LockImagesOGL(void);
+void W_UnlockImagesOGL(void);
+
+GLuint W_ImageGetOGL(const cached_image_t *c);
+
+#endif  // __W_IMAGE_OGL__
+
