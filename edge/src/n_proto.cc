2d2f9a84f00f86857822cb2e4e9d475f260f0f1b
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-03-19 10:16:04 +0000

    Merged protocol.cc body into n_proto.cc.

--- edge/src/n_proto.cc
+++ edge/src/n_proto.cc
@@ -19,8 +19,66 @@
 #include "i_defs.h"
 #include "version.h"
 
-#include "protocol.cc"
+#include "epi/types.h"
+#include "epi/endianess.h"
 
+#include "n_protocol.h"
+
+
+void ticcmd_t::ByteSwap()
+{
+	angleturn = EPI_BE_S16(angleturn);
+	mlookturn = EPI_BE_S16(mlookturn);
+
+	consistency = EPI_BE_S16(consistency);
+}
+
+void header_proto_t::ByteSwap()
+{
+	data_len = EPI_BE_S16(data_len);
+	flags    = EPI_BE_S16(flags);
+
+	seq_out  = EPI_BE_S16(seq_out);
+	seq_recv = EPI_BE_S16(seq_recv);
+}
+
+void connect_proto_t::ByteSwap()
+{
+	client_ver = EPI_BE_S16(client_ver);
+}
+
+void welcome_proto_t::ByteSwap()
+{
+	host_ver = EPI_BE_U16(host_ver);
+
+	gameplay    = EPI_BE_U32(gameplay);
+	random_seed = EPI_BE_U32(random_seed);
+
+	wad_checksum = EPI_BE_U16(wad_checksum);
+	def_checksum = EPI_BE_U16(def_checksum);
+}
+
+void player_info_t::ByteSwap()
+{
+	player_flags = EPI_BE_S16(player_flags);
+}
+
+void player_list_proto_t::ByteSwapPlayers(int num_players)
+{
+	for (int p = 0; p < num_players; p++)
+		players[p].ByteSwap();
+}
+
+void ticcmd_proto_t::ByteSwap()
+{
+	gametic = EPI_BE_U32(gametic);
+}
+
+void ticcmd_proto_t::ByteSwapCmds(int num_cmds)
+{
+	for (int t = 0; t < num_cmds; t++)
+		cmds[t].ByteSwap();
+}
 
 //--- editor settings ---
 // vi:ts=4:sw=4:noexpandtab
