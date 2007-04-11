e6b96a44468f690f70dbf10d2418be1daf8fe576
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-04-11 08:55:47 +0000

    Allow mouse wheel to be used in menus.

--- src/m_menu.cc
+++ src/m_menu.cc
@@ -1802,6 +1802,7 @@ bool M_Responder(event_t * ev)
 	switch (ch)
 	{
 		case KEYD_DOWNARROW:
+		case KEYD_MWHEEL_DN:
 			do
 			{
 				if (itemOn + 1 > currentMenu->numitems - 1)
@@ -1814,6 +1815,7 @@ bool M_Responder(event_t * ev)
 			return true;
 
 		case KEYD_UPARROW:
+		case KEYD_MWHEEL_UP:
 			do
 			{
 				if (itemOn == 0)
