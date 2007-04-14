29a1d667cd64b8cba6b53f9851b919fb1884adae
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-04-14 05:34:14 +0000

    Moved video mode stuff (R_ExecuteChangeResolution etc) from r_main to r_modes.

--- src/r_modes.h
+++ src/r_modes.h
@@ -89,6 +89,16 @@ void V_InitResolution(void);
 void V_AddAvailableResolution(i_scrmode_t *mode);
 void V_GetSysRes(scrmode_t *src, i_scrmode_t *dest);
 
+// call this to change the resolution before the next frame.
+// -ACB- 2005/03/06: Now uses an index in the scrmodelist
+void R_ChangeResolution(int res_idx); 
+
+// only call these when it really is time to do the actual resolution
+// or view size change, i.e. at the start of a frame.
+void R_ExecuteChangeResolution(void);
+void R_ExecuteSetViewSize(void);
+
+
 // Exported Vars
 extern int SCREENWIDTH;
 extern int SCREENHEIGHT;
