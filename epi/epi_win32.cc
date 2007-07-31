ecb62bfd87760f63ac4114cb0bd78d588172d0cc
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-07-31 11:53:38 +0000

    Removed the unused memory manager code.

--- epi/epi_win32.cc
+++ epi/epi_win32.cc
@@ -25,9 +25,7 @@
 namespace epi
 {
 	filesystem_c *the_filesystem;
-	mem_manager_c *the_mem_manager;
 
-	const unsigned int max_mem_size = 33554432; // 32Mb 
 	static bool inited = false;
 
     //
@@ -36,22 +34,12 @@ namespace epi
     bool Init(void)
     {
 		bool ok;
-		mem_manager_c* mm = NULL;
 		win32_filesystem_c* fs = NULL;
 
 		if (inited)
 			Shutdown();
 
 		ok = true;
-
-		if (ok)
-		{
-			mm = new mem_manager_c(max_mem_size);
-			if (mm)
-				the_mem_manager = mm;
-			else
-				ok = false;
-		}
 
 		if (ok)
 		{
@@ -75,12 +63,6 @@ namespace epi
 		{
 			delete the_filesystem;
 			the_filesystem = NULL;
-		}
-
-		if (the_mem_manager)
-		{
-			delete the_mem_manager;
-			the_mem_manager = NULL;
 		}
 
 		inited = false;
