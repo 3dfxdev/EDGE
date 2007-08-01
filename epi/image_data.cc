3513c7b7e21160b6d4ca1bb26f324c020cfdda80
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-01 09:39:15 +0000

    Misc improvements.

--- epi/image_data.cc
+++ epi/image_data.cc
@@ -27,29 +27,30 @@ namespace epi
 {
 
 image_data_c::image_data_c(int _w, int _h, int _bpp) :
-    width(_w), height(_h), bpp(_bpp)
+    width(_w), height(_h), bpp(_bpp), flags(0)
 {
-	int size = width * height * bpp;
-
-	pixels = new u8_t[size];
-
-	memset(pixels, 0, size);
+	pixels = new u8_t[width * height * bpp];
 }
 
 image_data_c::~image_data_c()
 {
 	delete[] pixels;
 
+	pixels = NULL;
 	width = height = 0;
-	pixels = 0;
 }
 
-image_data_c::Whiten()
+void image_data_c::Clear(u8_t val)
+{
+	memset(pixels, val, width * height * bpp);
+}
+
+void image_data_c::Whiten()
 {
 	EPI_ASSERT(bpp >= 3);
 
 	for (int y = 0; y < height; y++)
-	for (int x = 0; x < width; x++)
+	for (int x = 0; x < width;  x++)
 	{
 		u8_t *cur = img->PixelAt(x, y);
 
@@ -77,7 +78,7 @@ void image_data_c::Shrink(int new_w, int new_h)
     for (int dy=0; dy < new_h; dy++)
     for (int dx=0; dx < new_w; dx++)
     {
-      u8_t *dest_pix = pixelAt(dx, dy);
+      u8_t *dest_pix = pixels + (dy * new_w + x) * 3;
 
       int sx = x * step_x;
       int sy = y * step_y;
@@ -88,7 +89,7 @@ void image_data_c::Shrink(int new_w, int new_h)
       for (int dx=0; x < step_x; x++)
       for (int dy=0; y < step_y; y++)
       {
-        u8_t *src_pix = pixelAt(sx+x, sy+y);
+        u8_t *src_pix = PixelAt(sx+x, sy+y);
 
         r += src_pix[0];
         g += src_pix[1];
@@ -105,7 +106,7 @@ void image_data_c::Shrink(int new_w, int new_h)
     for (int dy=0; dy < new_h; dy++)
     for (int dx=0; dx < new_w; dx++)
     {
-      u8_t *dest_pix = pixelAt(dx, dy);
+      u8_t *dest_pix = pixels + (dy * new_w + x) * 4;
 
       int sx = dx * step_x;
       int sy = dy * step_y;
@@ -116,7 +117,7 @@ void image_data_c::Shrink(int new_w, int new_h)
       for (int x=0; x < step_x; x++)
       for (int y=0; y < step_y; y++)
       {
-        u8_t *src_pix = pixelAt(sx+x, sy+y);
+        u8_t *src_pix = PixelAt(sx+x, sy+y);
 
         r += src_pix[0];
         g += src_pix[1];
@@ -130,17 +131,16 @@ void image_data_c::Shrink(int new_w, int new_h)
       dest_pix[3] = a / total;
     }
   }
+
+  width  = new_w;
+  height = new_h;
 }
 
 void image_data_c::Grow(int new_w, int new_h)
 {
 	EPI_ASSERT(new_w >= width && new_h >= height);
 
-	int new_size = new_w * new_h * bpp;
-
-	u8_t *new_pixels = new u8_t[new_size];
-
-	// TODO: OPTIMISE this
+	u8_t *new_pixels = new u8_t[new_w * new_h * bpp];
 
 	for (int dy = 0; dy < new_h; dy++)
 	for (int dx = 0; dx < new_w; dx++)
@@ -148,19 +148,19 @@ void image_data_c::Grow(int new_w, int new_h)
 		int sx = dx * width  / new_w;
 		int sy = dy * height / new_h;
 
-		const u8_t *src = pixelAt(sx, sy);
+		const u8_t *src = PixelAt(sx, sy);
 
 		u8_t *dest = new_pixels + (dy * new_w + dx) * bpp;
 
 		for (int i = 0; i < bpp; i++)
 			*dest++ = *src++;
 	}
-	
+
 	delete[] pixels;
 
-	pixels = new_pixels;
-	width  = new_w;
-	height = new_h;
+	pixels  = new_pixels;
+	width   = new_w;
+	height  = new_h;
 }
 
 } // namespace epi
