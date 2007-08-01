90af144aaf4fbe6e53a9765ccd4f8692d050a6f8
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-01 13:22:41 +0000

    Updated Copyright year to 2007.

--- epi/math_crc.h
+++ epi/math_crc.h
@@ -2,7 +2,7 @@
 //  EDGE CRC : Cyclic Rendundancy Check
 //------------------------------------------------------------------------
 // 
-//  Copyright (c) 2003-2005  The EDGE Team.
+//  Copyright (c) 2003-2007  The EDGE Team.
 // 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
