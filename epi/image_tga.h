2de53ac2f53df729da11fbfb7b8a520bc4cc7103
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-01 09:52:16 +0000

    Skeletal files for TGA image loading.

---
+++ epi/image_tga.h
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------
+//  Targa Image Handling
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2006-2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __EPI_IMAGE_TGA_H__
+#define __EPI_IMAGE_TGA_H__
+
+#include "epi.h"
+#include "image_data.h"
+#include "files.h"
+
+#include <stdio.h>
+
+namespace epi
+{
+
+image_data_c *TGA_Load(file_c *f, bool invert = false, bool round_pow2 = false);
+// loads the given TGA image.  Returns 0 if something went wrong.
+// The image will be RGB or RGBA (never paletted).  The size of
+// image (width and height) are rounded to the next highest
+// power-of-two when round_pow2 is set.
+// FIXME: throw exception on failure
+
+bool TGA_GetInfo(file_c *f, int *width, int *height, bool *solid);
+// reads the principle information from the TGA header.
+// (should be much faster than loading the whole image).
+// Returns false if something went wrong.
+// Note: size returned here is the real size, and may be different
+// from the image returned by Load() which rounds to power-of-two.
+// FIXME: throw exception on failure
+
+} // namespace epi
+
+#endif  /* __EPI_IMAGE_TGA_H__ */
