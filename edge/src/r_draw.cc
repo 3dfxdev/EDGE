27556c0a9b6bc8b898a8efbf883c92d7d64f1b2c
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-02 06:29:00 +0000

    Split 2D drawing stuff from r_main.cc --> new file r_draw.cc

---
+++ edge/src/r_draw.cc
@@ -0,0 +1,275 @@
+//----------------------------------------------------------------------------
+//  EDGE 2D DRAWING STUFF
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+#include "i_defs_gl.h"
+
+#include "g_game.h"
+#include "r_misc.h"
+#include "r_gldefs.h"
+#include "r_units.h"
+#include "r_colors.h"
+#include "v_ctx.h"
+#include "r_modes.h"
+#include "r_image.h"
+
+#include "epi/strings.h"
+
+#include <string.h>
+
+
+
+//
+//  VIDEO CONTEXT STUFF
+//
+
+//
+// RGL_NewcreenSize
+//
+void RGL_NewScreenSize(int width, int height, int bits)
+{
+	//!!! quick hack
+	RGL_SetupMatrices2D();
+
+	// prevent a visible border with certain cards/drivers
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+//
+// RGL_DrawImage
+//
+void RGL_DrawImage(float x, float y, float w, float h, const image_c *image, 
+				   float tx1, float ty1, float tx2, float ty2,
+				   const colourmap_c *colmap, float alpha)
+{
+	int x1 = I_ROUND(x);
+	int y1 = I_ROUND(y);
+	int x2 = I_ROUND(x+w+0.25f);
+	int y2 = I_ROUND(y+h+0.25f);
+
+	if (x1 == x2 || y1 == y2)
+		return;
+
+	float r = 1.0f, g = 1.0f, b = 1.0f;
+
+	GLuint tex_id = W_ImageCache(image, false,
+		(colmap && (colmap->special & COLSP_Whiten)) ? font_whiten_map : NULL);
+
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, tex_id);
+ 
+	if (alpha < 0.99f || !image->img_solid)
+		glEnable(GL_BLEND);
+
+///  else if (!image->img_solid)
+///    glEnable(GL_ALPHA_TEST);
+ 
+	if (colmap)
+		V_GetColmapRGB(colmap, &r, &g, &b, true);
+
+	glColor4f(r, g, b, alpha);
+
+	glBegin(GL_QUADS);
+  
+	glTexCoord2f(tx1, 1.0f - ty1);
+	glVertex2i(x1, SCREENHEIGHT - y1);
+
+	glTexCoord2f(tx2, 1.0f - ty1); 
+	glVertex2i(x2, SCREENHEIGHT - y1);
+  
+	glTexCoord2f(tx2, 1.0f - ty2);
+	glVertex2i(x2, SCREENHEIGHT - y2);
+  
+	glTexCoord2f(tx1, 1.0f - ty2);
+	glVertex2i(x1, SCREENHEIGHT - y2);
+  
+	glEnd();
+
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_BLEND);
+}
+
+
+//
+// RGL_SolidBox
+//
+void RGL_SolidBox(int x, int y, int w, int h, rgbcol_t col, float alpha)
+{
+	if (alpha < 0.99f)
+		glEnable(GL_BLEND);
+  
+	glColor4f(RGB_RED(col), RGB_GRN(col), RGB_BLU(col), alpha);
+  
+	glBegin(GL_QUADS);
+
+	glVertex2i(x,   (SCREENHEIGHT - y));
+	glVertex2i(x,   (SCREENHEIGHT - y - h));
+	glVertex2i(x+w, (SCREENHEIGHT - y - h));
+	glVertex2i(x+w, (SCREENHEIGHT - y));
+  
+	glEnd();
+	glDisable(GL_BLEND);
+}
+
+//
+// RGL_SolidLine
+//
+void RGL_SolidLine(int x1, int y1, int x2, int y2, rgbcol_t col, float alpha)
+{
+	glColor4f(RGB_RED(col), RGB_GRN(col), RGB_BLU(col), alpha);
+  
+	glBegin(GL_LINES);
+
+	glVertex2i(x1, (SCREENHEIGHT - y1));
+	glVertex2i(x2, (SCREENHEIGHT - y2));
+  
+	glEnd();
+}
+
+//
+// RGL_ReadScreen
+//
+// -ACB- 29/04/2002: Implementation
+//
+void RGL_ReadScreen(int x, int y, int w, int h, byte *rgb_buffer)
+{
+	glFlush();
+
+	glPixelZoom(1.0f, 1.0f);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	for (; h > 0; h--, y++)
+	{
+		glReadPixels(x, y, w, 1, GL_RGB, GL_UNSIGNED_BYTE, rgb_buffer);
+
+		rgb_buffer += w * 3;
+	}
+}
+
+
+static void ProgressSection(const byte *logo_lum, int lw, int lh,
+	const byte *text_lum, int tw, int th,
+	float cr, float cg, float cb,
+	int *y, int perc, float alpha)
+{
+	float zoom = 1.0f;
+
+	(*y) -= (int)(lh * zoom);
+
+	glRasterPos2i(20, *y);
+	glPixelZoom(zoom, zoom);
+	glDrawPixels(lw, lh, GL_LUMINANCE, GL_UNSIGNED_BYTE, logo_lum);
+
+	(*y) -= th + 20;
+
+	glRasterPos2i(20, *y);
+	glPixelZoom(1.0f, 1.0f);
+	glDrawPixels(tw, th, GL_LUMINANCE, GL_UNSIGNED_BYTE, text_lum);
+
+	int px = 20;
+	int pw = SCREENWIDTH - 80;
+	int ph = 30;
+	int py = *y - ph - 20;
+
+	int x = (pw-8) * perc / 100;
+
+	glColor4f(0.6f, 0.6f, 0.6f, alpha);
+	glBegin(GL_POLYGON);
+	glVertex2i(px, py);  glVertex2i(px, py+ph);
+	glVertex2i(px+pw, py+ph); glVertex2i(px+pw, py);
+	glVertex2i(px, py);
+	glEnd();
+
+	glColor4f(0.0f, 0.0f, 0.0f, alpha);
+	glBegin(GL_POLYGON);
+	glVertex2i(px+2, py+2);  glVertex2i(px+2, py+ph-2);
+	glVertex2i(px+pw-2, py+ph-2); glVertex2i(px+pw-2, py+2);
+	glEnd();
+
+	glColor4f(cr, cg, cb, alpha);
+	glBegin(GL_POLYGON);
+	glVertex2i(px+4, py+4);  glVertex2i(px+4, py+ph-4);
+	glVertex2i(px+4+x, py+ph-4); glVertex2i(px+4+x, py+4);
+	glEnd();
+
+	(*y) = py;
+}
+
+//
+// RGL_DrawProgress
+//
+// Show EDGE logo and a progress indicator.
+//
+void RGL_DrawProgress(int perc, int glbsp_perc)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_BLEND);
+
+	int y = SCREENHEIGHT - 20;
+	
+	const byte *logo_lum; int lw, lh;
+	const byte *text_lum; int tw, th;
+
+	logo_lum = RGL_LogoImage(&lw, &lh);
+	text_lum = RGL_InitImage(&tw, &th);
+
+	ProgressSection(logo_lum, lw, lh, text_lum, tw, th,
+		0.4f, 0.6f, 1.0f, &y, perc, 1.0f);
+
+	y -= 40;
+
+	if (glbsp_perc >= 0 || glbsp_last_prog_time > 0)
+	{
+		// logic here is to avoid the brief flash of progress
+		int tim = I_GetTime();
+		float alpha = 1.0f;
+
+		if (glbsp_perc >= 0)
+			glbsp_last_prog_time = tim;
+		else
+		{
+			alpha = 1.0f - float(tim - glbsp_last_prog_time) / (TICRATE*3/2);
+
+			if (alpha < 0)
+			{
+				alpha = 0;
+				glbsp_last_prog_time = 0;
+			}
+
+			glbsp_perc = 100;
+		}
+
+		logo_lum = RGL_GlbspImage(&lw, &lh);
+		text_lum = RGL_BuildImage(&tw, &th);
+
+		ProgressSection(logo_lum, lw, lh, text_lum, tw, th,
+			1.0f, 0.2f, 0.1f, &y, glbsp_perc, alpha);
+	}
+
+	glDisable(GL_BLEND);
+
+	I_FinishFrame();
+	I_StartFrame();
+}
+
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
