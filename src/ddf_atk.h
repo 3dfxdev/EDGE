3181b5039ab65b984e7db6abf7b65efc0c9cdeeb
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-03 05:10:04 +0000

    Split off definitions into their own header files, such as attack stuff --> ddf_atk.h, etc...

---
+++ src/ddf_atk.h
@@ -0,0 +1,169 @@
+//----------------------------------------------------------------------------
+//  EDGE Data Definition File Code (Main)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __DDF_ATK_H__
+#define __DDF_ATK_H__
+
+#include "dm_defs.h"
+
+
+// ------------------------------------------------------------------
+// --------------------ATTACK TYPE STRUCTURES------------------------
+// ------------------------------------------------------------------
+
+// -KM- 1998/11/25 Added BFG SPRAY attack type.
+
+// FIXME!!! Move enums into attackdef_t
+typedef enum
+{
+	ATK_NONE = 0,
+	ATK_PROJECTILE,
+	ATK_SPAWNER,
+	ATK_TRIPLESPAWNER,
+	ATK_SPREADER,
+	ATK_RANDOMSPREAD,
+	ATK_SHOT,
+	ATK_TRACKER,
+	ATK_CLOSECOMBAT,
+	ATK_SHOOTTOSPOT,
+	ATK_SKULLFLY,
+	ATK_SMARTPROJECTILE,
+	ATK_SPRAY,
+	NUMATKCLASS
+}
+attackstyle_e;
+
+typedef enum
+{
+	AF_None            = 0,
+	AF_TraceSmoke      = 1,
+	AF_KillFailedSpawn = 2,
+	AF_PrestepSpawn    = 4,
+	AF_SpawnTelefrags  = 8,
+	AF_NeedSight       = 16,
+	AF_FaceTarget      = 32,
+	AF_Player          = 64,
+	AF_ForceAim        = 128,
+
+	AF_AngledSpawn     = 0x0100,
+	AF_NoTriggerLines  = 0x0200,
+	AF_SilentToMon     = 0x0400,
+	AF_NoTarget        = 0x0800
+}
+attackflags_e;
+
+// attack definition class
+class atkdef_c
+{
+public:
+	atkdef_c();
+	atkdef_c(atkdef_c &rhs); 
+	~atkdef_c();
+
+private:
+	void Copy(atkdef_c &src);
+
+public:
+	void CopyDetail(atkdef_c &src);
+	void Default();
+	atkdef_c& operator=(atkdef_c &rhs);
+
+	// Member vars
+	ddf_base_c ddf;
+
+	attackstyle_e attackstyle;
+	attackflags_e flags;
+	sfx_t *initsound;
+	sfx_t *sound;
+	float accuracy_slope;
+	angle_t accuracy_angle;
+	float xoffset;
+	float yoffset;
+	angle_t angle_offset;  // -AJA- 1999/09/10.
+	float slope_offset;    //
+	angle_t trace_angle; // -AJA- 2005/02/08.
+	float assault_speed;
+	float height;
+	float range;
+	int count;
+	int tooclose;
+	float berserk_mul;  // -AJA- 2005/08/06.
+	damage_c damage;
+
+	// class of the attack.
+	bitset_t attack_class;
+ 
+	// object init state.  The integer value only becomes valid after
+	// DDF_AttackCleanUp() has been called.
+	int objinitstate;
+	epi::strent_c objinitstate_ref;
+  
+	percent_t notracechance;
+	percent_t keepfirechance;
+
+	// the MOBJ that is integrated with this attack, or NULL
+	const mobjtype_c *atk_mobj;
+
+	// spawned object (for spawners).  The mobjdef pointer only becomes
+	// valid after DDF_AttackCleanUp().  Can be NULL.
+	const mobjtype_c *spawnedobj;
+	epi::strent_c spawnedobj_ref;
+	int spawn_limit;
+  
+	// puff object.  The mobjdef pointer only becomes valid after
+	// DDF_AttackCleanUp() has been called.  Can be NULL.
+	const mobjtype_c *puff;
+	epi::strent_c puff_ref;
+};
+
+class atkdef_container_c : public epi::array_c
+{
+public:
+	atkdef_container_c();
+	~atkdef_container_c();
+
+private:
+	void CleanupObject(void *obj);
+
+	int num_disabled;					// Number of disabled 
+
+public:
+	// List Management
+	int GetSize() {	return array_entries; } 
+	int Insert(atkdef_c *a) { return InsertObject((void*)&a); }
+	atkdef_c* operator[](int idx) { return *(atkdef_c**)FetchObject(idx); } 
+
+	// Inliners
+	int GetDisabledCount() { return num_disabled; }
+	void SetDisabledCount(int _num_disabled) { num_disabled = _num_disabled; }
+
+	// Search Functions
+	atkdef_c* Lookup(const char* refname);
+};
+
+
+// -----EXTERNALISATIONS-----
+
+extern atkdef_container_c atkdefs;			// -ACB- 2004/06/09 Implemented
+
+bool DDF_ReadAtks(void *data, int size);
+
+#endif // __DDF_ATK_H__
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ src/ddf_sfx.h
@@ -0,0 +1,151 @@
+//----------------------------------------------------------------------------
+//  EDGE Data Definition File Code (Main)
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __DDF_SFX_H__
+#define __DDF_SFX_H__
+
+#include "dm_defs.h"
+
+
+// ----------------------------------------------------------------
+// ------------------------ SOUND EFFECTS -------------------------
+// ----------------------------------------------------------------
+
+// -KM- 1998/10/29
+typedef struct sfx_s
+{
+	int num;
+	int sounds[1]; // -ACB- 1999/11/06 Zero based array is not ANSI compliant
+	// -AJA- I'm also relying on the [1] within sfxdef_c.
+}
+sfx_t;
+
+// Bastard SFX are sounds that are hardcoded into the
+// code.  They should be removed if at all possible
+
+typedef struct
+{
+	sfx_t *s;
+	char name[8];
+}
+bastard_sfx_t;
+
+extern bastard_sfx_t bastard_sfx[];
+
+#define sfx_swtchn bastard_sfx[0].s
+#define sfx_tink bastard_sfx[1].s
+#define sfx_radio bastard_sfx[2].s
+#define sfx_oof bastard_sfx[3].s
+#define sfx_pstop bastard_sfx[4].s
+#define sfx_stnmov bastard_sfx[5].s
+#define sfx_pistol bastard_sfx[6].s
+#define sfx_swtchx bastard_sfx[7].s
+#define sfx_jpmove bastard_sfx[8].s
+#define sfx_jpidle bastard_sfx[9].s
+#define sfx_jprise bastard_sfx[10].s
+#define sfx_jpdown bastard_sfx[11].s
+#define sfx_jpflow bastard_sfx[12].s
+
+#define sfx_None (sfx_t*) NULL
+
+// Sound Effect Definition Class
+class sfxdef_c
+{
+public:
+	sfxdef_c();
+	sfxdef_c(sfxdef_c &rhs);
+	~sfxdef_c();
+	
+private:
+	void Copy(sfxdef_c &src);
+	
+public:
+	void CopyDetail(sfxdef_c &src);
+	void Default(void);
+	sfxdef_c& operator=(sfxdef_c &rhs);
+	
+	// sound's name, etc..
+	ddf_base_c ddf;
+
+    // full sound lump name
+	lumpname_c lump_name;
+
+	// sfxinfo ID number
+	// -AJA- Changed to a sfx_t.  It serves two purposes: (a) hold the
+	//       sound ID, like before, (b) better memory usage, as we don't
+	//       need to allocate a new sfx_t for non-wildcard sounds.
+	sfx_t normal;
+
+    // Sfx singularity (only one at a time), or 0 if not singular
+	int singularity;
+
+	// Sfx priority
+	int priority;
+
+	// volume adjustment (100% is normal, lower is quieter)
+	percent_t volume;
+
+	// -KM- 1998/09/01  Looping: for non NULL origins
+	bool looping;
+
+	// -AJA- 2000/04/19: Prefer to play the whole sound rather than
+	//       chopping it off with a new sound.
+	bool precious;
+ 
+    // distance limit, if the hearer is further away than `max_distance'
+    // then the this sound won't be played at all.
+	float max_distance;
+ };
+
+// Our sound effect definition container
+class sfxdef_container_c : public epi::array_c 
+{
+public:
+	sfxdef_container_c() : epi::array_c(sizeof(sfxdef_c*)) { num_disabled = 0; }
+	~sfxdef_container_c() { Clear(); } 
+
+private:
+	void CleanupObject(void *obj);
+	
+	int num_disabled;					// Number of disabled 
+
+public:
+	// List management
+	int GetSize() { return array_entries; } 
+	int Insert(sfxdef_c *s) { return InsertObject((void*)&s); }
+	sfxdef_c* operator[](int idx) { return *(sfxdef_c**)FetchObject(idx); } 
+	
+	// Inliners
+	int GetDisabledCount() { return num_disabled; }
+	void SetDisabledCount(int _num_disabled) { num_disabled = _num_disabled; }
+	
+	// Lookup functions
+	sfx_t* GetEffect(const char *name, bool error = true);
+	sfxdef_c* Lookup(const char *name);
+};
+
+// ----------EXTERNALISATIONS----------
+
+extern sfxdef_container_c sfxdefs;			// -ACB- 2004/07/25 Implemented
+
+bool DDF_ReadSFX(void *data, int size);
+
+#endif // __DDF_SFX_H__
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
