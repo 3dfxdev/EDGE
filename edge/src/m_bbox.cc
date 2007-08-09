52ddda3cca1bf407ca5eab619d957d0060770c23
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-09 10:06:04 +0000

    Removed unused dirty-area handler.

--- edge/src/m_bbox.cc
+++ edge/src/m_bbox.cc
@@ -31,8 +31,8 @@
 
 void M_ClearBox(float * box)
 {
-	box[BOXTOP] = box[BOXRIGHT] = -FLT_MAX;
-	box[BOXBOTTOM] = box[BOXLEFT] = FLT_MAX;
+	box[BOXTOP]    = box[BOXRIGHT] = -FLT_MAX;
+	box[BOXBOTTOM] = box[BOXLEFT]  =  FLT_MAX;
 }
 
 void M_AddToBox(float * box, float x, float y)
@@ -72,71 +72,6 @@ void M_UnionBox(float * box, float * other)
 	if (other[BOXTOP] > box[BOXTOP])
 		box[BOXTOP] = other[BOXTOP];
 }
-
-//
-//  DIRTY REGION HANDLING
-//
-
-byte dirty_region[DIRT_REG_H][DIRT_REG_W];
-
-bool dirty_region_whole = true;
-bool dirty_region_always = false;
-
-//
-// M_CleanMatrix
-//
-// Make the dirty matrix totally clean (spick and span).
-
-void M_CleanMatrix(void)
-{
-	dirty_region_whole = false;
-
-	memset(dirty_region, 0, sizeof(dirty_region));
-}
-
-//
-// M_DirtyMatrix
-//
-// Make the dirty matrix totally dirty.
-
-void M_DirtyMatrix(void)
-{
-	if (dirty_region_whole)
-		return;
-
-	dirty_region_whole = true;
-
-	memset(dirty_region, 1, sizeof(dirty_region));
-}
-
-//
-// M_DirtyRegion
-//
-// Coordinates are in screen pixels (inclusive).
-
-void M_DirtyRegion(int x1, int y1, int x2, int y2)
-{
-	int x;
-
-	if (dirty_region_whole)
-		return;
-
-	SYS_ASSERT(x1 >= 0);  SYS_ASSERT(y1 >= 0);
-	SYS_ASSERT(x1 <= x2); SYS_ASSERT(y1 <= y2);
-
-	x1 /= DIRT_X; y1 /= DIRT_Y;
-	x2 /= DIRT_X; y2 /= DIRT_Y;
-
-	SYS_ASSERT(x2 < DIRT_REG_W); 
-	SYS_ASSERT(y2 < DIRT_REG_H);
-
-	for (; y1 <= y2; y1++)
-	{
-		for (x=x1; x <= x2; x++)
-			dirty_region[y1][x] = 1;
-	}
-}
-
 
 //--- editor settings ---
 // vi:ts=4:sw=4:noexpandtab
