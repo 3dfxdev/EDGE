f87668fbceaa5663e5a8b8e2e71d4274b0423cbc
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-13 08:51:11 +0000

    Moved strncasecmpwild() into the DDF code (only one file used it), and removed some dead stuff here (NEED_ASM_PREFIXES).

--- src/w32_compen.cc
+++ src/w32_compen.cc
@@ -65,25 +65,6 @@ int strncasecmp(const char *s1, const char *s2, int max)
 	return 0;
 }
 
-int strncasecmpwild(const char *s1, const char *s2, int n)
-{
-	int i;
-
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
-	{
-		if ((toupper(s1[i]) != toupper(s2[i])) && (s1[i] != '?') && (s2[i] != '?'))
-			break;
-	}
-
-	// -KM- 1999/01/29 If strings are equal return equal.
-	if (i == n)
-		return 0;
-
-	if (s1[i] == '?' || s2[i] == '?')
-		return 0;
-
-	return s1[i] - s2[i];
-}
 
 //--- editor settings ---
 // vi:ts=4:sw=4:noexpandtab
