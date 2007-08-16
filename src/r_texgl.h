a92f42dc3499bca16e3c7714f53e79b3c83036a5
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-16 09:00:32 +0000

    Check-in of new file: r_texgl.h

---
+++ src/r_texgl.h
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------------
+//  EDGE OpenGL Texture Upload
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __RGL_TEXGL_H__
+#define __RGL_TEXGL_H__
+
+
+#endif /* __RGL_TEXGL_H__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
