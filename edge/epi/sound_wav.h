064f01724c70918eb26ce2297c619ed3a977bad0
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-08-30 11:38:49 +0000

    Check-in for WAV decoding code (not yet working).

---
+++ edge/epi/sound_wav.h
@@ -0,0 +1,38 @@
+//----------------------------------------------------------------------------
+//  WAV Format Sound Loading
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 2007  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __EPI_SOUND_WAV_H__
+#define __EPI_SOUND_WAV_H__
+
+#include "file.h"
+#include "sound_data.h"
+
+namespace epi
+{
+
+sound_data_c * WAV_Load(file_c *f);
+// Decode WAV format sound data from the given file stream,
+// and return it in the form of a sound_data_c object.
+// Returns NULL if something went wrong.
+
+} // namespace epi
+
+#endif /* __EPI_SOUND_WAV_H__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
