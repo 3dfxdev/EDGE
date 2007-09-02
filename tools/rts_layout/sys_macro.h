67d662185cc995e31f204d54ac1a5eed29047ed5
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-09-02 12:38:17 +0000

    Added I_ROUND() macro.

--- tools/rts_layout/sys_macro.h
+++ tools/rts_layout/sys_macro.h
@@ -41,9 +41,12 @@
 #define SGN(a)  ((a) < 0 ? -1 : (a) > 0 ? +1 : 0)
 #endif
 
+#ifndef I_ROUND
+#define I_ROUND(x)  ((int) (((x) < 0) ? ((x) - 0.5f) : ((x) + 0.5f)))
+#endif
+
 #ifndef CLAMP
-#define CLAMP(x,low,high)  \
-    ((x) < (low) ? (low) : (x) > (high) ? (high) : (x))
+#define CLAMP(x,low,high)  ((x) < (low) ? (low) : (x) > (high) ? (high) : (x))
 #endif
 
 #endif // __SYS_MACRO_H__
