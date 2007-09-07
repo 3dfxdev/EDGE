7e745d9c55e06e893cf62fbc60431fbc75bf1eb2
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-09-07 11:44:10 +0000

    Initial implementation of Thing info box.

---
+++ tools/rts_layout/ui_thing.h
@@ -0,0 +1,65 @@
+//------------------------------------------------------------------------
+//  Spawn-Thing Info
+//------------------------------------------------------------------------
+//
+//  RTS Layout Tool (C) 2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __UI_THING_H__
+#define __UI_THING_H__
+
+
+class UI_ThingInfo : public Fl_Group
+{
+public:
+  UI_ThingInfo(int X, int Y, int W, int H, const char *label = NULL);
+  virtual ~UI_ThingInfo();
+
+private:
+  Fl_Box   *which;  // e.g. 'Thing #1234 of 7777'
+
+  Fl_Input *type;
+
+  Fl_Float_Input *pos_x;
+  Fl_Float_Input *pos_y;
+  Fl_Float_Input *pos_z;
+
+  Fl_Float_Input *angle;
+
+  Fl_Check_Button *ambush;
+
+  Fl_Int_Input *tag;
+
+  // when appear: two rows of three on/off buttons
+  Fl_Check_Button *appear_easy;
+  Fl_Check_Button *appear_medium;
+  Fl_Check_Button *appear_hard;
+
+#if 0
+  Fl_Check_Button *appear_sp;
+  Fl_Check_Button *appear_coop;
+  Fl_Check_Button *appear_dm;
+#endif
+
+public:
+
+
+private:
+
+};
+
+#endif // __UI_THING_H__
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
