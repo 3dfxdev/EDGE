5fa51bb45e230a535cf709e36626871d7d20a948
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-10-02 05:44:47 +0000

    Removed dead net code.

--- edge/src/unx_net.cc
+++ edge/src/unx_net.cc
@@ -17,7 +17,6 @@
 //----------------------------------------------------------------------------
 
 #include "i_defs.h"
-#include "i_sdlinc.h"
 #include "unx_sysinc.h"
 
 #include <unistd.h>
@@ -42,105 +41,6 @@
 #include <netinet/if_ether.h>
 #include <netinet/in.h>
 
-#ifdef USE_HAWKNL
-#include "nl.h"
-#endif
-
-bool nonet = true;
- 
-//
-// I_StartupNetwork
-//
-void I_StartupNetwork(void)
-{
-	if (SDLNet_Init() != 0)
-	{
-		I_Printf("I_StartupNetwork: SDL_net Initialisation FAILED.\n");
-		return;
-	}
-
-	I_Printf("I_StartupNetwork: SDL_net Initialised OK.\n");
-	nonet = false;
-
-
-#ifdef USE_HAWKNL
-	if (nlInit() != NL_TRUE)
-	{
-		I_Printf("HawkNL: Initialisation FAILED.\n");
-		return;
-	}
-
-	nlHint(NL_REUSE_ADDRESS, NL_TRUE);
-	nlHint(NL_TCP_NO_DELAY,  NL_TRUE);
-
-    if (nlSelectNetwork(NL_IP) != NL_TRUE)
-	{
-		I_Printf("HawkNL: Select Network failed:\n%s", I_NetworkReturnError());
-		nlShutdown();
-		return;
-	}
-
-	I_Printf("HawkNL: Initialisation OK.\n");
-	nonet = false;
-#endif
-}
-
-//
-// I_ShutdownNetwork
-//
-void I_ShutdownNetwork(void)
-{
-	if (! nonet)
-	{
-		nonet = true;
-		SDLNet_Quit();
-	}
-
-#ifdef USE_HAWKNL
-	if (! nonet)
-	{
-		nonet = true;
-		nlShutdown();
-	}
-#endif
-}
-
-//
-// I_NetworkReturnError
-//
-const char *I_NetworkReturnError(void)
-{
-#ifdef USE_HAWKNL
-	int err = nlGetError();
-
-	if (err == NL_NO_ERROR)
-		return "";
-
-	static char err_buf[512];
-
-	if (err == NL_SYSTEM_ERROR)
-	{
-		err = nlGetSystemError();
-		sprintf(err_buf, "(System) %s", nlGetSystemErrorStr(err));
-	}
-	else
-	{
-		sprintf(err_buf, "%s", nlGetErrorStr(err));
-	}
-
-	// remove newlines and other rubbish
-	for (int i = 0; err_buf[i] && (unsigned)i < sizeof(err_buf); i++)
-	{
-		if (! isprint(err_buf[i]))
-			err_buf[i] = ' ';
-	}
-
-	return err_buf;
-
-#else // USE_HAWKNL
-	return "";
-#endif
-}
 
 //
 // Determine IP address of local host via ethernet adapter.
