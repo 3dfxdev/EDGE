c30cda57593b849186c8c83c1c5631290ef4049d
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2007-10-02 10:45:53 +0000

    Added file for Network includes.

---
+++ src/i_netinc.h
@@ -0,0 +1,49 @@
+/*
+    SDL_net:  An example cross-platform network library for use with SDL
+    Copyright (C) 1997-2004 Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
+
+
+/* Include normal system headers */
+#include <errno.h>
+
+
+/* Include system network headers */
+#ifndef WIN32
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#endif
+
+
+/* System-dependent definitions */
+#ifndef WIN32
+#define closesocket	close
+#define SOCKET	int
+#define INVALID_SOCKET	-1
+#define SOCKET_ERROR	-1
+#endif
+
+
