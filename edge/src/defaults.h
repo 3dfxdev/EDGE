d2a224ce50efa30166018d757b258c694adb0be6
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-01-19 11:52:17 +0000

    For Linux, default to using Timidity for music.

--- edge/src/defaults.h
+++ edge/src/defaults.h
@@ -76,12 +76,17 @@
 // Sound and Music
 #define CFGDEF_SOUND_VOLUME     (10)
 #define CFGDEF_MUSIC_VOLUME     (10)
-#define CFGDEF_MUSIC_DEVICE     (0)  // System
 #define CFGDEF_SAMPLE_RATE      (1)  // 22050Hz
 #define CFGDEF_SOUND_BITS       (0)  // 8-bit
 #define CFGDEF_SOUND_STEREO     (1)  // Stereo
 #define CFGDEF_MIX_CHANNELS     (2)  // 32 channels
 #define CFGDEF_QUIET_FACTOR     (1)
+
+#ifdef LINUX
+#define CFGDEF_MUSIC_DEVICE     (1)  // Timidity
+#else
+#define CFGDEF_MUSIC_DEVICE     (0)  // System
+#endif
 
 // Video Options
 #define CFGDEF_CURRENT_GAMMA    (2)
