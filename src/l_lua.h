c5b7427fc21287640ece9038b6a7e5830261c6f7
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-03-06 03:13:20 +0000

    Check-in of preliminary lua hud code.

---
+++ src/l_lua.h
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------
+//  LUA HUD code
+//------------------------------------------------------------------------
+//
+//  Copyright (c) 2006-2008  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __L_LUA_H__
+#define __L_LUA_H__
+
+void LU_Init(void);
+void LU_Close(void);
+
+void LU_LoadScripts(void);
+
+void LU_RunHud(void);
+
+#endif // __L_LUA_H__
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
