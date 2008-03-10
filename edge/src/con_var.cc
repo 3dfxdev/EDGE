895dadb8bd9d0b7a7098b2f33ab27f4f528e2d51
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-03-10 12:47:51 +0000

    Check-in of preliminary new console variable (CVAR) code.

---
+++ edge/src/con_var.cc
@@ -0,0 +1,156 @@
+//----------------------------------------------------------------------------
+//  EDGE Console Variables
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 2007-2008  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+
+#include "con_var.h"
+
+
+cvar_c::cvar_c(int value) : d(value), f(value), str(buffer)
+{
+	sprintf(buffer, "%d", value);
+}
+
+cvar_c::cvar_c(float value) : d(I_ROUND(value)), f(value), str(buffer)
+{
+	DoFloat(value);
+}
+
+cvar_c::cvar_c(const char *value) : str(buffer)
+{
+	DoStr(value);
+}
+
+cvar_c::cvar_c(const cvar_c& other) : str(buffer)
+{
+	DoStr(other.str);
+}
+
+cvar_c::~cvar_c()
+{
+	if (Allocd())
+	{
+		free((void *) str);
+	}
+}
+
+
+cvar_c& cvar_c::operator= (int value)
+{
+	if (Allocd())
+	{
+		free((void *) str);
+	}
+
+	d = value;
+	f = value;
+
+	str = buffer;
+
+	sprintf(buffer, "%d", value);
+
+	return *this;
+}
+
+cvar_c& cvar_c::operator= (float value)
+{
+	if (Allocd())
+	{
+		free((void *) str);
+	}
+
+	d = I_ROUND(value);
+	f = value;
+
+	str = buffer;
+
+	DoFloat(value);
+
+	return *this;
+}
+
+cvar_c& cvar_c::operator= (const char *value)
+{
+	DoStr(value);
+
+	return *this;
+}
+
+cvar_c& cvar_c::operator= (std::string value)
+{
+	DoStr(value.c_str());
+
+	return *this;
+}
+
+cvar_c& cvar_c::operator= (const cvar_c& other)
+{
+	if (&other != this)
+	{
+		DoStr(other.str);
+	}
+
+	return *this;
+}
+
+void cvar_c::DoFloat(float value)
+{
+	float ab = fabs(value);
+
+	if (ab >= 1e5)
+		sprintf(buffer, "%1.1f", value);
+	else if (ab >= 1e3)
+		sprintf(buffer, "%1.3f", value);
+	else if (ab >= 1.0)
+		sprintf(buffer, "%1.5f", value);
+	else
+		sprintf(buffer, "%1.7f", value);
+}
+
+void cvar_c::DoStr(const char *value)
+{
+	if (Allocd())
+	{
+		free((void *) str);
+	}
+
+	int len = strlen(value);
+
+	if (len+1 <= BUFSIZE)
+	{
+		str = buffer;
+
+		strcpy(buffer, value);
+	}
+	else
+	{
+		str = strdup(value);
+	}
+
+	d = atoi(str);
+	f = atof(str);
+}
+
+
+//----------------------------------------------------------------------------
+//  LIST OF ALL CVARS
+//----------------------------------------------------------------------------
+
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ edge/src/con_var.h
@@ -0,0 +1,72 @@
+//----------------------------------------------------------------------------
+//  EDGE Console Variables
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 2007-2008  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __CON_VAR_H__
+#define __CON_VAR_H__
+
+class cvar_c
+{
+public:
+	// current value
+	int d;
+	float f;
+	const char *str;
+
+private:
+	enum bufsize_e { BUFSIZE = 32 };
+
+	// local buffer used for integers, floats and small strings.
+	// in use whenever (s == buffer).  Otherwise s is on the heap,
+	// using strdup() and free().
+	char buffer[BUFSIZE];
+
+public:
+	cvar_c() : d(0), f(0.0f), str(buffer)
+	{
+		buffer[0] = '0';
+		buffer[1] =  0;
+	}
+
+	cvar_c(int value);
+	cvar_c(float value);
+	cvar_c(const char *value);
+	cvar_c(const cvar_c& other);
+
+	~cvar_c();
+
+	cvar_c& operator= (int value);
+	cvar_c& operator= (float value);
+	cvar_c& operator= (const char *value);
+	cvar_c& operator= (std::string value);
+	cvar_c& operator= (const cvar_c& other);
+
+private:
+	inline bool Allocd()
+	{
+		return (str != NULL) && (str != buffer);
+	}
+
+	void DoFloat(float value);
+	void DoStr(const char *value);
+};
+
+
+#endif // __CON_VAR_H__
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
