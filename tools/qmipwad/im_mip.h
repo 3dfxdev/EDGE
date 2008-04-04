33d8ce3864dd200b23abcbe00c29be3e80b82268
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-04-04 08:09:15 +0000

    Added skeletal files: im_mip.cc/h

---
+++ tools/qmipwad/im_mip.h
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------
+//  Basic image storage
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J. Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __IMAGE_MIP_H__
+#define __IMAGE_MIP_H__
+
+
+void MIP_ProcessImage(const char *filename);
+
+#endif  /* __IMAGE_MIP_H__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
