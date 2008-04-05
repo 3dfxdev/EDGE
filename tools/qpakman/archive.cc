8aea3b2e9fad2bfcdead20d3c744e7c3eddaae6e
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-04-05 05:10:41 +0000

    Added skeletal 'archive.cc' code to the build.

---
+++ tools/qpakman/archive.cc
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------
+//  Archiving files to/from a PAK
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J. Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+
+#include <map>
+
+#include "archive.h"
+#include "pakfile.h"
+#include "q1_structs.h"
+
+extern std::vector<std::string> input_names;
+
+extern bool opt_recursive;
+extern bool opt_overwrite;
+
+
+void ARC_CreatePAK(const char *filename)
+{
+  // TODO
+  FatalError("PAK creation not yet implemented\n");
+}
+
+
+void ARC_ExtractPAK(const char *filename)
+{
+  // TODO
+  FatalError("PAK extraction not yet implemented\n");
+}
+
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
---
+++ tools/qpakman/archive.h
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------
+//  Archiving files to/from a PAK
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J. Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __ARCHIVE_BITS_H__
+#define __ARCHIVE_BITS_H__
+
+void ARC_CreatePAK(const char *filename);
+void ARC_ExtractPAK(const char *filename);
+
+#endif  /* __ARCHIVE_BITS_H__ */
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
