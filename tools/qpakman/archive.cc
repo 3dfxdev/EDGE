5f01cabedf9aa79abf4bb60f76c1e410107cfbf4
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-04-20 13:58:23 +0000

    Implemented the PAK extraction code.

--- tools/qpakman/archive.cc
+++ tools/qpakman/archive.cc
@@ -37,12 +37,104 @@ void ARC_CreatePAK(const char *filename)
 }
 
 
-void ARC_ExtractPAK(const char *filename)
+//------------------------------------------------------------------------
+
+bool ARC_ExtractOneFile(int entry, const char *name)
 {
-  // TODO: ARC_ExtractPAK
-  FatalError("PAK extraction not yet implemented\n");
+  int entry_len = PAK_EntryLen(entry);
+
+
+  // FIXME: validate/sanitize entry name
+  const char * filename = StringDup(name);
+
+
+  if (FileExists(filename) && ! opt_overwrite)
+  {
+    printf("FAILURE: will not overwrite file: %s\n\n", filename);
+
+    StringFree(filename);
+    return false;
+  }
+
+
+  bool failed = false;
+
+  FILE *fp = fopen(filename, "wb");
+  if (fp)
+  {
+    static byte buffer[2048];
+
+    // transfer data from PAK into new file
+    int position = 0;
+
+    while (position < entry_len)
+    {
+      int count = MIN((int)sizeof(buffer), entry_len - position);
+
+      if (! WAD2_ReadData(entry, position, count, buffer))
+      {
+        printf("FAILURE: read error on %d bytes\n\n", count);
+        failed = true;
+        break;
+      }
+
+      if (1 != fwrite(buffer, count, 1, fp))
+      {
+        printf("FAILURE: write error on %d bytes\n\n", count);
+        failed = true;
+        break;
+      }
+
+      position += count;
+    }
+
+    fclose(fp);
+  }
+  else
+  {
+    printf("FAILURE: cannot create output file: %s\n\n", filename);
+    failed = true;
+  }
+
+  StringFree(filename);
+
+  return ! failed;
 }
 
+
+void ARC_ExtractPAK(const char *filename)
+{
+  if (! PAK_OpenRead(filename))
+    FatalError("Cannot open WAD2 file: %s\n", filename);
+
+  printf("\n");
+  printf("--------------------------------------------------\n");
+
+  int num_files = PAK_NumEntries();
+
+  int skipped  = 0;
+  int failures = 0;
+
+  for (int i = 0; i < num_files; i++)
+  {
+    const char *name = PAK_EntryName(i);
+
+    printf("Unpacking entry %d/%d : %s\n", i+1, num_files, name);
+
+    if (! ARC_ExtractOneFile(i, name))
+      failures++;
+  }
+
+  printf("\n");
+  printf("--------------------------------------------------\n");
+
+  PAK_CloseRead();
+
+// printf("Skipped %d entries\n", skipped);
+
+  printf("Extracted %d entries, with %d failures\n",
+         num_files - failures - skipped, failures);
+}
 
 //--- editor settings ---
 // vi:ts=2:sw=2:expandtab
