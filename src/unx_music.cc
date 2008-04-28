9ef474016309bf1a57a3b4dd9f3ff0f837732151
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-04-28 09:40:41 +0000

    Dead code removal from platform-specific music handling.

--- src/unx_music.cc
+++ src/unx_music.cc
@@ -25,43 +25,11 @@
 #include "ddf/main.h"
 #include "ddf/playlist.h"
 
-
-// FIXME !!!!!! temporary
-#undef USE_OGG
-#undef USE_HUMID
-
 #include "s_sound.h"
 #include "s_music.h"
 #include "s_timid.h"
 #include "unx_sysinc.h"
 
-
-// #defines for handle information
-#define GETLIBHANDLE(_handle) (_handle&0xFF)
-#define GETLOOPBIT(_handle)   ((_handle&0x10000)>>16)
-#define GETTYPE(_handle)      ((_handle&0xFF00)>>8)
-
-#define MAKEHANDLE(_type,_loopbit,_libhandle) \
-	(((_loopbit&1)<<16)+(((_type)&0xFF)<<8)+(_libhandle))
-
-typedef enum
-{
-	support_CD   = 0x01,
-	support_MIDI = 0x02,
-	support_MUS  = 0x04,
-	support_OGG  = 0x08  // OGG Support - ACB- 2004/08/18
-}
-mussupport_e;
-
-static byte capable;
-
-#ifdef USE_OGG
-oggplayer_c *oggplayer = NULL;
-#endif
-
-#ifdef USE_HUMID
-humdinger_c *humdinger = NULL;
-#endif
 
 #define MUSICERRLEN 256
 static char errordesc[MUSICERRLEN];
@@ -80,30 +48,12 @@ void I_StartupMusic(void)
 
 	if (nomusic) return;
 
-	capable = 0;
-
-#ifdef USE_OGG
-	if (! nosound)
-	{
-		oggplayer = new oggplayer_c;
-		capable |= support_OGG;
-
-		I_Printf("I_StartupMusic: OGG Music Init OK\n");
-	}
-	else
-#endif // USE_OGG
-    {
-		I_Printf("I_StartupMusic: OGG Music Disabled\n");
-    }
-
-
 #if 1
 	if (! nosound)
 	{
 		if (S_StartupTimidity())
 		{
 			I_Printf("I_StartupMusic: Timidity Init OK\n");
-			capable |= support_MUS | support_MIDI;
 		}
 		else
 		{
@@ -123,128 +73,9 @@ void I_StartupMusic(void)
 	return;
 }
 
-#if 0
-//
-// I_MusicPlayback
-//
-int I_MusicPlayback(i_music_info_t *musdat, int type, bool looping,
-	float gain)
-{
-	int handle;
-
-	if (!(capable & support_CD)   && type == MUS_CD)   return -1;
-	if (!(capable & support_MIDI) && type == MUS_MIDI) return -1;
-	if (!(capable & support_MUS)  && type == MUS_MUS)  return -1;
-	if (!(capable & support_OGG)  && type == MUS_OGG)  return -1;
-
-	switch (type)
-	{
-		// CD Support...
-		case MUS_CD:
-		{
-			if (! I_CDStartPlayback(musdat->info.cd.track, looping, gain))
-				handle = -1;
-			else
-			{
-				handle = MAKEHANDLE(MUS_CD, looping, musdat->info.cd.track);
-			}
-			break;
-		}
-
-		case MUS_MUS:
-		case MUS_MIDI:
-		{
-#ifdef USE_HUMID
-			handle = -1;
-
-            if (humdinger)
-            {
-				int track = humdinger->Open((byte*)musdat->info.data.ptr,
-											musdat->info.data.size);
-				
-				if (track == -1)
-				{
-					handle = -1;
-				}
-				else
-				{
-					humdinger->Play(looping, gain);
-					handle = MAKEHANDLE(type, looping, track);
-				}
-			}
-#else // !USE_HUMID
-			I_PostMusicError("I_MusicPlayback: MUS/MIDI not supported.\n");
-			handle = -1;
-#endif
-			break;
-		}
-
-		case MUS_OGG:
-		{
-#ifdef USE_OGG
-            handle = -1;
-
-            if (musdat->format != IMUSSF_DATA &&
-                musdat->format != IMUSSF_FILE)
-            {
-                I_PostMusicError("I_MusicPlayback: OGG given in unsupported format.\n");
-                break;
-            }
-
-            if (musdat->format == IMUSSF_DATA)
-				oggplayer->Open(musdat->info.data.ptr, musdat->info.data.size);
-			else if (musdat->format == IMUSSF_FILE)
-				oggplayer->Open(musdat->info.file.name);
-
-			oggplayer->Play(looping, gain);
-			handle = MAKEHANDLE(MUS_OGG, looping, 1);
-#else // !USE_OGG
-			I_PostMusicError("I_MusicPlayback: OGG-Vorbis not supported.\n");
-			handle = -1;
-#endif
-			break;
-		}
-
-		case MUS_UNKNOWN:
-		{
-			I_PostMusicError("I_MusicPlayback: Unknown format type given.\n");
-			handle = -1;
-			break;
-		}
-
-		default:
-		{
-			I_PostMusicError("I_MusicPlayback: Weird Format given.\n");
-			handle = -1;
-			break;
-		}
-	}
-
-	return handle;
-}
-#endif
-
 
 void I_ShutdownMusic(void)
 {
-#ifdef USE_OGG
-	if (oggplayer)
-	{
-		delete oggplayer;
-		oggplayer = NULL;
-	}
-#endif
-
-#ifdef USE_HUMID
-	if (humdinger)
-	{
-		delete humdinger;
-		humdinger = NULL;
-
-		HumDingerTerm();
-	}
-#endif
-
 }
 
 
