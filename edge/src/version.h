89add5f390dd76107e448316bfe3b0fd9ed47df5
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-06-25 09:03:58 +0000

    Bumped patchver to 8 for RC#1.

--- edge/src/version.h
+++ edge/src/version.h
@@ -23,7 +23,7 @@
 #define EDGEVERSTR  "1.31-RC#1"
 
 // patch level (Savegames and Demos)
-#define EDGEPATCH  7
+#define EDGEPATCH  8
 
 // -ES- 2000/03/04 The version of EDGE.WAD we require.
 #define EDGE_WAD_VERSION 5
