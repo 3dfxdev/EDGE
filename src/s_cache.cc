dd7702797d52edf2b75cdf6fbacf6b0c65d90f3c
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-06-25 06:17:56 +0000

    Fixed a problem with loading certain OGG sounds, and made the OGG loader a bit more tolerant (less I_Error).

--- src/s_cache.cc
+++ src/s_cache.cc
@@ -129,6 +129,8 @@ void S_CacheClearAll(void)
 
 static bool DoCacheLoad(sfxdef_c *def, epi::sound_data_c *buf)
 {
+	I_Debugf("S_CacheLoad: [%s]\n", def->ddf.name.c_str());
+
 	// open the file or lump, and read it into memory
 	epi::file_c *F;
 
