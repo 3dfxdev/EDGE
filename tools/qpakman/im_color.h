f97bb1ca71b842418e0bf7b59555e45e62310ca1
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-08-25 09:50:28 +0000

    Moved color/palette stuff from im_mip.cc --> im_color.cc/h

---
+++ tools/qpakman/im_color.h
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------
+//  Color Management
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __IMAGE_COLOR_H__
+#define __IMAGE_COLOR_H__
+
+void COL_SetPalette(int game_type);
+void COL_SetFullBright(bool enable);
+
+byte COL_FindColor(const byte *palette, u32_t rgb_col);
+byte COL_MapColor(u32_t rgb_col);
+
+#endif  /* __IMAGE_COLOR_H__ */
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
