480c81833fab8b8f525c244fdaab64ae0bf4ba6b
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-10-04 11:36:10 +0000

    Added code for the "world" module [courtesy Andrew Baker].

---
+++ edge/src/p_vm.h
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------
+//  LUA Play Simulation Interface Header
+//------------------------------------------------------------------------
+//
+//  Copyright (c) 2006-2008  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+#ifndef __P_LUA_VM__
+#define __P_LUA_VM__
+
+void P_RegisterModules(void);
+void P_SetupPlayerModule(int player);
+
+#endif /* __P_LUA_VM__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
