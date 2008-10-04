9d8bcb4e8b26bf3417e0b3162d8792464fa2b9a4
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-10-04 11:38:52 +0000

    Preliminary work on class-ification of the Lua VM (based on Andrew Baker's patch).
    Moved FrameSetup() function into hu_vm.c

--- src/l_lua.h
+++ src/l_lua.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------
-//  LUA HUD code
+//  LUA General Stuff
 //------------------------------------------------------------------------
 //
 //  Copyright (c) 2006-2008  The EDGE Team.
@@ -19,13 +19,50 @@
 #ifndef __L_LUA_H__
 #define __L_LUA_H__
 
-void LU_Init(void);
-void LU_Close(void);
+// forward definitions
+struct lua_State;
+struct luaL_Reg;
 
-void LU_LoadScripts(void);
+// LUA VM Instance
+class lua_vm_c
+{
+public:
+    struct var_s
+    {
+        const char *name;
+        int type;
+        int value;
+    };
+    
+private:
+    lua_State *state;
 
-void LU_BeginLevel(void);
-void LU_RunHud(void);
+public:
+     lua_vm_c();
+    ~lua_vm_c();
+
+public:
+    void Open();
+    void CallFunction(const char *parent_name, const char *name);
+    lua_State* GetState() { return this->state; } 
+    void LoadBuffer(const char *name, char *buffer, int length);
+    void LoadModule(const char *name, const luaL_Reg *funcs);
+    std::string RunCommand(const char *cmd);
+
+    void SetIntVar(const char *table, const char *name, int value);
+    void SetBoolVar(const char *table, const char *name, int value);
+    void SetStringVar(const char *table, const char *name, const char * value);
+
+private:
+	static int init_lua(lua_State *L);
+	static void open_basic_libs(lua_State *L);
+	static void remove_loaders(lua_State *L);
+};
+
+extern lua_vm_c *vm; // The instance
+
+void LU_Init();
+void LU_Close();
 
 #endif // __L_LUA_H__
 
