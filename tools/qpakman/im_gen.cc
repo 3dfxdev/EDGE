d72bb8feb4a266037c3f98df95de2060fc00aa6c
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-10-23 11:26:29 +0000

    Added framework for generating special output files.

---
+++ tools/qpakman/im_gen.cc
@@ -0,0 +1,41 @@
+//------------------------------------------------------------------------
+//  Generation of Special files
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+#include "main.h"
+
+#include <map>
+#include <algorithm>
+
+#include "pakfile.h"
+#include "im_gen.h"
+#include "im_mip.h"
+#include "q1_structs.h"
+
+
+
+//------------------------------------------------------------------------
+
+bool GEN_TryCreateSpecial(const char *filename)
+{
+  // TODO GEN_TryCreateSpecial
+  return false;
+}
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
---
+++ tools/qpakman/im_gen.h
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------
+//  Generation of Special files
+//------------------------------------------------------------------------
+// 
+//  Copyright (c) 2008  Andrew J Apted
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __GENERATE_SPECIAL_H__
+#define __GENERATE_SPECIAL_H__
+
+bool GEN_TryCreateSpecial(const char *filename);
+
+#endif  /* __GENERATE_SPECIAL_H__ */
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
