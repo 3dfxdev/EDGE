5fb9a3079c715d7c68f453c4be77975f787747db
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2008-10-23 11:57:00 +0000

    New code to generate HexenII's inverse palette: "invpal.lmp"

--- tools/qpakman/im_gen.cc
+++ tools/qpakman/im_gen.cc
@@ -23,17 +23,62 @@
 #include <algorithm>
 
 #include "pakfile.h"
+#include "im_color.h"
+#include "im_image.h"
 #include "im_gen.h"
 #include "im_mip.h"
 #include "q1_structs.h"
 
+
+static void GEN_InversePalette(const char *filename)
+{
+  printf("Generating inverse palette: %s\n", filename);
+
+  FILE *fp = fopen(filename, "wb");
+  if (! fp)
+    FatalError("Cannot create file! %s\n", strerror(errno));
+
+  COL_SetFullBright(true);
+  COL_SetTransparent(255);
+
+  char buffer[64];
+
+  for (int r = 0; r < 64; r++)
+  for (int g = 0; g < 64; g++)
+  {
+    for (int b = 0; b < 64; b++)
+    {
+      buffer[b] = COL_MapColor(MAKE_RGB(r<<2, g<<2, b<<2));
+    }
+
+    if (1 != fwrite(buffer, sizeof(buffer), 1, fp))
+    {
+      fclose(fp);
+      FatalError("Error writing file!\n");
+    }
+  }
+
+  fclose(fp);
+
+  printf("Finished.\n");
+}
 
 
 //------------------------------------------------------------------------
 
 bool GEN_TryCreateSpecial(const char *filename)
 {
-  // TODO GEN_TryCreateSpecial
+  if (StringCaseCmp(FindBaseName(filename), "invpal.lmp") == 0)
+  {
+    GEN_InversePalette(filename);
+    return true;
+  }
+
+  // 16to8.dat
+
+  // tinttab.lmp
+  // tinttab2.lmp
+
   return false;
 }
 
