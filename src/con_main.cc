c7d2a4245f76477f386b99d81a0ec98b015bd5ad
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-06-04 07:24:18 +0000

    Console: Removed some functions: CMD_Eat, CMD_Set, CMD_TypeOf, and rewrote the GetArgs() implementation.

--- src/con_main.cc
+++ src/con_main.cc
@@ -41,7 +41,7 @@
 
 
 
-int CMD_Exec(const char **argv, int argc)
+int CMD_Exec(char **argv, int argc)
 {
 	FILE *script;
 	char buffer[SCREENCOLS];
@@ -68,7 +68,7 @@ int CMD_Exec(const char **argv, int argc)
 	return 0;
 }
 
-int CMD_Type(const char **argv, int argc)
+int CMD_Type(char **argv, int argc)
 {
 	FILE *script;
 	char buffer[SCREENCOLS];
@@ -94,7 +94,7 @@ int CMD_Type(const char **argv, int argc)
 	return 0;
 }
 
-int CMD_DumpArgs(const char **argv, int argc)
+int CMD_DumpArgs(char **argv, int argc)
 {
 	I_Printf("Arguments:\n");
 
@@ -104,154 +104,14 @@ int CMD_DumpArgs(const char **argv, int argc)
 	return 0;
 }
 
-//
-// Eats memory.
-//
-int CMD_Eat(const char **argv, int argc)
-{
-	int bytes;
-	static char *p = NULL;
-
-	if (argc != 2 || 1 != sscanf(argv[1], "%d", &bytes))
-	{
-		CON_Printf("Eat memory. Usage: eat <size>\n");
-		return 2;
-	}
-
-	if (!bytes)
-	{
-		Z_Free(p);
-		p = NULL;
-	}
-	else
-		Z_Resize(p, char, bytes);
-
-	return 0;
-}
-
-int CMD_ScreenShot(const char **argv, int argc)
+int CMD_ScreenShot(char **argv, int argc)
 {
 	G_DeferredScreenShot();
 
 	return 0;
 }
 
-int CMD_Set(const char **argv, int argc)
-{
-	cvar_t *var;
-	// temp argv: Just to handle 'const' keyword properly
-	char *tmpargv[3];
-	int i;
-	int e = 0;
-	char buf[1025];
-
-	for (i = 0; i < argc; i++)
-		argv[i] = tmpargv[i];
-
-	switch (argc)
-	{
-	case 2:
-	case 1:
-		for (i = 0; i < num_cvars; i++)
-		{
-			if (argc == 2)
-				if (stricmp(argv[1], cvars[i]->name))
-					continue;
-			if (cvars[i]->flags & cf_read)
-			{
-				cvars[i]->type->get_value_str(cvars[i]->value, buf);
-				CON_Printf("%s = %s\n", cvars[i]->name, buf);
-			}
-			else
-				CON_Printf("%s (Write Only)\n", cvars[i]->name);
-			if (argc == 2)
-				break;
-		}
-		if (i == num_cvars && argc == 2)
-		{  // not found
-
-			CON_Printf("No cvar '%s'!\n", argv[1]);
-			e = 3;
-		}
-		break;
-	default:
-		var = CON_CVarPtrFromName(argv[1]);
-		if (var)
-		{
-			if (!strlen(argv[2]) && var->flags & cf_delete)
-				CON_DeleteCVar(argv[1]);
-			else
-			{
-				if (var->flags & cf_write)
-				{
-					if (!var->type->set_value(var, argc - 2, &argv[2]))
-						e = 1, CON_Printf("\"%s\" is not a valid value for type %s\n", argv[2], var->type->get_name(var));
-				}
-				else
-					e = 2, CON_Printf("\"%s\" is read only\n", var->name);
-			}
-		}
-		else
-		{
-			if (argv[2][0] >= '0' && argv[2][0] <= '9')
-			{
-				int *x = Z_New(int, 1);
-
-				*x = strtol(argv[2], NULL, 0);
-				CON_CreateCVarInt(argv[1], (cflag_t) (cf_delete|cf_mem|cf_normal), x);
-			}
-			else if (!strcmp(argv[2], "true") || !strcmp(argv[2], "false"))
-			{
-				bool *b = Z_New(bool, 1);
-
-				*b = !strcmp(argv[2], "true");
-				CON_CreateCVarBool(argv[1], (cflag_t) (cf_delete|cf_mem|cf_normal), b);
-			}
-			else
-			{
-				char *s = Z_New(char, 256);
-
-				strcpy(s, argv[2]);
-				CON_CreateCVarStr(argv[1], (cflag_t) (cf_delete|cf_mem|cf_normal), s, 255);
-			}
-		}
-		break;
-	}
-
-	return e;
-}
-
-int CMD_TypeOf(const char **argv, int argc)
-{
-	cvar_t *v;
-	int e;
-
-	if (argc < 2)
-	{
-		CON_Printf("Usage: Typeof <cvar>\n");
-		e = 1;
-	}
-	else
-	{
-		v = CON_CVarPtrFromName(argv[1]);
-
-		if (v)
-		{
-			CON_Printf("The type of \"%s\" is \"%s\"\n", v->name, v->type->get_name(v));
-			e = 0;
-		}
-		else
-		{
-			CON_Printf("No console variable called \"%s\"!\n", argv[1]);
-			e = 2;
-		}
-	}
-
-	return e;
-}
-
-
-int CMD_QuitEDGE(const char **argv, int argc)
+int CMD_QuitEDGE(char **argv, int argc)
 {
 	M_QuitEDGE(0);
 
@@ -259,7 +119,7 @@ int CMD_QuitEDGE(const char **argv, int argc)
 }
 
 
-int CMD_Crc(const char **argv, int argc)
+int CMD_Crc(char **argv, int argc)
 {
 	int lump, length;
 	const byte *data;
@@ -294,7 +154,7 @@ int CMD_Crc(const char **argv, int argc)
 	return 0;
 }
 
-int CMD_PlaySound(const char **argv, int argc)
+int CMD_PlaySound(char **argv, int argc)
 {
 	sfx_t *sfx;
 
@@ -317,13 +177,13 @@ int CMD_PlaySound(const char **argv, int argc)
 	return 0;
 }
 
-int CMD_ShowFiles(const char **argv, int argc)
+int CMD_ShowFiles(char **argv, int argc)
 {
 	W_ShowFiles();
 	return 0;
 }
 
-int CMD_ShowLumps(const char **argv, int argc)
+int CMD_ShowLumps(char **argv, int argc)
 {
 	int for_file = -1;  // all files
 
@@ -334,19 +194,16 @@ int CMD_ShowLumps(const char **argv, int argc)
 
 	if (argc >= 3)
 	{
-		match = strdup(argv[2]);
+		match = argv[2];
 		strupr(match);
 	}
 
 	W_ShowLumps(for_file, match);
 
-	if (match)
-		free(match);
-
 	return 0;
 }
 
-int CMD_ShowVars(const char **argv, int argc)
+int CMD_ShowVars(char **argv, int argc)
 {
 	I_Printf("All console vars:\n");
 
@@ -363,66 +220,60 @@ int CMD_ShowVars(const char **argv, int argc)
 
 //----------------------------------------------------------------------------
 
+static char *StrDup(const char *start, int len)
+{
+	char *buf = new char[len + 2];
+
+	memcpy(buf, start, len);
+	buf[len] = 0;
+
+	return buf;
+}
 
 static int GetArgs(const char *line, char **argv, int max_argc)
 {
-	while (isspace(*line))
-		line++;
+	int argc = 0;
 
-	int i, j, k = 0, m;
-	const int len = (const int)strlen(line);
-	char *s;
-	int quote;
-
-	// skip any leading spaces
-	for (i = 0; args[i] == ' '; i++)
-		;
-
-	for (; i < len; i = j)
+	for (;;)
 	{
-		// find end of arg
-		if (args[i] == '\"')
-		{
-			quote = 1;
-			i++;
-			for (j = i; args[j] != '\"' && j < len; j++)
-				;
-		} else
-		{
-			quote = 0;
-			for (j = i; args[j] != ' ' && j < len; j++)
-				;
-		}
+		while (isspace(*line))
+			line++;
 
-		s = Z_New(char, j - i + 1);
-		for (m = 0; m < j - i; m++)
-		{
-			// -ES- 1999/07/25 Convert to lowercase, to avoid case sensitivity
-			s[m] = tolower(args[i + m]);
-		}
-		s[j - i] = '\0';
-
-		argv[k] = s;
-
-		if (++k == argc)
+		if (! *line)
 			break;
 
-		if (quote)
-			// skip the ending quote
-			j++;
+		// silent truncation (bad?)
+		if (argc >= max_argc)
+			break;
 
-		// skip whitespace until the next arg
-		while (args[j] == ' ' && j < len)
-			j++;
+		const char *start = line;
+
+		if (*line == '"')
+		{
+			start++; line++;
+
+			while (*line && *line != '"')
+				line++;
+		}
+		else
+		{
+			while (*line && !isspace(*line))
+				line++;
+		}
+
+		argv[argc++] = StrDup(start, line - start);
+
+		if (*line)
+			line++;
 	}
 
-	return k;
+	return argc;
 }
 
 static void KillArgs(char **argv, int argc)
 {
 	for (int i = 0; i < argc; i++)
-		free(argv[i]);
+		delete[] argv[i];
 }
 
 
@@ -430,7 +281,7 @@ typedef struct
 {
 	const char *name;
 
-	int (* func) (const char **argv, int argc);
+	int (* func) (char **argv, int argc);
 }
 con_cmd_t;
 
@@ -442,15 +293,12 @@ static const con_cmd_t builtin_commands[] =
 	{ "args",           CMD_DumpArgs },
 	{ "crc",            CMD_Crc },
 	{ "playsound",      CMD_PlaySound },
-	{ "eat",            CMD_Eat },
 	{ "exec",           CMD_Exec },
 	{ "show_files",     CMD_ShowFiles },
 	{ "show_lumps",     CMD_ShowLumps },
 	{ "show_vars",      CMD_ShowVars },
 	{ "screenshot",     CMD_ScreenShot },
-	{ "set",            CMD_Set },
 	{ "type",           CMD_Type },
-	{ "typeof",         CMD_TypeOf },
 	{ "quit",           CMD_QuitEDGE },
 	{ "exit",           CMD_QuitEDGE },
 
@@ -478,11 +326,11 @@ void CON_TryCommand(const char *cmd)
 	if (argc == 0)
 		return;
 
-	int cmd = FindCommand(argv[0]);
+	int index = FindCommand(argv[0]);
 
-	if (cmd >= 0)
+	if (index >= 0)
 	{
-		int err = (* builtin_commands[cmd].func)(argv, argc);
+		int err = (* builtin_commands[index].func)(argv, argc);
 	}
 	else
 	{
