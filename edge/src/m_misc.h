1b21e82924c9f415eb1980241c296329e1dd8a95
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-06-08 14:19:53 +0000

    Added 's_mixchannels' cvar.

--- edge/src/m_misc.h
+++ edge/src/m_misc.h
@@ -83,7 +83,6 @@ extern cvar_c m_obituaries;
 extern int var_sample_rate;
 extern int var_sound_bits;
 extern int var_sound_stereo;
-extern int var_mix_channels;
 extern int var_quiet_factor;
 extern int var_timid_factor;
 
