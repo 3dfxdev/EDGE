4ef3d8c2503073695d8159ceec527e3692c87e95
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-06-17 03:13:53 +0000

    Renamed cvar: s_timidity --> s_musicdevice.
    Reformatted the con_link table.

--- src/con_link.cc
+++ src/con_link.cc
@@ -50,16 +50,16 @@ extern cvar_c r_dynamiclight, r_invultex;
 extern cvar_c r_gamma, r_detaillevel;
 extern cvar_c r_wipemethod, r_wipereverse;
 
-extern cvar_c s_volume, s_musicvol, s_mixchan;
+extern cvar_c s_volume, s_mixchan, s_quietfactor;
 extern cvar_c s_rate, s_bits, s_stereo;
-extern cvar_c s_quietfactor, s_timidity;
+extern cvar_c s_musicvol, s_musicdevice;
 extern cvar_c tim_quietfactor;
 
 
-#ifdef LINUX
-#define S_TIMIDITY_CFG  "1"
+#ifdef WIN32
+#define S_MUSICDEV_CFG  "0"  // hardware
 #else
-#define S_TIMIDITY_CFG  "0"
+#define S_MUSICDEV_CFG  "1"  // timidity
 #endif
 
 
@@ -88,57 +88,57 @@ cvar_link_t  all_cvars[] =
     { "g_weaponkick",   &g_weaponkick,   "c",   "0"  },
     { "g_weaponswitch", &g_weaponswitch, "c",   "1"  },
 
-	{ "am_rotate",    &am_rotate,    "c",   "0"  },
-	{ "am_smoothing", &am_smoothing, "c",   "1"  },
+	{ "am_rotate",      &am_rotate,      "c",   "0"  },
+	{ "am_smoothing",   &am_smoothing,   "c",   "1"  },
+                                        
+	{ "in_autorun",     &in_autorun,     "c",   "0"  },
+                                        
+	{ "m_diskicon",     &m_diskicon,     "c",   "1"  },
+	{ "m_busywait",     &m_busywait,     "c",   "1"  },
+	{ "m_messages",     &m_messages,     "c",   "1"  },
+	{ "m_obituaries",   &m_obituaries,   "c",   "1"  },
+	{ "m_screenhud",    &m_screenhud,    "c",   "0"  },
+                                        
+	{ "r_width",        &r_width,        "rc",  "640"   },
+	{ "r_height",       &r_height,       "rc",  "480"   },
+    { "r_depth",        &r_depth,        "rc",  "32"    },
+    { "r_fullscreen",   &r_fullscreen,   "rc",  "1"     },
+												
+	{ "r_gamma",        &r_gamma,        "c",   "1"  },
+	{ "r_nearclip",     &r_nearclip,     "c",   "4"  },
+	{ "r_farclip",      &r_farclip,      "c",   "64000" },
+	{ "r_fadepower",    &r_fadepower,    "c",   "1"  },
+	{ "r_fov",          &r_fov,          "c",   "90" },
+	{ "r_zoomedfov",    &r_zoomedfov,    "c",   "10" },
+                                        
+	{ "r_mipmapping",   &r_mipmapping,   "c",   "0"  },
+	{ "r_smoothing",    &r_smoothing,    "c",   "0"  },
+	{ "r_dithering",    &r_dithering,    "c",   "0"  },
+	{ "r_hq2x",         &r_hq2x,         "c",   "1"  },
 
-	{ "in_autorun",   &in_autorun,   "c",   "0"  },
+	{ "r_dynamiclight", &r_dynamiclight, "c",   "1"  },
+	{ "r_detaillevel",  &r_detaillevel,  "c",   "1"  },
+	{ "r_invultex",     &r_invultex,     "c",   "1"  },
+	{ "r_wipemethod",   &r_wipemethod,   "c",   "1" /* Melt */ },
+	{ "r_wipereverse",  &r_wipereverse,  "c",   "0"  },
 
-	{ "m_diskicon",   &m_diskicon,   "c",   "1"  },
-	{ "m_busywait",   &m_busywait,   "c",   "1"  },
-	{ "m_messages",   &m_messages,   "c",   "1"  },
-	{ "m_obituaries", &m_obituaries, "c",   "1"  },
-	{ "m_screenhud",  &m_screenhud,  "c",   "0"  },
+	{ "r_colormaterial",&r_colormaterial,"",    "1"  },
+	{ "r_colorlighting",&r_colorlighting,"",    "1"  },
+	{ "r_dumbsky",      &r_dumbsky,      "",    "0"  },
+	{ "r_dumbmulti",    &r_dumbmulti,    "",    "0"  },
+	{ "r_dumbcombine",  &r_dumbcombine,  "",    "0"  },
+	{ "r_dumbclamp",    &r_dumbclamp,    "",    "0"  },
 
-	{ "r_width",      &r_width,      "rc",  "640"   },
-	{ "r_height",     &r_height,     "rc",  "480"   },
-    { "r_depth",      &r_depth,      "rc",  "32"    },
-    { "r_fullscreen", &r_fullscreen, "rc",  "1"     },
+	{ "s_volume",       &s_volume,       "c",   "0.5"  },
+	{ "s_mixchan",      &s_mixchan,      "c",   "32"   },
+	{ "s_rate",         &s_rate,         "c",   "22050" },
+	{ "s_bits",         &s_bits,         "c",   "16" },
+	{ "s_stereo",       &s_stereo,       "c",   "1"  },
+	{ "s_musicvol",     &s_musicvol,     "c",   "0.5"  },
+	{ "s_musicdevice",  &s_musicdevice,  "c",   S_MUSICDEV_CFG  },
 
-	{ "r_gamma",      &r_gamma,      "c",   "1"  },
-	{ "r_nearclip",   &r_nearclip,   "c",   "4"     },
-	{ "r_farclip",    &r_farclip,    "c",   "64000" },
-	{ "r_fadepower",  &r_fadepower,  "c",   "1",    },
-	{ "r_fov",        &r_fov,        "c",   "90",   },
-	{ "r_zoomedfov",  &r_zoomedfov,  "c",   "10",   },
-
-	{ "r_mipmapping", &r_mipmapping, "c",   "0"  },
-	{ "r_smoothing",  &r_smoothing,  "c",   "0"  },
-	{ "r_dithering",  &r_dithering,  "c",   "0"  },
-	{ "r_hq2x",       &r_hq2x,       "c",   "1"  },
-
-	{ "r_dynamiclight",  &r_dynamiclight,  "c",  "1"  },
-	{ "r_detaillevel",   &r_detaillevel,   "c",  "1"  },
-	{ "r_invultex",      &r_invultex,      "c",  "1"  },
-	{ "r_wipemethod",    &r_wipemethod,    "c",  "1" /* Melt */ },
-	{ "r_wipereverse",   &r_wipereverse,   "c",  "0"  },
-
-	{ "r_colormaterial", &r_colormaterial, "",   "1"  },
-	{ "r_colorlighting", &r_colorlighting, "",   "1"  },
-	{ "r_dumbsky",       &r_dumbsky,       "",   "0"  },
-	{ "r_dumbmulti",     &r_dumbmulti,     "",   "0"  },
-	{ "r_dumbcombine",   &r_dumbcombine,   "",   "0"  },
-	{ "r_dumbclamp",     &r_dumbclamp,     "",   "0"  },
-
-	{ "s_volume",     &s_volume,     "c",   "0.5"  },
-	{ "s_musicvol",   &s_musicvol,   "c",   "0.5"  },
-	{ "s_mixchan",    &s_mixchan,    "c",   "32"   },
-	{ "s_rate",       &s_rate,       "c",   "22050" },
-	{ "s_bits",       &s_bits,       "c",   "16" },
-	{ "s_stereo",     &s_stereo,     "c",   "1"  },
-	{ "s_timidity",   &s_timidity,   "c",   S_TIMIDITY_CFG  },
-
-	{ "s_quietfactor",   &s_quietfactor,   "c",  "1"  },
-	{ "tim_quietfactor", &tim_quietfactor, "c",  "1"  },
+	{ "s_quietfactor",  &s_quietfactor,  "c",   "1"  },
+	{ "tim_quietfactor",&tim_quietfactor,"c",   "1"  },
 
 	{ "debug_nomonsters", &debug_nomonsters, "h", "0" },
 	{ "debug_fullbright", &debug_fullbright, "h", "0" },
