0c34f79f611122c1f967dcec31ba327b9bc929cd
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-09 11:58:06 +0000

    Coal: yet more stuff moved to local header.

--- coal/coal.h
+++ coal/coal.h
@@ -17,18 +17,18 @@
     See file, 'COPYING', for details.
 */
 
-#ifndef __QCC_H__
-#define __QCC_H__
+#ifndef __COAL_API_H__
+#define __COAL_API_H__
 
 
-// COMPILER:
+// === COMPILER ==================================
 
 void PR_InitData(void);
 void PR_BeginCompilation(void);
 bool PR_FinishCompilation(void);
 void PR_ShowStats(void);
 
-// EXECUTOR:
+// === VM ========================================
 
 void PR_SetTrace(bool enable);
 func_t PR_FindFunction(const char *func_name);
@@ -37,25 +37,10 @@ int PR_ExecuteProgram(func_t fnum);
 
 //============================================================================
 
-#define	MAX_ERRORS		20
-
-#define	MAX_REGS		16384
-
-//============================================================================
-
-
-extern def_t *pr_global_defs[MAX_REGS];	// to find def for a global variable
-
-void PR_PrintStatement(statement_t *s);
-
-
 #define	OFS_NULL		0
 #define	OFS_RETURN		1
 
 #define	RESERVED_OFS	10
-
-
-void PR_PrintDefs(void);
 
 
 #define	G_FLOAT(o) (pr_globals[o])
@@ -63,38 +48,11 @@ void PR_PrintDefs(void);
 #define	G_STRING(o) (strings + (int)pr_globals[o])
 #define	G_FUNCTION(o) (pr_globals[o])
 
-
 bool PR_CompileFile(char *string, char *filename);
-
-
-//=============================================================================
-
-#define	MAX_STRINGS		500000
-#define	MAX_GLOBALS		16384
-#define	MAX_FIELDS		1024
-#define	MAX_STATEMENTS	65536
-#define	MAX_FUNCTIONS	8192
-
-#define	MAX_DATA_PATH	64
-
-extern	char	strings[MAX_STRINGS];
-extern	int		strofs;
-
-extern	statement_t	statements[MAX_STATEMENTS];
-extern	int			numstatements;
-extern	int			statement_linenums[MAX_STATEMENTS];
-
-extern	function_t  functions[MAX_FUNCTIONS];
-extern	int			numfunctions;
-
-extern	double		pr_globals[MAX_REGS];
-extern	int			numpr_globals;
-
-int	CopyString(char *str);
 
 double * PR_Parameter(int p);
 
-#endif
+#endif /* __COAL_API_H__ */
 
 //--- editor settings ---
 // vi:ts=4:sw=4:noexpandtab
