9bbe60c3e3d0bb92a186ed21e5ae4fab7b17b6b4
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-10 04:38:39 +0000

    Eureka: merged s_split.cc, s_slice.cc into --> s_misc.cc

--- tools/eureka/src/s_misc.cc
+++ tools/eureka/src/s_misc.cc
@@ -29,7 +29,9 @@ Place, Suite 330, Boston, MA 02111-1307, USA.
 
 
 #include "yadex.h"
+
 #include <math.h>
+#include <map>
 
 /// #include "entry.h"
 #include "gfx.h"
@@ -38,6 +40,9 @@ Place, Suite 330, Boston, MA 02111-1307, USA.
 #include "selectn.h"
 #include "objects.h"
 #include "dialog.h"
+#include "x_hover.h"
+#include "s_linedefs.h"
+#include "s_misc.h"
 
 
 /*
@@ -515,4 +520,466 @@ void SuperSectorSelector (int map_x, int map_y, int new_sec)
    }
 }
 
-/* end of file */
+
+
+/*
+   split a sector in two, adding a new linedef between the two vertices
+*/
+
+void SplitSector (int vertex1, int vertex2)
+{
+SelPtr llist;
+int    curv, s, l, sd;
+char   msg1[80], msg2[80];
+
+/* AYM 1998-08-09 : FIXME : I'm afraid this test is not relevant
+   if the sector contains subsectors. I should ask Jim (Flynn),
+   he did something about that in DETH. */
+/* Check if there is a sector between the two vertices (in the middle) */
+Objid o;
+GetCurObject (o, OBJ_SECTORS,
+        (Vertices[vertex1].x + Vertices[vertex2].x) / 2,
+        (Vertices[vertex1].y + Vertices[vertex2].y) / 2);
+s = o.num;
+if (s < 0)
+   {
+   Beep ();
+   sprintf (msg1, "There is no sector between vertex #%d and vertex #%d",
+     vertex1, vertex2);
+   Notify (-1, -1, msg1, NULL);
+   return;
+   }
+
+/* Check if there is a closed path from <vertex1> to <vertex2>,
+   along the edge of sector <s>. To make it faster, I scan only
+   the set of linedefs that face sector <s>. */
+
+obj_no_t *ld_numbers;
+int nlinedefs = linedefs_of_sector (s, ld_numbers);
+if (nlinedefs < 1)  // Can't happen
+   {
+   nf_bug ("SplitSector: no linedef for sector %d\n", s);
+   return;
+   }
+llist = NULL;
+curv = vertex1;
+while (curv != vertex2)
+   {
+   printf ("%d\n", curv);
+   int n;
+   for (n = 0; n < nlinedefs; n++)
+      {
+      if (IsSelected (llist, ld_numbers[n]))
+   continue;  // Already been there
+      const LDPtr ld = LineDefs + ld_numbers[n];
+      if (ld->start == curv
+    && is_sidedef (ld->sidedef1) && SideDefs[ld->sidedef1].sector == s)
+         {
+   curv = ld->end;
+   SelectObject (&llist, ld_numbers[n]);
+   break;
+         }
+      if (ld->end == curv
+    && is_sidedef (ld->sidedef2) && SideDefs[ld->sidedef2].sector == s)
+   {
+   curv = ld->start;
+   SelectObject (&llist, ld_numbers[n]);
+   break;
+         }
+      }
+   if (n >= nlinedefs)
+      {
+      Beep ();
+      sprintf (msg1, "Cannot find a closed path from vertex #%d to vertex #%d",
+        vertex1, vertex2);
+      if (curv == vertex1)
+   sprintf (msg2, "There is no sidedef starting from vertex #%d"
+     " on sector #%d", vertex1, s);
+      else
+   sprintf (msg2, "Check if sector #%d is closed"
+     " (cannot go past vertex #%d)", s, curv);
+      Notify (-1, -1, msg1, msg2);
+      ForgetSelection (&llist);
+      delete[] ld_numbers;
+      return;
+      }
+   if (curv == vertex1)
+      {
+      Beep ();
+      sprintf (msg1, "Vertex #%d is not on the same sector (#%d)"
+        " as vertex #%d", vertex2, s, vertex1);
+      Notify (-1, -1, msg1, NULL);
+      ForgetSelection (&llist);
+      delete[] ld_numbers;
+      return;
+      }
+   }
+delete[] ld_numbers;
+/* now, the list of linedefs for the new sector is in llist */
+
+/* add the new sector, linedef and sidedefs */
+InsertObject (OBJ_SECTORS, s, 0, 0);
+InsertObject (OBJ_LINEDEFS, -1, 0, 0);
+LineDefs[NumLineDefs - 1].start = vertex1;
+LineDefs[NumLineDefs - 1].end = vertex2;
+LineDefs[NumLineDefs - 1].flags = 4;
+InsertObject (OBJ_SIDEDEFS, -1, 0, 0);
+SideDefs[NumSideDefs - 1].sector = s;
+strncpy (SideDefs[NumSideDefs - 1].middle, "-", WAD_TEX_NAME);
+InsertObject (OBJ_SIDEDEFS, -1, 0, 0);
+strncpy (SideDefs[NumSideDefs - 1].middle, "-", WAD_TEX_NAME);
+
+LineDefs[NumLineDefs - 1].sidedef1 = NumSideDefs - 2;
+LineDefs[NumLineDefs - 1].sidedef2 = NumSideDefs - 1;
+
+/* bind all linedefs in llist to the new sector */
+while (llist)
+{
+   sd = LineDefs[llist->objnum].sidedef1;
+   if (sd < 0 || SideDefs[sd].sector != s)
+      sd = LineDefs[llist->objnum].sidedef2;
+   SideDefs[sd].sector = NumSectors - 1;
+   UnSelectObject (&llist, llist->objnum);
+}
+
+/* second check... useful for sectors within sectors */
+
+for (l = 0; l < NumLineDefs; l++)
+{
+   sd = LineDefs[l].sidedef1;
+   if (sd >= 0 && SideDefs[sd].sector == s)
+   {
+      curv = GetOppositeSector (l, 1);
+      
+      if (curv == NumSectors - 1)
+   SideDefs[sd].sector = NumSectors - 1;
+   }
+   sd = LineDefs[l].sidedef2;
+   if (sd >= 0 && SideDefs[sd].sector == s)
+   {
+      curv = GetOppositeSector (l, 0);
+      
+      if (curv == NumSectors - 1)
+   SideDefs[sd].sector = NumSectors - 1;
+   }
+}
+
+MadeChanges = 1;
+MadeMapChanges = 1;
+}
+
+
+
+/*
+   split two linedefs, then split the sector and add a new linedef between the new vertices
+*/
+
+void SplitLineDefsAndSector (int linedef1, int linedef2)
+{
+SelPtr llist;
+int    s1, s2, s3, s4;
+char   msg[80];
+
+/* check if the two linedefs are adjacent to the same sector */
+
+s1 = LineDefs[linedef1].sidedef1;
+s2 = LineDefs[linedef1].sidedef2;
+s3 = LineDefs[linedef2].sidedef1;
+s4 = LineDefs[linedef2].sidedef2;
+
+if (s1 >= 0)
+   s1 = SideDefs[s1].sector;
+if (s2 >= 0)
+   s2 = SideDefs[s2].sector;
+if (s3 >= 0)
+   s3 = SideDefs[s3].sector;
+if (s4 >= 0)
+   s4 = SideDefs[s4].sector;
+if ((s1 < 0 || (s1 != s3 && s1 != s4)) && (s2 < 0 || (s2 != s3 && s2 != s4)))
+{
+   Beep ();
+   sprintf (msg, "Linedefs #%d and #%d are not adjacent to the same sector",
+     linedef1, linedef2);
+   Notify (-1, -1, msg, NULL);
+   return;
+}
+/* split the two linedefs and create two new vertices */
+llist = NULL;
+SelectObject (&llist, linedef1);
+SelectObject (&llist, linedef2);
+SplitLineDefs (llist);
+ForgetSelection (&llist);
+/* split the sector and create a linedef between the two vertices */
+SplitSector (NumVertices - 1, NumVertices - 2);
+}
+
+
+
+class Secref      // Reference to a sector
+{
+  public :
+    Secref () : linedef1 ('\0'), linedef2 ('\0') { }
+    char linedef1;
+    char linedef2;
+};
+
+
+/*
+ *  sector_slice - cut a slice out of a sector
+ */
+void sector_slice (obj_no_t linedef1, obj_no_t linedef2)
+{
+  /* We have between 0 and 4 sidedefs. We need to make sure that
+     there are exactly 2 that face the same sector and that they
+     belong to different linedefs.
+
+     If a linedef has more than one sidedef that faces the same
+     sector, we can't decide which one faces the other linedef.
+     Well, we can but it's hard for a dummy like me.
+
+     The problem is the same if the linedefs have two sectors in
+     common. Consider the following setup :
+    ____________________________________
+         |                                    |
+         |              sector 0              |
+         |      ________________________      |
+         |     |                        |     |
+         |     |        sector 1        |     |
+         |     |      ____________      |     |
+         |     |     |            |     |     |
+         |     |     |  sector 0  |-L1  |-L2  |
+         |     |     |____________|     |     |
+         |     |                        |     |
+         |     |                        |     |
+         |     |________________________|     |
+         |                                    |
+         |                                    |
+         |____________________________________|
+
+     How do you decide to which vertex of L2 should the start
+     vertex of L1 be linked ? */
+
+  if (! is_linedef (linedef1))    // Paranoia
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf,
+  "First argument is not a valid linedef number");
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  if (! is_linedef (linedef2))    // Paranoia
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf,
+  "Second argument is not a valid linedef number");
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  if (linedef1 == linedef2)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf, "Both arguments are the same linedef");
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  obj_no_t l1start = LineDefs[linedef1].start;
+  obj_no_t l1end   = LineDefs[linedef1].end;
+  obj_no_t l2start = LineDefs[linedef2].start;
+  obj_no_t l2end   = LineDefs[linedef2].end;
+
+  if (l1start == l2start && l1end == l2end
+    || l1start == l2end && l1end == l2start)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf, "Linedefs %d and %d are superimposed",
+        int (linedef1), int (linedef2));
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  obj_no_t l1sdr = LineDefs[linedef1].sidedef1;
+  obj_no_t l1sdl = LineDefs[linedef1].sidedef2;
+  obj_no_t l2sdr = LineDefs[linedef2].sidedef1;
+  obj_no_t l2sdl = LineDefs[linedef2].sidedef2;
+
+  obj_no_t l1sr = is_sidedef (l1sdr) ? SideDefs[l1sdr].sector : OBJ_NO_NONE;
+  obj_no_t l1sl = is_sidedef (l1sdl) ? SideDefs[l1sdl].sector : OBJ_NO_NONE;
+  obj_no_t l2sr = is_sidedef (l2sdr) ? SideDefs[l2sdr].sector : OBJ_NO_NONE;
+  obj_no_t l2sl = is_sidedef (l2sdl) ? SideDefs[l2sdl].sector : OBJ_NO_NONE;
+
+  if (is_sector (l1sr) && is_sector (l1sl) && l1sr == l1sl)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf, "Linedef %d has both sides in the same sector",
+  int (linedef1));
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  if (is_sector (l2sr) && is_sector (l2sl) && l2sr == l2sl)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf, "Linedef %d has both sides in the same sector",
+  int (linedef2));
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  // Verify that the linedefs have exactly one sector in common
+  typedef std::map<obj_no_t, Secref> secref_list_t;
+  secref_list_t secref;
+  if (is_sector (l1sr))
+    secref[l1sr].linedef1 = 'r';
+  if (is_sector (l1sl))
+    secref[l1sl].linedef1 = 'l';
+  if (is_sector (l2sr))
+    secref[l2sr].linedef2 = 'r';
+  if (is_sector (l2sl))
+    secref[l2sl].linedef2 = 'l';
+  obj_no_t sector;
+  int count = 0;
+  for (secref_list_t::iterator i = secref.begin (); i != secref.end (); i++)
+  {
+    if (i->second.linedef1 != '\0' && i->second.linedef2 != '\0')
+    {
+      sector = i->first;
+      count++;
+    }
+  }
+  if (count < 1)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf,
+  "Linedefs %d and %d don't face the same sector",
+  int (linedef1), int (linedef2));
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+  if (count > 1)
+  {
+    char buf[100];
+    y_snprintf (buf, sizeof buf,
+  "Linedefs %d and %d have more than one sector in common",
+  int (linedef1), int (linedef2));
+    Notify (-1, -1, buf, 0);
+    return;
+  }
+
+  // Insert new sector between linedefs
+  obj_no_t la0, la1;    // Start and end of the first linedef (the
+        // one that goes from linedef1 to linedef2)
+  obj_no_t lb0, lb1;    // Start and end of the second linedef (the
+        // one that goes from linedef2 to linedef1)
+  char side = secref[sector].linedef1;
+  if (side == 'r')
+  {
+    la0 = l1end;
+    lb1 = l1start;
+  }
+  else if (side == 'l')
+  {
+    la0 = l1start;
+    lb1 = l1end;
+  }
+  else          // Can't happen
+  {
+    nf_bug ("sector %d: linedef1 = %02Xh", int (sector), side);
+    return;
+  }
+
+  side = secref[sector].linedef2;
+  if (side == 'r')
+  {
+    la1 = l2start;
+    lb0 = l2end;
+  }
+  else if (side == 'l')
+  {
+    la1 = l2end;
+    lb0 = l2start;
+  }
+  else          // Can't happen
+  {
+    nf_bug ("sector %d: linedef2 = %02Xh", int (sector), side);
+    return;
+  }
+
+  // Verify that there's no linedef already between linedef1 and linedef2
+  {
+    for (int n = 0; n < NumLineDefs; n++)
+    {
+      if (n == linedef1 || n == linedef2)
+  continue;
+      if (LineDefs[n].start == la0 && LineDefs[n].end == la1
+       || LineDefs[n].start == la1 && LineDefs[n].end == la0
+       || LineDefs[n].start == lb0 && LineDefs[n].end == lb1
+       || LineDefs[n].start == lb1 && LineDefs[n].end == lb0)
+      {
+  char buf[100];
+  y_snprintf (buf, sizeof buf,
+      "A linedef already exists between linedefs %d and %d (linedef %d)",
+      int (linedef1), int (linedef2), int (n));
+  Notify (-1, -1, buf, 0);
+  return;
+      }
+    }
+  }
+  
+  // Create new sector
+  InsertObject (OBJ_SECTORS, sector, 0, 0);
+
+  // Create new linedef from linedef1 to linedef2
+  if (la0 != la1)
+  {
+    InsertObject (OBJ_LINEDEFS, -1, 0, 0);
+    LineDefs[NumLineDefs - 1].start = la0;
+    LineDefs[NumLineDefs - 1].end   = la1;
+    LineDefs[NumLineDefs - 1].flags = 4;
+    InsertObject (OBJ_SIDEDEFS, -1, 0, 0);    // Right sidedef
+    SideDefs[NumSideDefs - 1].sector = NumSectors - 1;  // Redundant
+    strncpy (SideDefs[NumSideDefs - 1].middle, "-", 8);
+    LineDefs[NumLineDefs - 1].sidedef1 = NumSideDefs - 1;
+    InsertObject (OBJ_SIDEDEFS, -1, 0, 0);    // Left sidedef
+    SideDefs[NumSideDefs - 1].sector = sector;
+    strncpy (SideDefs[NumSideDefs - 1].middle, "-", 8);
+    LineDefs[NumLineDefs - 1].sidedef2 = NumSideDefs - 1;
+  }
+
+  // Create new linedef from linedef2 to linedef1
+  if (lb0 != lb1)
+  {
+    InsertObject (OBJ_LINEDEFS, -1, 0, 0);
+    LineDefs[NumLineDefs - 1].start = lb0;
+    LineDefs[NumLineDefs - 1].end   = lb1;
+    LineDefs[NumLineDefs - 1].flags = 4;
+    InsertObject (OBJ_SIDEDEFS, -1, 0, 0);    // Right sidedef
+    SideDefs[NumSideDefs - 1].sector = NumSectors - 1;  // Redundant
+    strncpy (SideDefs[NumSideDefs - 1].middle, "-", 8);
+    LineDefs[NumLineDefs - 1].sidedef1 = NumSideDefs - 1;
+    InsertObject (OBJ_SIDEDEFS, -1, 0, 0);    // Left sidedef
+    SideDefs[NumSideDefs - 1].sector = sector;
+    strncpy (SideDefs[NumSideDefs - 1].middle, "-", 8);
+    LineDefs[NumLineDefs - 1].sidedef2 = NumSideDefs - 1;
+  }
+
+  // Adjust sector references for linedef1
+  side = secref[sector].linedef1;
+  if (side == 'r')
+    SideDefs[LineDefs[linedef1].sidedef1].sector = NumSectors - 1;
+  else if (side == 'l')
+    SideDefs[LineDefs[linedef1].sidedef2].sector = NumSectors - 1;
+
+  // Adjust sector references for linedef2
+  side = secref[sector].linedef2;
+  if (side == 'r')
+    SideDefs[LineDefs[linedef2].sidedef1].sector = NumSectors - 1;
+  else if (side == 'l')
+    SideDefs[LineDefs[linedef2].sidedef2].sector = NumSectors - 1;
+
+  MadeChanges = 1;
+  MadeMapChanges = 1;
+}
+
