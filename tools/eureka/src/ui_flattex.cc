fbfd4c34122c8396f1050818b74d57f1efa8090f
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-11 08:07:17 +0000

    Eureka: more work on the Texture list widget.

--- tools/eureka/src/ui_flattex.cc
+++ tools/eureka/src/ui_flattex.cc
@@ -26,11 +26,17 @@
 #include "things.h"
 #include "w_structs.h"
 #include "r_images.h"
+#include "levels.h"
 
 
 FlatTex_Choice::FlatTex_Choice(int X, int Y, int W, int H) :
-	Fl_Box(FL_BORDER_BOX, X, Y, W, H, "")
+	Fl_Box(FL_DOWN_BOX, X, Y, W, H, "")
 {
+	align(FL_ALIGN_INSIDE | FL_ALIGN_LEFT);
+	labelfont(FL_COURIER);
+	labelsize(QF_F);
+	color(FL_DARK3, FL_DARK3);
+
 	// TODO
 }
 
@@ -41,9 +47,28 @@ FlatTex_Choice::~FlatTex_Choice()
 
 void FlatTex_Choice::draw()
 {
-	// TODO
+	Fl_Box::draw();
 }
 
+
+int FlatTex_Choice::CalcHeight() const
+{
+	return h();
+}
+
+
+class My_ClipGroup : public Fl_Group
+{
+	public:
+		My_ClipGroup(int X, int Y, int W, int H, const char *L = NULL) :
+			Fl_Group(X, Y, W, H, L)
+	{
+		clip_children(1);
+	}
+
+	virtual ~My_ClipGroup()
+	{ }
+};
 
 //------------------------------------------------------------------------
 
@@ -81,23 +106,191 @@ UI_FlatTexList::UI_FlatTexList(int X, int Y, int W, int H, const char *label) :
 	add(mode);
 
 
-	int mx = X+0;
-	int my = Y + 100;
-	int mw = W-0 - Fl::scrollbar_size();
-	int mh = H - 100;
+	mx = X + Fl::scrollbar_size();
+	my = Y + 100;
+	mw = W-0 - Fl::scrollbar_size();
+	mh = H - 100;
 
-	sbar = new Fl_Scrollbar(mx+mw*0, my, Fl::scrollbar_size(), mh);
-//	sbar->callback(callback_Scroll, this);
+	offset_y = 0;
+	total_h  = 0;
+
+
+	sbar = new Fl_Scrollbar(X, my, Fl::scrollbar_size(), mh);
+  	sbar->callback(callback_Scroll, this);
 	sbar->color(FL_DARK2, FL_DARK2);
 
 	add(sbar);
 
 
-	// TODO
+	pack = new My_ClipGroup(mx, my, mw, mh);
+	pack->end();
+
+	add(pack);
+
+
+	Populate();
+
+	PositionAll();
 }
 
 UI_FlatTexList::~UI_FlatTexList()
 {
+}
+
+
+void UI_FlatTexList::Populate()
+{
+	int y = pack->y();
+
+	int mx = pack->x();
+	int mw = pack->w();
+
+	for (int i = 0; i < NumWTexture; i++)
+	{
+		FlatTex_Choice *ftc = new FlatTex_Choice(mx, y, mw, 24);
+
+		ftc->name = std::string(WTexture[i]);
+		ftc->label(ftc->name.c_str());
+fprintf(stderr, "Added FlatTex: %s\n", ftc->name.c_str());
+		pack->add(ftc);
+
+		y += ftc->h();
+	}
+}
+
+
+void UI_FlatTexList::PositionAll(FlatTex_Choice *focus)
+{
+	// determine focus [closest to top without going past it]
+	if (! focus)
+	{
+		int best_dist = 9999;
+
+		for (int j = 0; j < pack->children(); j++)
+		{
+			FlatTex_Choice *M = (FlatTex_Choice *) pack->child(j);
+			SYS_ASSERT(M);
+
+			if (!M->visible() || M->y() < my || M->y() >= my+mh)
+				continue;
+
+			int dist = M->y() - my;
+
+			if (dist < best_dist)
+			{
+				focus = M;
+				best_dist = dist;
+			}
+		}
+	}
+
+
+	// calculate new total height
+	int new_height = 0;
+	int spacing = 0;
+
+	for (int k = 0; k < pack->children(); k++)
+	{
+		FlatTex_Choice *M = (FlatTex_Choice *) pack->child(k);
+		SYS_ASSERT(M);
+
+		if (M->visible())
+			new_height += M->CalcHeight() + spacing;
+	}
+
+
+	// determine new offset_y
+	if (new_height <= mh)
+	{
+		offset_y = 0;
+	}
+	else if (focus)
+	{
+		int focus_oy = focus->y() - my;
+
+		int above_h = 0;
+		for (int k = 0; k < pack->children(); k++)
+		{
+			FlatTex_Choice *M = (FlatTex_Choice *) pack->child(k);
+
+			if (M->visible() && M->y() < focus->y())
+			{
+				above_h += M->CalcHeight() + spacing;
+			}
+		}
+
+		offset_y = above_h - focus_oy;
+
+		offset_y = MAX(offset_y, 0);
+		offset_y = MIN(offset_y, new_height - mh);
+	}
+	else
+	{
+		// when not shrinking, offset_y will remain valid
+		if (new_height < total_h)
+			offset_y = 0;
+	}
+
+	total_h = new_height;
+
+	SYS_ASSERT(offset_y >= 0);
+	SYS_ASSERT(offset_y <= total_h);
+
+
+	// reposition all the choices
+	int ny = my - offset_y;
+
+	for (int j = 0; j < pack->children(); j++)
+	{
+		FlatTex_Choice *M = (FlatTex_Choice *) pack->child(j);
+		SYS_ASSERT(M);
+
+		int nh = M->visible() ? M->CalcHeight() : 1;
+
+		if (ny != M->y() || nh != M->h())
+		{
+			M->resize(M->x(), ny, M->w(), nh);
+		}
+
+		if (M->visible())
+			ny += M->CalcHeight() + spacing;
+	}
+
+
+	// p = position, first line displayed
+	// w = window, number of lines displayed
+	// t = top, number of first line
+	// l = length, total number of lines
+	sbar->value(offset_y, mh, 0, total_h);
+
+	if (sbar->slider_size() < 0.06)
+		sbar->slider_size(0.06);
+
+	pack->redraw();
+}
+
+
+void UI_FlatTexList::callback_Scroll(Fl_Widget *w, void *data)
+{
+	Fl_Scrollbar *sbar = (Fl_Scrollbar *)w;
+	UI_FlatTexList *that = main_win->tex_list;
+
+	int previous_y = that->offset_y;
+
+	that->offset_y = sbar->value();
+
+	int dy = that->offset_y - previous_y;
+
+	// simply reposition all the UI_Module widgets
+	for (int j = 0; j < that->pack->children(); j++)
+	{
+		Fl_Widget *F = that->pack->child(j);
+		SYS_ASSERT(F);
+
+		F->resize(F->x(), F->y() - dy, F->w(), F->h());
+	}
+
+	that->redraw();
 }
 
 
