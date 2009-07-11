17775630f7df0fdb9afb9c6d5764f8cfd22c2a9d
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-11 05:04:50 +0000

    Eureka: bit more work on texture selector.

--- tools/eureka/src/ui_flattex.cc
+++ tools/eureka/src/ui_flattex.cc
@@ -53,9 +53,45 @@ UI_FlatTexList::UI_FlatTexList(int X, int Y, int W, int H, const char *label) :
 {
 	end(); // cancel begin() in Fl_Group constructor
 
-	box(FL_FLAT_BOX);
+	box(FL_FLAT_BOX);  // box(FL_DOWN_BOX);
 
-	color(FL_DARK3, FL_DARK3);
+	color(FL_DARK3, FL_DARK3); // FL_GRAY0+1, FL_GRAY0+1);
+
+
+///---	Fl_Group *top = new Fl_Group(X, Y, W, 100);
+///---	top->end();
+///---	top->box(FL_DOWN_BOX);
+///---	top->color(FL_GRAY0+1, FL_GRAY0+1);
+///---
+///---	add(top);
+
+	title_lab = new Fl_Box(X, Y+10, W, 22+QF*4, "Texture List");
+	title_lab->labelsize(20+QF*4);
+	add(title_lab);
+
+	mode = new Fl_Choice(X+50, Y+36, W-60, 24, "Mode:");
+	mode->align(FL_ALIGN_LEFT);
+	mode->color(FL_DARK3, FL_DARK3);
+	mode->add("Images|Names");
+	mode->value(0);
+//	mode->callback(mode_callback, this);
+	mode->labelsize(QF_F);
+	mode->textsize(QF_F);
+
+	add(mode);
+
+
+	int mx = X+0;
+	int my = Y + 100;
+	int mw = W-0 - Fl::scrollbar_size();
+	int mh = H - 100;
+
+	sbar = new Fl_Scrollbar(mx+mw*0, my, Fl::scrollbar_size(), mh);
+//	sbar->callback(callback_Scroll, this);
+	sbar->color(FL_DARK2, FL_DARK2);
+
+	add(sbar);
+
 
 	// TODO
 }
