84c5975b94fbdd37edb3a26ac49f12630f12c668
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-17 04:21:44 +0000

    Eureka: fleshed out the BASIS API (BA_Begin, BA_Change, etc).

--- tools/eureka/src/e_basis.h
+++ tools/eureka/src/e_basis.h
@@ -45,6 +45,20 @@
 //
 
 
+// Object types
+typedef enum
+{
+	OBJ_NONE,
+	OBJ_THINGS,
+	OBJ_LINEDEFS,
+	OBJ_SIDEDEFS,
+	OBJ_VERTICES,
+	OBJ_SECTORS,
+	OBJ_RADTRIGS,
+}
+obj_type_t;
+
+
 class Thing
 {
 public:
@@ -185,6 +199,12 @@ extern std::vector<RadTrig *> RadTrigs;
 #define NumLineDefs   ((int)LineDefs.size())
 #define NumRadTrigs   ((int)RadTrigs.size())
 
+#define is_thing(n)    ((n) >= 0 && (n) < NumThings  )
+#define is_vertex(n)   ((n) >= 0 && (n) < NumVertices)
+#define is_sector(n)   ((n) >= 0 && (n) < NumSectors )
+#define is_sidedef(n)  ((n) >= 0 && (n) < NumSideDefs)
+#define is_linedef(n)  ((n) >= 0 && (n) < NumLineDefs)
+#define is_radtrig(n)  ((n) >= 0 && (n) < NumRadTrigs)
 
 enum
 {
@@ -219,6 +239,56 @@ private:
 
 
 typedef std::vector<edit_op_c> op_group_c;
+
+
+/* BASIS API */
+
+void BA_Begin();
+// begin a group of operations that will become a single undo/redo
+// step.  All stored _redo_ steps will be removed.  The BA_New,
+// BA_Delete and BA_Change calls must only be called between
+// BA_Begin() and BA_End() pairs.
+
+void BA_End();
+// finish a group of operations.
+
+int BA_New(obj_type_t type);
+// returns the new objnum
+
+void BA_Delete(obj_type_t type, int objnum);
+// deletes the given object, and in certain cases other types of
+// objects bound to it (e.g. deleting a vertex will cause all
+// bound linedefs to also be deleted).
+
+bool BA_Change(obj_type_t type, int objnum, byte field, int value);
+// change a field of an existing object.  If the value was the
+// same as before, nothing happens and false is returned.
+// Otherwise returns true.
+
+bool BA_Undo();
+// attempt to undo the last normal or redo operation.  Returns
+// false if the undo history is empty.
+
+bool BA_Redo();
+// attempt to re-do the last undo operation.  Returns false if
+// there is no stored redo steps.
+
+int BA_InternaliseString(const char *str);
+// add this string to the basis string table (if it doesn't
+// already exist) and return its integer offset.
+
+const char *BA_GetString(int offset);
+// get the string from the basis string table.
+
+
+/* HELPERS */
+
+bool BA_ChangeTH(int thing, byte field, int value);
+bool BA_ChangeLD(int line,  byte field, int value);
+bool BA_ChangeSD(int side,  byte field, int value);
+bool BA_ChangeSEC(int sec,  byte field, int value);
+bool BA_ChangeVR(int vert,  byte field, int value);
+bool BA_ChangeRT(int rad,   byte field, int value);
 
 
 #endif  /* __E_BASIS_H__ */
