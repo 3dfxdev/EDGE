a1e2ba39ac47c8b163e08a838fc91b119e58e463
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-18 13:27:22 +0000

    Coal: added appropriate top-of-file comments.

--- coal/c_execute.cc
+++ coal/c_execute.cc
@@ -1,22 +1,26 @@
-/*
-Copyright (C) 1996-1997 Id Software, Inc.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
+//----------------------------------------------------------------------
+//  COAL EXECUTION ENGINE
+//----------------------------------------------------------------------
+// 
+//  Copyright (C)      2009  Andrew Apted
+//  Copyright (C) 1996-1997  Id Software, Inc.
+//
+//  Coal is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as
+//  published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  Coal is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+//  the GNU General Public License for more details.
+//
+//----------------------------------------------------------------------
+//
+//  Based on QCC (the Quake-C Compiler) and the corresponding
+//  execution engine from the Quake source code.
+//
+//----------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
