eed6f84dfc17ffd25ba61112a2d52bc27dcb2a16
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-07-19 10:24:02 +0000

    Engine: tweaked dynlight rendering.

--- edge/src/p_blockmap.h
+++ edge/src/p_blockmap.h
@@ -31,7 +31,7 @@
 // mapblocks are used to check movement
 // against lines and things
 #define BLOCKMAP_UNIT  128
-#define LIGHTMAP_UNIT  512
+#define LIGHTMAP_UNIT  256
 
 #define BMAP_END  ((unsigned short) 0xFFFF)
 
