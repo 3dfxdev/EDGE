26a030eab065daef3ce2f462b80ad6160f2973d8
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-08-02 08:12:54 +0000

    Eureka: dead code removal.

--- tools/eureka/src/im_color.h
+++ tools/eureka/src/im_color.h
@@ -90,48 +90,6 @@ extern int colour0;   // Game colour to which g. colour 0 is remapped
 extern int sky_colour;    // Game colour for a medium blue sky
 
 
-/*
- *  Game_colour_24 - convert game colours to pixel values
- *  
- *  This class is used to speed up displaying of images on
- *  visuals where bits_per_pixel is 24.
- *
- *  Game_colour_24::lut() returns a pointer to a const table
- *  that is similar to game_colour[] except that it is
- *  optimized toward the needs of display_img() :
- *  each member can be readily copied into the XImage buffer
- *  (this is not true of game_colour[], at least not if the
- *  client is big-endian or does not have the same
- *  endianness as the server).
- *
- *  There is exactly 1 instance of this class (global). It's
- *  refreshed in InitGfx() and used in display_img().
- *  To avoid wasting memory when it's not needed (when the
- *  depth is != 24), the array is allocated upon refresh and
- *  refresh() is only called when the depth is == 24.
- */
-
-
-typedef byte pv24_t[3];  // A 24-bit pixel value
-
-
-class Game_colour_24
-{
-  public :
-    Game_colour_24 () { pv_table = 0; }
-    ~Game_colour_24 () { if (pv_table) delete[] pv_table; }
-    // Create/refresh the table
-    void refresh (const pcolour_t *game_colour, const bool big_endian);
-    // Return a pointer on an array of pv24_t[DOOM_COLOURS]
-    const pv24_t *lut () { return pv_table; }
-  private :
-    pv24_t *pv_table;
-};
-
-
-extern Game_colour_24 game_colour_24;
-
-
 //------------------------------------------------------------//
 
 
