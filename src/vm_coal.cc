0e5d7fc86b91e9c00c1157f7c309ab4e01de4eed
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-08-09 10:18:45 +0000

    Implemented these coal native functions:     sys.print    math.floor     sys.debug    math.ceil     sys.assert   math.random

--- src/vm_coal.cc
+++ src/vm_coal.cc
@@ -64,15 +64,99 @@ void VM_CallFunction(coal::vm_c *vm, const char *name)
 
 
 //------------------------------------------------------------------------
+//  BASE MODULES
+//------------------------------------------------------------------------
+
+
+// sys.print(str)
+//
+static void SYS_print(coal::vm_c *vm, int argc)
+{
+	const char * s = vm->AccessParamString(0);
+
+	I_Printf("%s\n", s);
+}
+
+
+// sys.debug(str)
+//
+static void SYS_debug(coal::vm_c *vm, int argc)
+{
+	const char * s = vm->AccessParamString(0);
+
+	I_Debugf("%s\n", s);
+}
+
+
+// sys.assert(val)
+//
+static void SYS_assert(coal::vm_c *vm, int argc)
+{
+	double * p = vm->AccessParam(0);
+
+	if (*p == 0)
+		I_Error("Assertion in coal script failed.\n");
+}
+
+
+// math.floor(val)
+//
+static void MATH_floor(coal::vm_c *vm, int argc)
+{
+	double * p = vm->AccessParam(0);
+
+//!!!!!!	vm->SetResult(floor(*p));
+}
+
+
+// math.ceil(val)
+//
+static void MATH_ceil(coal::vm_c *vm, int argc)
+{
+	double * p = vm->AccessParam(0);
+
+//!!!!!!	vm->SetResult(ceil(*p));
+}
+
+
+// math.random()
+//
+static void MATH_random(coal::vm_c *vm, int argc)
+{
+	int r = rand();
+
+	r = (r ^ (r >> 18)) & 0xFFFF;
+
+	//!!!!!!	vm->SetResult(r / double(0x10000));
+}
+
+
+//------------------------------------------------------------------------
+
+void VM_RegisterBASE(coal::vm_c *vm)
+{
+	// SYSTEM
+    vm->AddNativeFunction("sys.print",       SYS_print);
+    vm->AddNativeFunction("sys.debug",       SYS_debug);
+    vm->AddNativeFunction("sys.assert",      SYS_assert);
+
+	// MATH
+    vm->AddNativeFunction("math.floor",      MATH_floor);
+    vm->AddNativeFunction("math.ceil",       MATH_ceil);
+    vm->AddNativeFunction("math.random",     MATH_random);
+
+	// STRINGS
+}
+
 
 void VM_InitCoal()
 {
 	ui_vm = coal::CreateVM();
 
+	VM_RegisterBASE(ui_vm);
 	VM_RegisterHUD();
 	VM_RegisterPlaysim();
 }
-
 
 void VM_QuitCoal()
 {
