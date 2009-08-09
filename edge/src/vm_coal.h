9f33138b177f28abb76e1093805edf8b13bb2dcb
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-08-09 09:33:26 +0000

    Beginnings of coal interface for EDGE...

--- edge/src/vm_coal.h
+++ edge/src/vm_coal.h
@@ -1,8 +1,8 @@
 //------------------------------------------------------------------------
-//  LUA General Stuff
+//  COAL General Stuff
 //------------------------------------------------------------------------
 //
-//  Copyright (c) 2006-2008  The EDGE Team.
+//  Copyright (c) 2006-2009  The EDGE Team.
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -16,59 +16,18 @@
 //
 //------------------------------------------------------------------------
 
-#ifndef __L_LUA_H__
-#define __L_LUA_H__
+#ifndef __VM_COAL_H__
+#define __VM_COAL_H__
 
-// forward definitions
-struct lua_State;
-struct luaL_Reg;
+void VM_InitCoal();
+void VM_QuitCoal();
 
-// LUA VM Instance
-class lua_vm_c
-{
-public:
-    struct var_s
-    {
-        const char *name;
-        int type;
-        int value;
-    };
-    
-private:
-    lua_State *state;
+void VM_LoadCoalFire(const char *filename);
 
-public:
-     lua_vm_c();
-    ~lua_vm_c();
+void VM_RegisterHUD();
+void VM_RegisterPlaysim();
 
-public:
-    void Open();
-
-    void CallFunction(const char *parent_name, const char *name);
-    void LoadModule(const char *name, const luaL_Reg *funcs);
-    void LoadBuffer(const char *name, char *buffer, int length);
-
-/// lua_State* GetState() { return this->state; } 
-/// std::string RunCommand(const char *cmd);
-
-    void SetIntVar(const char *table, const char *name, int value);
-    void SetBoolVar(const char *table, const char *name, bool value);
-    void SetStringVar(const char *table, const char *name, const char * value);
-
-private:
-	static int init_lua(lua_State *L);
-	static void open_basic_libs(lua_State *L);
-	static void remove_loaders(lua_State *L);
-};
-
-extern lua_vm_c *vm; // The instance
-
-void LU_Init();
-void LU_Close();
-
-void LU_LoadScripts();
-
-#endif // __L_LUA_H__
+#endif // __VM_COAL_H__
 
 //--- editor settings ---
 // vi:ts=4:sw=4:noexpandtab
