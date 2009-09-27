7bf8b79317363860c5721c21c71c3936dc05f94e
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-09-27 06:22:44 +0000

    heck in 'n134' branch (code from 1.31b).

---
+++ n134/src/p_action.h
@@ -0,0 +1,185 @@
+//----------------------------------------------------------------------------
+//  EDGE Play Simulation Action routines
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2008  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+//  Based on the DOOM source code, released by Id Software under the
+//  following copyright:
+//
+//    Copyright (C) 1993-1996 by id Software, Inc.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __P_ACTION_H__
+#define __P_ACTION_H__
+
+struct mobj_s;
+
+// Weapon Action Routine pointers
+void A_Light0(struct mobj_s *mo);
+void A_Light1(struct mobj_s *mo);
+void A_Light2(struct mobj_s *mo);
+
+void A_WeaponReady(struct mobj_s *mo);
+void A_WeaponEmpty(struct mobj_s *mo);
+void A_WeaponShoot(struct mobj_s *mo);
+void A_WeaponEject(struct mobj_s *mo);
+void A_WeaponJump(struct mobj_s *mo);
+void A_Lower(struct mobj_s *mo);
+void A_Raise(struct mobj_s *mo);
+void A_ReFire(struct mobj_s *mo);
+void A_NoFire(struct mobj_s *mo);
+void A_NoFireReturn(struct mobj_s *mo);
+void A_CheckReload(struct mobj_s *mo);
+void A_SFXWeapon1(struct mobj_s *mo);
+void A_SFXWeapon2(struct mobj_s *mo);
+void A_SFXWeapon3(struct mobj_s *mo);
+void A_WeaponPlaySound(struct mobj_s *mo);
+void A_WeaponKillSound(struct mobj_s *mo);
+void A_WeaponTransSet(struct mobj_s *mo);
+void A_WeaponTransFade(struct mobj_s *mo);
+void A_WeaponEnableRadTrig(struct mobj_s *mo);
+void A_WeaponDisableRadTrig(struct mobj_s *mo);
+
+void A_SetCrosshair(struct mobj_s *mo);
+void A_TargetJump(struct mobj_s *mo);
+void A_FriendJump(struct mobj_s *mo);
+void A_GunFlash(struct mobj_s *mo);
+void A_WeaponKick(struct mobj_s *mo);
+void A_WeaponSetSkin(struct mobj_s *mo);
+
+void A_WeaponShootSA(struct mobj_s *mo);
+void A_ReFireSA(struct mobj_s *mo);
+void A_NoFireSA(struct mobj_s *mo);
+void A_NoFireReturnSA(struct mobj_s *mo);
+void A_CheckReloadSA(struct mobj_s *mo);
+void A_GunFlashSA(struct mobj_s *mo);
+
+// Needed for the bossbrain.
+void P_ActBrainScream(struct mobj_s *mo);
+void P_ActBrainDie(struct mobj_s *mo);
+void P_ActBrainSpit(struct mobj_s *mo);
+void P_ActCubeSpawn(struct mobj_s *mo);
+void P_ActBrainMissileExplode(struct mobj_s *mo);
+
+// Visibility Actions
+void P_ActTransSet(struct mobj_s *mo);
+void P_ActTransFade(struct mobj_s *mo);
+void P_ActTransMore(struct mobj_s *mo);
+void P_ActTransLess(struct mobj_s *mo);
+void P_ActTransAlternate(struct mobj_s *mo);
+
+// Sound Actions
+void P_ActPlaySound(struct mobj_s *mo);
+void P_ActKillSound(struct mobj_s *mo);
+void P_ActMakeAmbientSound(struct mobj_s *mo);
+void P_ActMakeAmbientSoundRandom(struct mobj_s *mo);
+void P_ActMakeCloseAttemptSound(struct mobj_s *mo);
+void P_ActMakeDyingSound(struct mobj_s *mo);
+void P_ActMakeOverKillSound(struct mobj_s *mo);
+void P_ActMakePainSound(struct mobj_s *mo);
+void P_ActMakeRangeAttemptSound(struct mobj_s *mo);
+void P_ActMakeActiveSound(struct mobj_s *mo);
+void P_ActPlayerScream(struct mobj_s *mo);
+
+// Explosion Damage Actions
+void P_ActDamageExplosion(struct mobj_s *mo);
+void P_ActThrust(struct mobj_s *mo);
+
+// Stand-by / Looking Actions
+void P_ActStandardLook(struct mobj_s *mo);
+void P_ActPlayerSupportLook(struct mobj_s *mo);
+
+// Meander, aimless movement actions.
+void P_ActStandardMeander(struct mobj_s *mo);
+void P_ActPlayerSupportMeander(struct mobj_s *mo);
+
+// Chasing Actions
+void P_ActResurrectChase(struct mobj_s *mo);
+void P_ActStandardChase(struct mobj_s *mo);
+void P_ActWalkSoundChase(struct mobj_s *mo);
+
+// Attacking Actions
+void P_ActComboAttack(struct mobj_s *mo);
+void P_ActMeleeAttack(struct mobj_s *mo);
+void P_ActRangeAttack(struct mobj_s *mo);
+void P_ActSpareAttack(struct mobj_s *mo);
+void P_ActRefireCheck(struct mobj_s *mo);
+void P_ActReloadCheck(struct mobj_s *mo);
+void P_ActReloadReset(struct mobj_s *mo);
+
+// Miscellanous
+void P_ActFaceTarget(struct mobj_s *mo);
+void P_ActMakeIntoCorpse(struct mobj_s *mo);
+void P_ActResetSpreadCount(struct mobj_s *mo);
+void P_ActExplode(struct mobj_s *mo);
+void P_ActActivateLineType(struct mobj_s *mo);
+void P_ActEnableRadTrig(struct mobj_s *mo);
+void P_ActDisableRadTrig(struct mobj_s *mo);
+void P_ActTouchyRearm(struct mobj_s *mo);
+void P_ActTouchyDisarm(struct mobj_s *mo);
+void P_ActBounceRearm(struct mobj_s *mo);
+void P_ActBounceDisarm(struct mobj_s *mo);
+void P_ActPathCheck(struct mobj_s *mo);
+void P_ActPathFollow(struct mobj_s *mo);
+
+void P_ActDropItem(struct mobj_s *mo);
+void P_ActSpawn(struct mobj_s *mo);
+void P_ActDLightSet(struct mobj_s *mo);
+void P_ActDLightSet2(struct mobj_s *mo);
+void P_ActDLightFade(struct mobj_s *mo);
+void P_ActDLightRandom(struct mobj_s *mo);
+void P_ActDLightColour(struct mobj_s *mo);
+void P_ActSetSkin(struct mobj_s *mo);
+void P_ActDie(struct mobj_s *mo);
+void P_ActKeenDie(struct mobj_s *mo);
+void P_ActCheckBlood(struct mobj_s *mo);
+void P_ActJump(struct mobj_s *mo);
+void P_ActBecome(struct mobj_s *mo);
+void P_ActSetInvuln(struct mobj_s *mo);
+void P_ActClearInvuln(struct mobj_s *mo);
+
+// Movement actions
+void P_ActFaceDir(struct mobj_s *mo);
+void P_ActTurnDir(struct mobj_s *mo);
+void P_ActTurnRandom(struct mobj_s *mo);
+void P_ActMlookFace(struct mobj_s *mo);
+void P_ActMlookTurn(struct mobj_s *mo);
+void P_ActMoveFwd(struct mobj_s *mo);
+void P_ActMoveRight(struct mobj_s *mo);
+void P_ActMoveUp(struct mobj_s *mo);
+void P_ActStopMoving(struct mobj_s *mo);
+void P_ActCheckMoving(struct mobj_s *mo);
+void P_ActCheckActivity(struct mobj_s *mo);
+
+// Projectiles
+void P_ActHomingProjectile(struct mobj_s *mo);
+void P_ActLaunchOrderedSpread(struct mobj_s *mo);
+void P_ActLaunchRandomSpread(struct mobj_s *mo);
+void P_ActCreateSmokeTrail(struct mobj_s *mo);
+void P_ActHomeToSpot(struct mobj_s *mo);
+bool P_ActLookForTargets(struct mobj_s *mo);
+
+// Trackers
+void P_ActEffectTracker(struct mobj_s *mo);
+void P_ActTrackerActive(struct mobj_s *mo);
+void P_ActTrackerFollow(struct mobj_s *mo);
+void P_ActTrackerStart(struct mobj_s *mo);
+
+#endif /* __P_ACTION_H__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ n134/src/p_switch.cc
@@ -0,0 +1,260 @@
+//----------------------------------------------------------------------------
+//  EDGE Switch Handling Code
+//----------------------------------------------------------------------------
+// 
+//  Copyright (c) 1999-2008  The EDGE Team.
+// 
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+//  Based on the DOOM source code, released by Id Software under the
+//  following copyright:
+//
+//    Copyright (C) 1993-1996 by id Software, Inc.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+
+#include "ddf/main.h"
+#include "ddf/switch.h"
+
+#include "dm_defs.h"
+#include "dm_state.h"
+#include "g_game.h"
+#include "p_local.h"
+#include "r_state.h"
+#include "s_sound.h"
+#include "w_texture.h"
+
+
+std::vector<button_t *> active_buttons;
+
+
+void P_InitSwitchList(void)
+{
+	// only called at game initialization.
+
+	epi::array_iterator_c it;
+	
+	for (it = switchdefs.GetBaseIterator(); it.IsValid(); it++)
+	{
+		switchdef_c *sw = ITERATOR_TO_TYPE(it, switchdef_c*);
+
+		sw->cache.image[0] = W_ImageLookup(sw->name1, INS_Texture, ILF_Null);
+		sw->cache.image[1] = W_ImageLookup(sw->name2, INS_Texture, ILF_Null);
+	}
+}
+
+//
+// Start a button counting down till it turns off.
+//
+static void StartButton(switchdef_c *sw, line_t *line, bwhere_e w,
+		const image_c *image)
+{
+	// See if button is already pressed
+	if (P_ButtonIsPressed(line))
+		return;
+
+	button_t * b = NULL;
+
+	std::vector<button_t *>::iterator BI;
+
+	for (BI = active_buttons.begin(); BI != active_buttons.end(); BI++)
+	{
+		if ((*BI)->btimer == 0)
+		{
+			b = *BI; break;
+		}
+	}
+
+	if (! b)
+	{
+		b = new button_t;
+
+		active_buttons.push_back(b);
+	}
+
+	b->line = line;
+	b->where = w;
+	b->btimer = sw->time;
+	b->off_sound = sw->off_sfx;
+	b->bimage = image;
+}
+
+//
+// P_ChangeSwitchTexture
+//
+// Function that changes wall texture.
+// Tell it if switch is ok to use again.
+//
+// -KM- 1998/09/01 All switches referencing a certain tag are switched
+//
+
+#define CHECK_SW(PART)  (sw->cache.image[k] == side->PART.image)
+#define SET_SW(PART)    side->PART.image = sw->cache.image[k^1] 
+#define OLD_SW          sw->cache.image[k]
+
+void P_ChangeSwitchTexture(line_t * line, bool useAgain,
+		line_special_e specials, bool noSound)
+{
+	for (int j = 0; j < numlines; j++)
+	{
+		if (line != &lines[j])
+		{
+			if (line->tag == 0 ||
+				line->tag != lines[j].tag ||
+                (specials & LINSP_SwitchSeparate) ||
+                (useAgain && line->special && line->special != lines[j].special))
+			{
+				continue;
+			}
+		}
+
+		side_t *side = lines[j].side[0];
+
+		position_c *sfx_origin = &lines[j].frontsector->sfx_origin;
+
+		bwhere_e pos = BWH_None;
+
+		epi::array_iterator_c it;
+	
+		// Note: reverse order, give priority to newer switches.
+		for (it = switchdefs.GetTailIterator(); 
+             it.IsValid() && (pos == BWH_None); 
+             it--)
+		{
+			switchdef_c *sw = ITERATOR_TO_TYPE(it, switchdef_c*);
+
+			if (!sw->cache.image[0] && !sw->cache.image[1])
+				continue;
+
+			int k;
+
+			// some like it both ways...
+			for (k = 0; k < 2; k++)
+			{
+				if (CHECK_SW(top))
+				{
+					SET_SW(top);
+					pos = BWH_Top;
+					break;
+				}
+				else if (CHECK_SW(middle))
+				{
+					SET_SW(middle);
+					pos = BWH_Middle;
+					break;
+				}
+				else if (CHECK_SW(bottom))
+				{
+					SET_SW(bottom);
+					pos = BWH_Bottom;
+					break;
+				}
+			}   // k < 2
+
+			if (pos != BWH_None)
+			{
+				// -KM- 98/07/31 Implement sounds
+				if (! noSound && sw->on_sfx)
+				{
+					SYS_ASSERT(sfx_origin);
+					S_StartFX(sw->on_sfx, SNCAT_Level, sfx_origin);
+					noSound = true;
+				}
+
+				if (useAgain)
+					StartButton(sw, &lines[j], pos, OLD_SW);
+
+				break;
+			}
+		}   // it.IsValid() - switchdefs
+	}   // j < numlines
+}
+
+#undef CHECK_SW
+#undef SET_SW
+#undef OLD_SW
+
+
+void P_ClearButtons(void)
+{
+	std::vector<button_t *>::iterator BI;
+
+	for (BI = active_buttons.begin(); BI != active_buttons.end(); BI++)
+	{
+		delete (*BI);
+	}
+	active_buttons.clear();
+}
+
+bool P_ButtonIsPressed(line_t *ld)
+{
+	std::vector<button_t *>::iterator BI;
+
+	for (BI = active_buttons.begin(); BI != active_buttons.end(); BI++)
+	{
+		if ((*BI)->btimer > 0 && (*BI)->line == ld)
+			return true;
+	}
+
+	return false;
+}
+
+void P_UpdateButtons(void)
+{
+	std::vector<button_t *>::iterator BI;
+
+	for (BI = active_buttons.begin(); BI != active_buttons.end(); BI++)
+	{
+		button_t *b = *BI;
+		SYS_ASSERT(b);
+
+		if (b->btimer == 0)
+			continue;
+
+		b->btimer--;
+
+		if (b->btimer == 0)
+		{
+			switch (b->where)
+			{
+				case BWH_Top:
+					b->line->side[0]->top.image = b->bimage;
+					break;
+
+				case BWH_Middle:
+					b->line->side[0]->middle.image = b->bimage;
+					break;
+
+				case BWH_Bottom:
+					b->line->side[0]->bottom.image = b->bimage;
+					break;
+
+				case BWH_None:
+					I_Error("INTERNAL ERROR: bwhere is BWH_None!\n");
+			}
+
+			if (b->off_sound)
+			{
+				S_StartFX(b->off_sound, SNCAT_Level,
+						  &b->line->frontsector->sfx_origin);
+			}
+
+			Z_Clear(b, button_t, 1);
+		}
+	}
+}
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
