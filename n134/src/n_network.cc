3ad77db41833867f61e6bffd8b31ceb34a80d4f1
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-10-04 07:19:50 +0000

    n134: replaced var_diskicon and var_hogcpu with cvar equivalents.

--- n134/src/n_network.cc
+++ n134/src/n_network.cc
@@ -44,10 +44,7 @@ bool netgame = false;
 
 int base_port;
 
-
-bool var_hogcpu = true;
-
-extern gameflags_t default_gameflags;
+cvar_c m_busywait;
 
 
 int gametic;
@@ -55,6 +52,8 @@ int maketic;
 
 static int last_update_tic;
 static int last_tryrun_tic;
+
+extern gameflags_t default_gameflags;
 
 
 //----------------------------------------------------------------------------
@@ -97,7 +96,7 @@ static void GetPackets(bool do_delay)
 	if (! netgame)
 	{
 		// -AJA- This can make everything a bit "jerky" :-(
-		if (do_delay && ! var_hogcpu)
+		if (do_delay && ! m_busywait.d)
 			I_Sleep(10 /* millis */);
 
 		return;
@@ -107,7 +106,7 @@ static void GetPackets(bool do_delay)
 
 	NLsocket socks[4];  // only one in the group
 
-	int delay = (do_delay && ! var_hogcpu) ? 10 /* millis */ : 0;
+	int delay = (do_delay && ! m_busywait.d) ? 10 /* millis */ : 0;
 	int num = nlPollGroup(sk_group, NL_READ_STATUS, socks, 4, delay);
 
 	if (num < 1)
