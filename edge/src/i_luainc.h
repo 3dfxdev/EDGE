7a82ff975de0a4d5680c8449e7c1a704ed251050
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2009-10-19 03:33:24 +0000

    Updated copyright year for 2009.

--- edge/src/i_luainc.h
+++ edge/src/i_luainc.h
@@ -2,7 +2,7 @@
 //  LUA INCLUDES
 //------------------------------------------------------------------------
 //
-//  Copyright (c) 2005-2008  The EDGE Team.
+//  Copyright (c) 2005-2009  The EDGE Team.
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
