30e65935c368a0ec7d28474067d654bf303e1482
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2010-06-13 04:26:06 +0000

    Began work on configurable automap keys.

--- edge/src/m_misc.cc
+++ edge/src/m_misc.cc
@@ -228,6 +228,19 @@ static default_t defaults[] =
     {CFGT_Key,      "key_reload",        &key_reload,     CFGDEF_KEY_RELOAD},  // -AJA- 2004/11/11.
     {CFGT_Key,      "key_action1",       &key_action1,    CFGDEF_KEY_ACTION1},  // -AJA- 2009/09/07
     {CFGT_Key,      "key_action2",       &key_action2,    CFGDEF_KEY_ACTION2},  // -AJA- 2009/09/07
+
+	// -AJA- 2010/06/13: automap keys
+	{CFGT_Key,      "key_am_up",         &key_am_up,      0},
+	{CFGT_Key,      "key_am_down",       &key_am_down,    0},
+	{CFGT_Key,      "key_am_left",       &key_am_left,    0},
+	{CFGT_Key,      "key_am_right",      &key_am_right,   0},
+	{CFGT_Key,      "key_am_zoomin",     &key_am_zoomin,  0},
+	{CFGT_Key,      "key_am_zoomout",    &key_am_zoomout, 0},
+	{CFGT_Key,      "key_am_gobig",      &key_am_gobig,   0},
+	{CFGT_Key,      "key_am_follow",     &key_am_follow,  0},
+	{CFGT_Key,      "key_am_grid",       &key_am_grid,    0},
+	{CFGT_Key,      "key_am_addmark",    &key_am_addmark, 0},
+	{CFGT_Key,      "key_am_clear",      &key_am_clear,   0},
 };
 
 
