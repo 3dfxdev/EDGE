a51b9c90368c900faf0a80dcb3a9a03d570a2abd
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2010-12-01 12:32:48 +0000

    Added "hacx" as an IWAD name to look for.

--- src/e_main.cc
+++ src/e_main.cc
@@ -818,7 +818,7 @@ void InitDirectories(void)
 //
 // Adds an IWAD and EDGE.WAD. -ES-  2000/01/01 Rewritten.
 //
-const char *wadname[] = { "doom2", "doom", "plutonia", "tnt", "freedoom", "freedm", NULL };
+const char *wadname[] = { "doom2", "doom", "plutonia", "tnt", "hacx", "freedoom", "freedm", NULL };
 
 static void IdentifyVersion(void)
 {
