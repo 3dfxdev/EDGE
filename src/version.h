4a1f4a3197e5f4244772def6fb77bd150bdbd75b
ajapted <ajapted@28424279-154d-4b81-a449-c98279361746>
2011-01-14 03:59:40 +0000

    Version bump to RC#2

--- src/version.h
+++ src/version.h
@@ -20,10 +20,10 @@
 
 #define EDGEVER       135
 #define EDGEVERHEX  0x135
-#define EDGEVERSTR  "1.35(RC1)"
+#define EDGEVERSTR  "1.35(RC2)"
 
 // patch level (Savegames and Demos)
-#define EDGEPATCH  5
+#define EDGEPATCH  6
 
 // -ES- 2000/03/04 The version of EDGE.WAD we require.
 #define EDGE_WAD_VERSION  601
