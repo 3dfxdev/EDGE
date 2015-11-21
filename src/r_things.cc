cd1fe6c0192e882720340a75d5098a96226a6e07
Coraline <corbinne@gmail.com>
2015-11-21 14:00:55 -0800

    Update r_things.cc
    updated header

--- src/r_things.cc
+++ src/r_things.cc
@@ -1,8 +1,9 @@
 //----------------------------------------------------------------------------
-//  EDGE2 OpenGL Rendering (Things)
+//  EDGE2 OpenGL Thing Rendering
 //----------------------------------------------------------------------------
 // 
 //  Copyright (c) 1999-2009  The EDGE2 Team.
+//  Copyright (c) 2015 Isotope SoftWorks
 // 
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
