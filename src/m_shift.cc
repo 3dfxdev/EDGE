dd6a2a57bf8fe0c19008f60cd00c1f164b09f75e
Brendan Doe <brendan.doe@gmail.com>
2016-06-04 11:22:39 -0700

    fixed a problem with the shift transform which was causing garbage characters to output to the console when pressing shift.

--- src/m_shift.cc
+++ src/m_shift.cc
@@ -1,18 +1,16 @@
 //m_shift.c
 //shift transformation tables
 #include "i_defs.h"
-
+//TODO: there's probably a more layout-independant way to do this with SDL2.
 const char* shiftxform;
 
 const char english_shiftxform[] = 
 {
-
-    0,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31,
-    ' ', '!', '"', '#', '$', '%', '&',
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+	10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+	20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+	30, 31, 32,
+    '!', '"', '#', '$', '%', '&',
     '"', // shift-'
     '(', ')', '*', '+',
     '<', // shift-,
@@ -36,17 +34,38 @@ const char english_shiftxform[] =
     '>', '?', '@',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '[', // shift-[
-    '!', // shift-backslash - OH MY GOD DOES WATCOM SUCK
-    ']', // shift-]
+    '{', // shift-[
+    '|', // shift-backslash
+    '}', // shift-]
     '"', '_',
-    '\'', // shift-`
+    '~', // shift-`
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     '{', '|', '}', '~', 127
 };
 
+const char debug_shiftxform[] = 
+{
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+	10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+	20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+	30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+	40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+	50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+	60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+	70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+	80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+	90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+	100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+	110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+	120, 121, 122, 123, 124, 125, 126, 127
+};
+
 void M_InitShiftXForm(void) 
 {
-    shiftxform = english_shiftxform;
+#ifdef DEBUGSHIFTXFORM 
+	shiftxform = debug_shiftxform;
+#else
+	shiftxform = english_shiftxform;
+#endif
 }
