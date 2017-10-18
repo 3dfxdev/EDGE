d3fe13adf57f5408643b737c2257c1a00a9337a8
Corbachu <corbinne@gmail.com>
2017-10-18 13:46:31 -0700

    ROTT: More Rise of the Traid support code (though, most of this, like RTL/Rlew expansion, can be handled from existing WLF_UTIL . . )

--- src/games/rott/rott_util.cc
+++ src/games/rott/rott_util.cc
@@ -16,14 +16,16 @@
 //
 //----------------------------------------------------------------------------
 
-#include "../src/i_defs.h"
+//#include "../../system/i_defs.h"
 
-#include "../../epi/endianess.h"
+#include "../../../epi/endianess.h"
 
 #include "rott_local.h"
 
-#define NEARTAG	 0xA7
-#define FARTAG	 0xA8
+//#define NEARTAG	 0xA7
+//#define FARTAG	 0xA8
+
+int elevatorstart;
 
 /// CA- 6.2.2016: CA_RLEW_EXPAND UTILITY FUNCTION (from rt_ted.c)
 //  AJA - also swaps endianess
@@ -61,6 +63,69 @@ L_WriteDebug("%04x%c", source[k], (k%8 == 7) ? '\n' : ' ');
 				*dest++ = val2;
 		}
 	}
+}
+
+
+int GetWallIndex (int texture)
+{
+    int wallstart;
+    int exitstart;
+
+    wallstart = W_GetNumForName ("WALLSTRT");
+    exitstart = W_GetNumForName ("EXITSTRT");
+    elevatorstart = W_GetNumForName ("ELEVSTRT");
+
+    if (texture & 0x1000) {
+	texture &= ~0x1000;
+	if (texture == 0)
+	    return 41;
+	else if (texture == 1)
+	    return 90;
+	else if (texture == 2)
+	    return 91;
+	else if (texture == 3)
+	    return 42;
+	else if (texture == 4)
+	    return 92;
+	else if (texture == 5)
+	    return 93;
+	else if (texture == 6)
+	    return 94;
+	else if (texture == 7)
+	    return 95;
+	else if (texture == 8)
+	    return 96;
+	else if (texture == 9)
+	    return 97;
+	else if (texture == 10)
+	    return 98;
+	else if (texture == 11)
+	    return 99;
+	else if (texture == 12)
+	    return 100;
+	else if (texture == 13)
+	    return 101;
+	else if (texture == 14)
+	    return 102;
+	else if (texture == 15)
+	    return 103;
+	else if (texture == 16)
+	    return 104;
+    } else if (texture > elevatorstart)
+	return (texture - elevatorstart + 68);
+//      else if (texture > specialstart)
+//      return (texture - specialstart + 41);
+    else if (texture > exitstart)
+	return (texture - exitstart + 43);
+    else {
+	if (texture > wallstart + 63)
+	    return (texture - (wallstart + 63) + 76);
+	else if (texture > wallstart + 40)
+	    return (texture - (wallstart + 40) + 45);
+	else
+	    return (texture - wallstart);
+    }
+    return 0x8000;
 }
 
 /* void WF_Huff_Expand(const byte *source, int src_len, byte *dest, int dest_len,
