70428fa845c81cfb5cb14f8e43cdb468dc407d42
Corbachu <corbinne@gmail.com>
2017-10-28 16:56:52 -0700

    ROTT: Adding rott_resource.h and rott_writebmp.cc to src/games/rott directory

---
+++ src/games/rott/rott_resource.h
@@ -0,0 +1,58 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ROTTConv.rc
+//
+#define IDC_MYICON                      2
+#define IDD_ROTTCONV_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_ROTTCONV                    107
+#define IDI_SMALL                       108
+#define IDC_ROTTCONV                    109
+#define IDR_MAINFRAME                   128
+#define ID_FILE_CONVERTROTTSPRITE       129
+#define ID_FILE_CONVERTPCX              130
+#define ID_FILE_OPENPCX                 131
+#define ID_FILE_OPENROTTSPRITE          132
+#define IDR_PAL1                        134
+#define IDD_ROTTSPRITEINFO              135
+#define ID_FILE_BREAKWAD                136
+#define ID_FILE_MAKEWAD                 137
+#define IDD_SPRITEDRAW                  138
+#define ID_GRAPHICS_VIEWRLFSPRITES      139
+#define ID_WADS_VIEWWADINFO             141
+#define IDC_STATIC1                     1000
+#define IDC_STATIC2                     1001
+#define IDOK1                           1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT3                       1005
+#define IDC_EDIT4                       1006
+#define IDC_EDIT5                       1007
+#define IDC_EDIT6                       1008
+#define IDC_STATIC11                    1009
+#define IDC_STATIC12                    1010
+#define IDC_STATIC13                    1011
+#define IDC_STATIC14                    1012
+#define IDC_STATIC15                    1013
+#define IDC_STATIC16                    1014
+#define IDC_STATIC17                    1015
+#define IDC_STATIC18                    1016
+#define IDC_SFXLIST                     1017
+#define IDC_TAB1                        1018
+#define IDC_STATIC                      -1
+#define IDC_STATIC3                     -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
