0f8df17dca5aa24bccbd169d05d91d449557ca6d
Corbachu <corbinne@gmail.com>
2017-10-28 17:04:33 -0700

    ROTT: Moved all graphic conversion defines out of r_rottgfx.cc and into rott_local.h - which should be included for any and all ROTT stuff

--- src/games/rott/rott_local.h
+++ src/games/rott/rott_local.h
@@ -22,6 +22,11 @@
 //#include "rott_data.h"
 #include "rott_defs.h"
 
+// WinROTT specific ----->
+#include "tile.h"
+#include "rott_resource.h"
+// <----- END WINROTT specific
+
 #include "../src/r_defs.h"
 
 #include "../epi/image_data.h"
@@ -31,5 +36,25 @@
 // --- globals ---
 
 #define MAPSIZE 128	// maps are 64*64 max
+
+// -- R_ROTTGFX.CC --
+int CntNbOfNotBGbyteu8_ts(u8_t *src, int w, int h);
+int CntNbOfBGbyteu8_ts(u8_t *src, int w, int h);
+u8_t makecol(u8_t r, u8_t g, u8_t b);
+int ConvertMaskedRottSpriteIntoPCX(u8_t  *RottSpriteMemptr, u8_t  *RottPCXptr);
+int ConvertRottSpriteIntoPCX(u8_t  *RottSpriteMemptr, u8_t  *RottPCXptr);
+void ConvertPicToPPic(int width, int height, u8_t *source, u8_t *target);
+void ConvertPPicToPic(int width, int height, u8_t *src, u8_t *tgt);
+int  ConvertPCXIntoRottSprite(u8_t  *RottSpriteMemptr, u8_t*srcdata, int lump, int lumplength, int origsize, int w, int h, int leftoffset, int topoffset);
+void RotatePicture(u8_t *src, int iWidth, int  iHeight);
+int ConvertPCXIntoRottTransSprite(u8_t  *RottSpriteMemptr, u8_t*srcdata, int lump, int lumplength, int origsize, int h, int w, int leftoffset, int topoffset, int translevel);
+int ConvertPCXIntoRott_pic_t(u8_t  *RottSpriteMemptr, u8_t*srcdata, int lump, int lumplength, int h, int w);
+void loadconvpal();
+void FlipPictureUpsideDown(int width, int height, u8_t *src);
+void ConvertPicToPPic(int width, int height, u8_t *source, u8_t *target);
+int GetRightShiftCount(DWORD dwVal);
+int GetLeftShiftCount(DWORD dwVal);
+void ShrinkMemPictureExt(int orgw, int orgh, int neww, int newy, u8_t* src, u8_t * target);
+
 
 #endif // __ROTT_LOCAL_H__
