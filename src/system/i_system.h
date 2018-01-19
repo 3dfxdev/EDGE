9327fcc0ba6161fc9b870b80ea025b5dc0359d49
Corbachu <corbinne@gmail.com>
2018-01-18 23:11:00 -0800

    System: Minor changes to system-specific stuff, as well as the addition of openglfile, which is a new debug.txt that we can create/call                 so it can isolate and record rendering-specific stuff (without needing to dig through DEBUG.txt).

--- src/system/i_system.h
+++ src/system/i_system.h
@@ -320,8 +320,11 @@ void I_SetGamma(float gamma);
 
 void I_Logf(const char *message,...) GCCATTR((format(printf, 1, 2)));
 
+void I_GLf(const char *message, ...) GCCATTR((format(printf, 1, 2)));
+
 #define L_WriteDebug  I_Debugf
 #define L_WriteLog    I_Logf
+#define L_WriteOpenGL I_GLf
 
 
 #endif /*__I_SYSTEM_H__*/
