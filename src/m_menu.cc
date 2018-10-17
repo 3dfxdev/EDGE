3b788058e336335163111a33e1283c8a8a824e81
Corbachu <corbinne@gmail.com>
2018-10-17 09:03:00 -0700

    Small additions
    Small additions for QDOOM, and usernameak's fix did indeed work, was just using modified version of QDOOM. (Sorry friend)

--- src/m_menu.cc
+++ src/m_menu.cc
@@ -1558,8 +1558,8 @@ void M_DrawEpisode(void)
 	
 	// Below fix from usernameak indeed works, but for modifiations that do not use the normal DOOM episode style (QDOOM), this breaks that system.
 	// Needs review, or at least a hack for EDGE-specific mods that use the episodic structure. .
-	//if (EpiDef.numitems == 1) 
-	//	M_Episode(0);
+	if (EpiDef.numitems == 1) 
+		M_Episode(0);
 
 	HUD_DrawImage(54, 38, menu_episode);
 }
