36f91b89424b89b4ad35d7858c0d29b545a40290
Rachael Alexanderson <raa-eruanna@users.noreply.github.com>
2018-10-27 00:34:19 -0400

    - fix unix physfs getter, update travis

--- quick_setup_scripts/unix-getphysfs.sh
+++ quick_setup_scripts/unix-getphysfs.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 
+cd $0/../..
+git clone https://github.com/Didstopia/physfs
 mkdir physfs/build
 cd physfs/build
-git clone https://github.com/Didstopia/physfs
 cmake .. -DCMAKE_BUILD_TYPE=Release -DPHYSFS_BUILD_STATIC=1
