fc50dfd05dedca087035fff6be12ec27da4da2de
Corbachu <corbinne@gmail.com>
2018-11-30 10:14:05 -0800

    Update i_sdlinc.h
    Testing this out strictly for the autobuild systems...don't panic if you suddenly can't build! [switch to /master and don't use this branch ;)]

--- src/system/i_sdlinc.h
+++ src/system/i_sdlinc.h
@@ -1,8 +1,8 @@
 //----------------------------------------------------------------------------
-//  EDGE2 SDL System Internal header
+//  EDGE SDL System Internal header
 //----------------------------------------------------------------------------
 //
-//  Copyright (c) 2005-2009  The EDGE2 Team.
+//  Copyright (c) 2005-2018  The EDGE Team.
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -19,14 +19,32 @@
 #ifndef __SDL_SYSTEM_INTERNAL_H__
 #define __SDL_SYSTEM_INTERNAL_H__
 
+// -CA- 2018/11/30:
+// Fix below so autobuild systems can find SDL.h. If you are using a system that supports
+// GCC/Linux/OSX and this does not work or invalidates your build, please promptly report
+// it so we can come up with another solution. Just open up an ISSUE or a PR via Github if
+// it does not work and/or you have a better way.
+
+#ifndef WIN32
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_keycode.h> /// Keys
+#include <SDL2/SDL_opengl.h> // opengl
+#include <SDL2/SDL_render.h> // kitchensink
+#include <SDL2/SDL_thread.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_mutex.h>
+
+#else
+
 #include <SDL.h>
 #include <SDL_keycode.h> /// Keys
 #include <SDL_opengl.h> // opengl
-
 #include <SDL_render.h> // kitchensink
 #include <SDL_thread.h>
 #include <SDL_surface.h>
 #include <SDL_mutex.h>
+
+#endif
 
 static inline SDL_bool sbool(bool b)
 {
