2217cd4d569769561d43185dc1b928a1d43a433f
Corbachu <corbinne@gmail.com>
2019-01-16 20:34:56 -0800

    Update con_main.cc
    Fix for XCode compiler

--- src/con_main.cc
+++ src/con_main.cc
@@ -402,7 +402,7 @@ int CMD_Help(char **argv, int argc)
 
 int CMD_Version(char **argv, int argc)
 {
-	I_Printf("EDGE (" EDGEPRINTBIT ") v" EDGEVERSTR "\n");
+	I_Printf("EDGE v" EDGEVERSTR "\n");
 	I_Printf("Platform: " EDGEPRINTBIT "\n");
 	return 0;
 }
