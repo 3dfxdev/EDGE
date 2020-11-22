fdfe564855d57207609838092778c789835975c3
Corbin <corbinne@gmail.com>
2020-11-22 14:12:29 -0800

    TIDY: SDL_Kit from /tools/ to /deps
    simple move

--- tools/SDL_kitchensink/src/internal/kitlibstate.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "kitchensink/internal/kitlibstate.h"
-
-static Kit_LibraryState _librarystate = {0, NULL, NULL};
-
-Kit_LibraryState* Kit_GetLibraryState() {
-    return &_librarystate;
-}
