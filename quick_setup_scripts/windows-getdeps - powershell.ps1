2ed41e1bd5a2e929d31b02e4a34500f8bb6438db
Corbin <corbinne@gmail.com>
2020-11-23 18:06:37 -0800

    quick_setup_scripts
    Tidied these up.

--- quick_setup_scripts/windows-getdeps - powershell.ps1
+++ quick_setup_scripts/windows-getdeps - powershell.ps1
@@ -1,6 +1,6 @@
 @echo -off
 setlocal
 set path=%path%;%~dp0\vcpkg
-./vcpkg install bzip2:x64-windows-static bzip2:x86-windows-static zlib:x64-windows-static zlib:x86-windows-static libogg:x64-windows-static libogg:x86-windows-static libvorbis:x64-windows-static libvorbis:x86-windows-static SDL2:x86-windows SDL2:x64-windows sdl2-net:x64-windows sdl2-net:x86-windows liblzma:x64-windows-static liblzma:x86-windows-static
+./vcpkg install sdl2:x86-windows sdl2:x64-windows libogg:x64-windows-static libogg:x86-windows-static libvorbis:x64-windows-static libvorbis:x86-windows-static physfs:x86-windows-static physfs:x64-windows-static zlib:x64-windows-static zlib:x86-windows-static bzip2:x64-windows-static bzip2:x86-windows-static liblzma:x64-windows-static liblzma:x86-windows-static
 ./vcpkg integrate install
  
