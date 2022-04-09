869db0540f3ce94af4b2d5b57c739e191526e258
Corbin <corbinne@gmail.com>
2022-04-08 18:42:39 -0700

    Hack for incorrect sprite naming
    https://github.com/dashodanger/EDGE-classic/commit/d08cc2db498de08fec5d8c8b17501478760c1afa

--- src/w_sprite.cc
+++ src/w_sprite.cc
@@ -403,9 +403,12 @@ static void MarkCompletedFrames(void)
 			frame->finished = 1;
 			finish_num++;
 
-			if (rot_count < frame->rots)
-				I_Warning("Sprite %s:%c is missing rotations (%d of %d).\n",
-					def->name, frame_ch, frame->rots - rot_count, frame->rots);
+			I_Warning("Sprite %s:%c is missing rotations (%d of %d).\n",
+				def->name, frame_ch, frame->rots - rot_count, frame->rots);
+
+			//Lobo: try to fix cases where some dumbass used A1 instead of A0
+			if (rot_count == 1)
+				frame->rots = 1;
 		}
 
 		// remove complete sprites from sprite_map
